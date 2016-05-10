aceda8aa6d0908421d9c2ed5f69c32850fbdbbc2
Paul Crowder <paul.crowder@blackbaud.com>
2016-05-10 13:25:13 -0400

    Animate tile expand/collapse (#3)

---
+++ src/modules/animation/slide.service.ts
@@ -0,0 +1,54 @@
+import {ElementRef, Injectable} from 'angular2/core';
+import {AnimationBuilder} from 'angular2/animate';
+
+@Injectable()
+export class SkySlideService {
+  private autoHeightTimeoutId: any;
+
+  constructor(private animationBuilder: AnimationBuilder) {
+
+  }
+
+  public slide(el: ElementRef, selector: string, direction: string, animate = false) {
+    clearTimeout(this.autoHeightTimeoutId);
+
+    let animateEl = el.nativeElement.querySelector(selector);
+    let animation = this.animationBuilder.css();
+    let duration = animate ? 250 : 0;
+
+    animation.setDuration(duration);
+
+    animateEl.removeAttribute('hidden');
+    animateEl.style.height = 'auto';
+    animateEl.style.display = 'block';
+
+    let height = animateEl.offsetHeight;
+
+    if (direction === 'up') {
+      animation
+        .setFromStyles({
+          height: height + 'px',
+          overflow: 'hidden'
+        })
+        .setToStyles({
+          height: '0px'
+        });
+    } else {
+      animation
+        .setFromStyles({
+          height: '0px'
+        })
+        .setToStyles({
+          height: height + 'px'
+        });
+    }
+
+    animation.start(animateEl);
+
+    if (direction === 'down') {
+      this.autoHeightTimeoutId = setTimeout(function () {
+        animateEl.style.height = 'auto';
+      }, duration + 50);
+    }
+  }
+}
