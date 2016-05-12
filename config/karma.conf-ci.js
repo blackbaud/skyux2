921f726e3e3c005cf24fefadaee5b148df0dcc48
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-12 15:35:53 -0400

    Specifying Browserstack base

--- config/karma.conf-ci.js
+++ config/karma.conf-ci.js
@@ -2,44 +2,44 @@ module.exports = function (config) {
   var defaultConfig = require('./karma.conf.js'),
     customLaunchers = {
       bs_windows_ie_11: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'ie',
         browser_version: '11.0',
         os: 'Windows',
         os_version: '10'
       },
       bs_windows_edge: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'edge',
         os: 'Windows',
         os_version: '10'
       },
       bs_windows_chrome_latest: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'chrome',
         os: 'Windows',
         os_version: '8.1'
       },
       bs_windows_firefox_latest: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'firefox',
         os: 'Windows',
         os_version: '8.1'
       },
       bs_osx_safari_latest: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'safari',
         os: 'OS X',
         os_version: 'Yosemite'
       },
       bs_osx_chrome_latest: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'chrome',
         os: 'OS X',
         os_version: 'Yosemite'
       },
       bs_osx_firefox_latest: {
-        base: base,
+        base: 'BrowserStack',
         browser: 'firefox',
         os: 'OS X',
         os_version: 'Yosemite'
@@ -48,7 +48,7 @@ module.exports = function (config) {
       // browser we're disabling this for now.
       //,
       // bs_android_samsung_galaxy_s5_4_4: {
-      //     base: base,
+      //     base: 'BrowserStack',
       //     device: 'Samsung Galaxy S5',
       //     os: 'android',
       //     os_version: '4.4'
