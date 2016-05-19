a6c65a66a5535f3680b2a416634a5ae317164b30
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-19 14:59:13 -0400

    Dots and progress both look bad

--- config/karma.conf-ci.js
+++ config/karma.conf-ci.js
@@ -117,7 +117,7 @@ module.exports = function (config) {
      * possible values: 'dots', 'progress'
      * available reporters: https://npmjs.org/browse/keyword/karma-reporter
      */
-    reporters: ['mocha', 'progress', 'coverage'],
+    reporters: ['mocha', 'coverage'],
 
     // web server port
     port: 9876,
