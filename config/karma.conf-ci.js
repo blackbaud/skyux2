a1f0ba6803ba89184ff4c5ec4b91c2701027f9e5
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-19 14:44:33 -0400

    Trying progress instead of dots

--- config/karma.conf-ci.js
+++ config/karma.conf-ci.js
@@ -117,7 +117,7 @@ module.exports = function (config) {
      * possible values: 'dots', 'progress'
      * available reporters: https://npmjs.org/browse/keyword/karma-reporter
      */
-    reporters: ['mocha', 'dots', 'coverage'],
+    reporters: ['mocha', 'progress', 'coverage'],
 
     // web server port
     port: 9876,
