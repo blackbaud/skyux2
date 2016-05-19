238d520bb0ea2af2fddb57fceb5a71034f8177f2
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-19 11:34:30 -0400

    Only running script if necessary

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -1,12 +1,7 @@
 # Fail the build if this step fails
 set -e
 
-# Only on a tagged commit
-if [ -n "$TRAVIS_TAG" ]; then
-
-  # Login to NPM, publish, and display message
-  echo -e "blackbaud-skyux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
-  npm publish
-  echo -e "blackbaud-skyux2 successfully deployed to NPM.\n"
-
-fi
+# Login to NPM, publish, and display message
+echo -e "blackbaud-skyux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
+npm publish
+echo -e "blackbaud-skyux2 successfully deployed to NPM.\n"
