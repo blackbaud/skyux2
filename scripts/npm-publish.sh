df37f979815c0854884d3f0235f5e369a37870c5
Bobby Earl <bobby.earl@blackbaud.com>
2016-05-20 13:10:52 -0400

    Moved npm publish script (#24)

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -4,4 +4,5 @@ set -e
 # Login to NPM, publish, and display message
 echo -e "blackbaud-skyux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
 npm publish
+npm logout
 echo -e "blackbaud-skyux2 successfully deployed to NPM.\n"
