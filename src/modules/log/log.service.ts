6e56da64c73cdfddbe7e6d355d9e4a458c2f9557
Paul Crowder <paul.crowder@blackbaud.com>
2016-06-01 10:58:30 -0400

    Fixed name clash

--- src/modules/log/log.service.ts
+++ src/modules/log/log.service.ts
@@ -2,13 +2,13 @@ import { Injectable, Optional } from '@angular/core';
 
 @Injectable()
 export class SkyLogService {
-  constructor(@Optional() private window: Window) {
-    this.window = this.window || window;
+  constructor(@Optional() private logWindow: Window) {
+    this.logWindow = this.logWindow || window;
   }
 
   public warn(message?: any, ...optionalParams: any[]) {
-    if (this.window.console) {
-      this.window.console.warn.apply(this, arguments);
+    if (this.logWindow.console) {
+      this.logWindow.console.warn.apply(this, arguments);
     }
   }
 }
