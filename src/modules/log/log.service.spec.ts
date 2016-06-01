a682c7d2c3272a9651ff6a2d6b224c58e4b2bbbe
Paul Crowder <paul.crowder@blackbaud.com>
2016-06-01 10:45:37 -0400

    Log service tests

--- src/modules/log/log.service.spec.ts
+++ src/modules/log/log.service.spec.ts
@@ -1,20 +1,45 @@
+import { provide } from '@angular/core';
 import {
+  beforeEachProviders,
   describe,
   expect,
+  inject,
   it
 } from '@angular/core/testing';
 
 import { SkyLogService } from './log.service';
 
 describe('Log service', () => {
-  'use strict';
+  let mockWindow = {
+    console: {
+      warn: () => {
 
-  it('should log warnings to the console', () => {
-    let logService = new SkyLogService(window);
-    let warnSpy = spyOn(console, 'warn');
+      }
+    }
+  };
 
-    logService.warn('This is the message', 'an optional parameter');
+  beforeEachProviders(() => [
+    provide(Window, {useValue: mockWindow}),
+    SkyLogService
+  ]);
 
-    expect(warnSpy).toHaveBeenCalledWith('This is the message', 'an optional parameter');
-  });
+  it(
+    'should log warnings to the console',
+    inject([SkyLogService], (logService: SkyLogService) => {
+      let warnSpy = spyOn(mockWindow.console, 'warn');
+
+      logService.warn('This is the message', 'an optional parameter');
+
+      expect(warnSpy).toHaveBeenCalledWith('This is the message', 'an optional parameter');
+    })
+  );
+
+  it(
+    'should sanity check the existence of console when warn() is called',
+    inject([SkyLogService], (logService: SkyLogService) => {
+      mockWindow.console = undefined;
+
+      logService.warn('This is the message', 'an optional parameter');
+    })
+  );
 });
