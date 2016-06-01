b0b7799b8d1e235ace578fb14ab1a0e6e1b6a0df
Paul Crowder <paul.crowder@blackbaud.com>
2016-06-01 11:07:30 -0400

    Stop injecting window since some browsers choke on it

--- src/modules/log/log.service.spec.ts
+++ src/modules/log/log.service.spec.ts
@@ -1,4 +1,3 @@
-import { provide } from '@angular/core';
 import {
   beforeEachProviders,
   describe,
@@ -10,36 +9,18 @@ import {
 import { SkyLogService } from './log.service';
 
 describe('Log service', () => {
-  let mockWindow = {
-    console: {
-      warn: () => {
-
-      }
-    }
-  };
-
   beforeEachProviders(() => [
-    provide(Window, {useValue: mockWindow}),
     SkyLogService
   ]);
 
   it(
     'should log warnings to the console',
     inject([SkyLogService], (logService: SkyLogService) => {
-      let warnSpy = spyOn(mockWindow.console, 'warn');
+      let warnSpy = spyOn(window.console, 'warn');
 
       logService.warn('This is the message', 'an optional parameter');
 
       expect(warnSpy).toHaveBeenCalledWith('This is the message', 'an optional parameter');
-    })
-  );
-
-  it(
-    'should sanity check the existence of console when warn() is called',
-    inject([SkyLogService], (logService: SkyLogService) => {
-      mockWindow.console = undefined;
-
-      logService.warn('This is the message', 'an optional parameter');
     })
   );
 });
--- src/modules/log/log.service.ts
+++ src/modules/log/log.service.ts
@@ -1,14 +1,11 @@
-import { Injectable, Optional } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SkyLogService {
-  constructor(@Optional() private logWindow: Window) {
-    this.logWindow = this.logWindow || window;
-  }
-
   public warn(message?: any, ...optionalParams: any[]) {
-    if (this.logWindow.console) {
-      this.logWindow.console.warn.apply(this, arguments);
+    /*istanbul ignore else */
+    if (window.console) {
+      window.console.warn.apply(this, arguments);
     }
   }
 }
