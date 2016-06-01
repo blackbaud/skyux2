b603b893a0a74180dda6c82274bc8c4cec77dfaa
Paul Crowder <paul.crowder@blackbaud.com>
2016-06-01 11:15:42 -0400

    Log tests (#33)
    * Log service test
    
    * Log service tests
    
    * Fixed name clash
    
    * Stop injecting window since some browsers choke on it

---
+++ src/modules/log/log.service.spec.ts
@@ -0,0 +1,26 @@
+import {
+  beforeEachProviders,
+  describe,
+  expect,
+  inject,
+  it
+} from '@angular/core/testing';
+
+import { SkyLogService } from './log.service';
+
+describe('Log service', () => {
+  beforeEachProviders(() => [
+    SkyLogService
+  ]);
+
+  it(
+    'should log warnings to the console',
+    inject([SkyLogService], (logService: SkyLogService) => {
+      let warnSpy = spyOn(window.console, 'warn');
+
+      logService.warn('This is the message', 'an optional parameter');
+
+      expect(warnSpy).toHaveBeenCalledWith('This is the message', 'an optional parameter');
+    })
+  );
+});
--- src/modules/log/log.service.ts
+++ src/modules/log/log.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SkyLogService {
   public warn(message?: any, ...optionalParams: any[]) {
+    /*istanbul ignore else */
     if (window.console) {
       window.console.warn.apply(this, arguments);
     }
