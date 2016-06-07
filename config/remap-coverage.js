f983a8e33aa48bc9a45392292d76d8e195a4bb52
Bobby Earl <bobby.earl@blackbaud.com>
2016-06-07 19:26:29 -0400

    Handling coverage remap when running locally

--- config/remap-coverage.js
+++ config/remap-coverage.js
@@ -1,17 +1,26 @@
 'use strict';
 
-var glob = require('glob'),
-  remap = require('remap-istanbul');
+let glob = require('glob');
+let remap = require('remap-istanbul');
 
-function remapSourceMap() {
-  var files = glob.sync('coverage/**/coverage-final.json');
-  var promises = [];
+function remapSourceMap(type) {
+  let files = glob.sync('coverage/**/coverage-final.json');
+  let promises = [];
 
-  console.log('Remapping ' + files.length + ' coverage JSON files.');
+  console.log('Remapping ' + files.length + ' coverage JSON files into ' + type);
+
   files.forEach(function (file) {
-    promises.push(remap(file, {
-      json: file
-    }));
+    let destination = file;
+    let output = {};
+
+    if (type === 'html') {
+      let folders = file.split('/');
+      let browser = folders.length > 2 ? folders[folders.length - 2] : '';
+      destination = 'coverage-remapped/' + browser + '/';
+    }
+
+    output[type] = destination;
+    promises.push(remap(file, output));
   });
 
   Promise.all(promises).then(function (a) {
@@ -19,4 +28,9 @@ function remapSourceMap() {
   });
 }
 
-remapSourceMap();
+// Read remap type
+if (process.argv.length > 2) {
+  remapSourceMap(process.argv[2]);
+} else {
+  console.log('Remapping type required.');
+}
