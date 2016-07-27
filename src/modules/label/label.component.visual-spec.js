07bdb1ec4d536225b8b562fd68ca81782a7c6be1
Paul Crowder <paul.crowder@blackbaud.com>
2016-07-27 10:46:50 -0400

    Disable visual test

--- src/modules/label/label.component.visual-spec.js
+++ src/modules/label/label.component.visual-spec.js
@@ -1,14 +1,15 @@
-describe('Label', () => {
-  'use strict';
+// TODO: Wait for PR#84 to be merged before attempting to visually test
+// describe('Label', () => {
+//   'use strict';
 
-  it('should match previous label screenshot', (done) => {
-    browser
-      .setupTest('/label.html')
-      .compareScreenshot({
-        screenshotName: 'labels-all',
-        selector: '#screenshot-label',
-        checkAccessibility: true
-      })
-      .call(done);
-  });
-});
+//   it('should match previous label screenshot', (done) => {
+//     browser
+//       .setupTest('/label.html')
+//       .compareScreenshot({
+//         screenshotName: 'labels-all',
+//         selector: '#screenshot-label',
+//         checkAccessibility: true
+//       })
+//       .call(done);
+//   });
+// });
