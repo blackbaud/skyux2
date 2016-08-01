7ee1ac1731e9ca0e7dc397e1d8c72177bffc6a30
Bobby Earl <bobby.earl@blackbaud.com>
2016-08-01 14:26:41 -0400

    Added tests for repeater.  Uncommented tests for label

--- src/modules/label/fixtures/label.component.visual-fixture.ts
+++ src/modules/label/fixtures/label.component.visual-fixture.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
-import { SkyLabelComponent } from '../label.component';
+import { SkyLabelComponent } from '../../../../src/core';
+import { Bootstrapper } from '../../../../visual/bootstrapper';
 
 @Component({
   selector: 'sky-demo-app',
   template: require('./label.component.visual-fixture.html'),
   directives: [SkyLabelComponent]
 })
-class AppComponent {
-}
+class AppComponent { }
+
+Bootstrapper.bootstrap(AppComponent);
--- src/modules/label/label.component.visual-spec.js
+++ src/modules/label/label.component.visual-spec.js
@@ -1,15 +1,14 @@
-// TODO: Wait for PR#84 to be merged before attempting to visually test
-// describe('Label', () => {
-//   'use strict';
+describe('Label', () => {
+  'use strict';
 
-//   it('should match previous label screenshot', (done) => {
-//     browser
-//       .setupTest('/label.html')
-//       .compareScreenshot({
-//         screenshotName: 'labels-all',
-//         selector: '#screenshot-label',
-//         checkAccessibility: true
-//       })
-//       .call(done);
-//   });
-// });
+  it('should match previous label screenshot', (done) => {
+    browser
+      .setupTest('/label.html')
+      .compareScreenshot({
+        screenshotName: 'labels-all',
+        selector: '#screenshot-label',
+        checkAccessibility: true
+      })
+      .call(done);
+  });
+});
