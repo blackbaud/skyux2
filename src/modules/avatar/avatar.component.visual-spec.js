9edebf13794a4305e6f11304051c95bf33c740f6
Paul Crowder <paul.crowder@blackbaud.com>
2016-08-29 17:36:23 -0400

    Visual tests

---
+++ src/modules/avatar/avatar.component.visual-spec.js
@@ -0,0 +1,25 @@
+describe('Avatar', () => {
+  'use strict';
+
+  it('should match previous screenshot when image is present', (done) => {
+    browser
+      .setupTest('/avatar.html')
+      .compareScreenshot({
+        screenshotName: 'avatar-image',
+        selector: '#screenshot-avatar-img',
+        checkAccessibility: true
+      })
+      .call(done);
+  });
+
+  it('should match previous screenshot when initials are present', (done) => {
+    browser
+      .setupTest('/avatar.html')
+      .compareScreenshot({
+        screenshotName: 'avatar-initials',
+        selector: '#screenshot-avatar-initials',
+        checkAccessibility: true
+      })
+      .call(done);
+  });
+});
---
+++ src/modules/avatar/fixtures/avatar.component.visual-fixture.ts
@@ -0,0 +1,34 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { SkyModule } from '../../../../src/core';
+
+import { Bootstrapper } from '../../../../visual/bootstrapper';
+
+@Component({
+  selector: 'sky-demo-app',
+  template: require('./avatar.component.visual-fixture.html')
+})
+class AppComponent {
+  public name = 'Robert C. Hernandez';
+
+  public get src(): string {
+    return require('./avatar.png');
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    SkyModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+class AppModule { }
+
+Bootstrapper.bootstrapModule(AppModule);
