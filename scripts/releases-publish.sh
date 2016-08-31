204c44f48d5c53ecf7a31d027758600ec3f3fe11
Bobby Earl <bobby.earl@blackbaud.com>
2016-08-31 11:23:45 -0400

    Adjusted to only run on release + push to master

--- scripts/releases-publish.sh
+++ scripts/releases-publish.sh
@@ -1,7 +1,7 @@
 # Read the version from package.json
-version=$(node -p -e "require('../package.json').version").$TRAVIS_BUILD_ID
+version=$(node -p -e "require('../package.json').version")
 src=../releases/.
-destination=releases/skyux2/$version/
+destination=skyux2/$version/
 checkout=skyux2-releases-repo
 
 # What user will be committing to the sky-docs repo
@@ -22,8 +22,7 @@ git add -f .
 if [ -z "$(git status --porcelain)" ]; then
   echo -e "No changes to commit to skyux2-releases."
 else
-  git commit -m "Release $version [skip ci]"
-  git tag $version
-  git push -fq origin master --tags > /dev/null
+  git commit -m "$version"
+  git push -fq origin master > /dev/null
   echo -e "skyux2-releases successfully updated.\n"
 fi
