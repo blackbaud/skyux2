1aa5a4e42edafa7d4960a38f47c4c6e909fb8179
Bobby Earl <bobby.earl@blackbaud.com>
2016-08-31 22:23:16 -0400

    Using core as filename

--- config/webpack/webpack.releases.js
+++ config/webpack/webpack.releases.js
@@ -2,11 +2,11 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
-entry: {
-  'skyux': './src/core.ts'
-},
-output: {
-  filename: '[name].js',
-  path: 'releases'
-}
+  entry: {
+    'core': './src/core.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: 'releases'
+  }
 });
