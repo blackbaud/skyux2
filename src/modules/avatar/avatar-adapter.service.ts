c374f0b1544a2f453523f2690598256353c726e9
Paul Crowder <paul.crowder@blackbaud.com>
2016-09-02 15:07:27 -0400

    Added avatar module

---
+++ src/modules/avatar/avatar-adapter.service.ts
@@ -0,0 +1,49 @@
+import { ElementRef, Injectable } from '@angular/core';
+
+import { SkyAvatarSrc } from './avatar-src';
+
+@Injectable()
+export class SkyAvatarAdapterService {
+  private blobUrl: string;
+
+  public updateImage(elementRef: ElementRef, src: SkyAvatarSrc) {
+    this.revokeBlobUrl();
+
+    let el = elementRef.nativeElement;
+
+    /*istanbul ignore else */
+    if (el) {
+      let imageEl = el.querySelector('.sky-avatar-image');
+
+      /*istanbul ignore else */
+      if (imageEl) {
+        let url: string;
+
+        if (src) {
+          if (src instanceof Blob) {
+            url = URL.createObjectURL(src);
+
+            // Keep the last blob URL around so we can revoke it later.
+            // https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL
+            this.blobUrl = url;
+          } else {
+            url = src;
+          }
+        }
+
+        imageEl.style.backgroundImage = url ? 'url(' + url + ')' : '';
+      }
+    }
+  }
+
+  public destroy() {
+    this.revokeBlobUrl();
+  }
+
+  private revokeBlobUrl() {
+    if (this.blobUrl) {
+      URL.revokeObjectURL(this.blobUrl);
+      this.blobUrl = undefined;
+    }
+  }
+}
