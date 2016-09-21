767b1b5c7e7a7adf7f7d780ed71b162f7cf0d55f
Paul Crowder <paul.crowder@blackbaud.com>
2016-09-21 10:54:27 -0400

    Node module updates (#123)
    * Update Dragula
    
    * Webpack config cleanup
    
    * More package updates

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,109 +1,38 @@
-/**
- * @author: @AngularClass
- */
-
 var webpack = require('webpack');
 var helpers = require('../utils/helpers');
-
-/*
- * Webpack Plugins
- */
-//var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var extractScss = new ExtractTextPlugin('sky.css');
 
-/*
- * Webpack Constants
- */
 var METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'SKY UX 2',
   baseUrl: '/'
 };
 
-/*
- * Webpack configuration
- *
- * See: http://webpack.github.io/docs/configuration.html#cli
- */
 module.exports = {
 
-  /*
-   * Static metadata for index.html
-   *
-   * See: (custom attribute)
-   */
   metadata: METADATA,
 
-  /*
-   * Cache generated modules and chunks to improve performance for multiple incremental builds.
-   * This is enabled by default in watch mode.
-   * You can pass false to disable it.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#cache
-   * cache: false,
-   *
-   * The entry point for the bundle
-   * Our Angular.js app
-   *
-   * See: http://webpack.github.io/docs/configuration.html#entry
-   */
   entry: {
     'polyfills': './src/polyfills.ts',
     'vendor': './src/vendor.ts'
   },
 
-  /*
-   * Options affecting the resolving of modules.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#resolve
-   */
   resolve: {
 
-    /*
-     * An array of extensions that should be used to resolve modules.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
-     */
     extensions: ['', '.ts', '.js'],
 
-    // Make sure root is src
     root: helpers.root('src'),
 
-    // remove other default values
     modulesDirectories: ['node_modules']
 
   },
 
-  /*
-   * Options affecting the normal modules.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#module
-   */
   module: {
 
-    /*
-     * An array of applied pre and post loaders.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#module-preloaders-module-postloaders
-     */
     preLoaders: [
 
-      /*
-       * Tslint loader support for *.ts files
-       *
-       * See: https://github.com/wbuchwalter/tslint-loader
-       */
-      // { test: /\.ts$/, loader: 'tslint-loader', exclude: [ helpers.root('node_modules') ] },
-
-      /*
-       * Source map loader support for *.js files
-       * Extracts SourceMaps for source files that as added as sourceMappingURL comment.
-       *
-       * See: https://github.com/webpack/source-map-loader
-       */
       {
         test: /\.js$/,
         loader: 'source-map-loader',
@@ -116,14 +45,6 @@ module.exports = {
 
     ],
 
-    /*
-     * An array of automatically applied loaders.
-     *
-     * IMPORTANT: The loaders here are resolved relative to the resource which they are applied to.
-     * This means they are not resolved relative to the configuration file.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#module-loaders
-     */
     loaders: [
 
       {
@@ -132,33 +53,16 @@ module.exports = {
         exclude: [/\.(spec|e2e)\.ts$/]
       },
 
-      /*
-       * Json loader support for *.json files.
-       *
-       * See: https://github.com/webpack/json-loader
-       */
       {
         test: /\.json$/,
         loader: 'json-loader'
       },
 
-      /*
-       * Raw loader support for *.css files
-       * Returns file content as string
-       *
-       * See: https://github.com/webpack/raw-loader
-       */
       {
         test: /\.css$/,
         loader: 'raw-loader'
       },
 
-      /*
-       * Raw loader support for *.css files
-       * Returns file content as string
-       *
-       * See: https://github.com/webpack/raw-loader
-       */
       {
         test: /\.scss$/,
         loader: 'raw-loader!sass-loader',
@@ -172,19 +76,12 @@ module.exports = {
         loader: extractScss.extract(['css', 'sass'])
       },
 
-      /* Raw loader support for *.html
-       * Returns file content as string
-       *
-       * See: https://github.com/webpack/raw-loader
-       */
       {
         test: /\.html$/,
         loader: 'raw-loader',
         exclude: [helpers.root('src/index.html')]
       },
 
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=10000&mimetype=application/font-woff'
@@ -203,66 +100,18 @@ module.exports = {
 
   },
 
-  /*
-   * Add additional plugins to the compiler.
-   *
-   * See: http://webpack.github.io/docs/configuration.html#plugins
-   */
   plugins: [
 
     extractScss,
 
-    /*
-     * Plugin: ForkCheckerPlugin
-     * Description: Do type checking in a separate process, so webpack don't need to wait.
-     *
-     * See: https://github.com/s-panferov/awesome-typescript-loader#forkchecker-boolean-defaultfalse
-     */
-    new ForkCheckerPlugin(),
-
-    /*
-     * Plugin: OccurenceOrderPlugin
-     * Description: Varies the distribution of the ids to get the smallest id length
-     * for often used ids.
-     *
-     * See: https://webpack.github.io/docs/list-of-plugins.html#occurrenceorderplugin
-     * See: https://github.com/webpack/docs/wiki/optimization#minimize
-     */
     new webpack.optimize.OccurenceOrderPlugin(true),
 
-    /*
-     * Plugin: CommonsChunkPlugin
-     * Description: Shares common code between the pages.
-     * It identifies common modules and put them into a commons chunk.
-     *
-     * See: https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
-     * See: https://github.com/webpack/docs/wiki/optimization#multi-page-app
-     */
     new webpack.optimize.CommonsChunkPlugin({
       name: helpers.reverse(['polyfills', 'vendor'])
     }),
 
-    /*
-     * Plugin: CopyWebpackPlugin
-     * Description: Copy files and directories in webpack.
-     *
-     * Copies project static assets.
-     *
-     * See: https://www.npmjs.com/package/copy-webpack-plugin
-     */
-    // new CopyWebpackPlugin([{
-    //   from: 'src/assets',
-    //   to: 'assets'
-    // }])
-
   ],
 
-  /*
-   * Include polyfills or mocks for various node stuff
-   * Description: Node configuration
-   *
-   * See: https://webpack.github.io/docs/configuration.html#node
-   */
   node: {
     global: 'window',
     crypto: 'empty',
