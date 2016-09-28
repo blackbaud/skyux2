be7b89cf5f48135ed54a92e1f0eb1deb100d6458
Paul Crowder <paul.crowder@blackbaud.com>
2016-09-28 11:52:44 -0400

    More visual tests.

--- src/modules/navbar/navbar.component.visual-spec.js
+++ src/modules/navbar/navbar.component.visual-spec.js
@@ -11,6 +11,17 @@ describe('Navbar', () => {
       .call(done);
   });
 
+  it('should match previous navbar screenshot when the user mouses over an item', (done) => {
+    browser
+      .setupTest('/navbar.html')
+      .moveToObject('#screenshot-navbar .first-item')
+      .compareScreenshot({
+        screenshotName: 'navbar-item-over',
+        selector: '#screenshot-navbar'
+      })
+      .call(done);
+  });
+
   it('should match previous navbar screenshot when a dropdown is open', (done) => {
     browser
       .setupTest('/navbar.html')
@@ -21,5 +32,56 @@ describe('Navbar', () => {
       })
       .call(done);
   });
+
+  it('should match previous navbar screenshot when the user is over a dropdown item', (done) => {
+    browser
+      .setupTest('/navbar.html')
+      .moveToObject('#screenshot-navbar .sky-dropdown-button')
+      .moveToObject('#screenshot-navbar .sky-dropdown-item:first-child')
+      .compareScreenshot({
+        screenshotName: 'navbar-dropdown-item-over',
+        selector: '#screenshot-navbar'
+      })
+      .call(done);
+  });
+
+  it(
+    'should match previous navbar screenshot when an item is active via the item CSS class',
+    (done) => {
+      browser
+        .setupTest('/navbar.html')
+        .compareScreenshot({
+          screenshotName: 'navbar-item-active',
+          selector: '#screenshot-navbar-active-item'
+        })
+        .call(done);
+    }
+  );
+
+  it(
+    'should match previous navbar screenshot when an item is active via a child CSS class',
+    (done) => {
+      browser
+        .setupTest('/navbar.html')
+        .compareScreenshot({
+          screenshotName: 'navbar-item-active-child',
+          selector: '#screenshot-navbar-active-child'
+        })
+        .call(done);
+    }
+  );
+
+  it(
+    'should match previous navbar screenshot when a dropdown is active',
+    (done) => {
+      browser
+        .setupTest('/navbar.html')
+        .compareScreenshot({
+          screenshotName: 'navbar-item-active-child',
+          selector: '#screenshot-navbar-active-child'
+        })
+        .call(done);
+    }
+  );
 
 });
