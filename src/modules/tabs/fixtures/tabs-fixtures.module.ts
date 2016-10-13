6dd5ffd7fc2f41c64eba4a5d88bc9457bf5c2eb5
Paul Crowder <paul.crowder@blackbaud.com>
2016-10-13 11:30:50 -0400

    Fixed issue where two tabsets on one page interfered with one another

--- src/modules/tabs/fixtures/tabs-fixtures.module.ts
+++ src/modules/tabs/fixtures/tabs-fixtures.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SkyTabsModule } from '../';
-import { SkyTabsetAdapterService } from '../tabset-adapter.service';
 import { TabsetTestComponent } from './tabset.component.fixture';
-import { MockTabsetAdapterService } from './tabset-adapter.service.mock';
 
 @NgModule({
   declarations: [
     TabsetTestComponent
-  ],
-  providers: [
-    {
-      provide: SkyTabsetAdapterService,
-      useClass: MockTabsetAdapterService
-    }
   ],
   imports: [
     CommonModule,
