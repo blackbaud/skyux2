b4779ad49aa318055fc8c47305093f7fefcf6c54
Paul Crowder <paul.crowder@blackbaud.com>
2016-10-24 13:54:07 -0400

    More docs

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SkyTilesDemoTile1Component } from './components/tiles/tiles-demo-tile1.component';
 import { SkyTilesDemoTile2Component } from './components/tiles/tiles-demo-tile2.component';
+import { SkyDemoComponentsService } from './components/demo-components.service';
 
 require('style-loader!prismjs/themes/prism.css');
 require('style-loader!./styles.scss');
@@ -10,6 +11,9 @@ require('style-loader!./styles.scss');
   entryComponents: [
     SkyTilesDemoTile1Component,
     SkyTilesDemoTile2Component
+  ],
+  providers: [
+    SkyDemoComponentsService
   ]
 })
 export class AppExtrasModule { }
---
+++ src/app/components/demo-components.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import { SkyDemoComponent } from './demo-component';
+
+@Injectable()
+export class SkyDemoComponentsService {
+  public getComponents(filter?: string): SkyDemoComponent[] {
+    return [
+      {
+        name: 'Alert',
+        icon: 'bell',
+        summary: 'The alert component displays a SKY UX-themed alert.',
+        url: '/components/alert'
+      },
+      {
+        name: 'Avatar',
+        icon: 'user',
+        summary: `The avatar component displays an image with an option to let users change the image.`,
+        url: '/components/avatar'
+      },
+      {
+        name: 'Tiles',
+        icon: 'th-large',
+        summary: `Tiles provides a collapsible container that is the building block for pages and forms in SKY UX applications.`,
+        url: '/components/tiles'
+      }
+    ];
+  }
+}
