bdec225bce43e0549499976cbe2e3a513476d691
Paul Crowder <paul.crowder@blackbaud.com>
2016-10-26 08:17:33 -0400

    Content max width (#189)
    * Added sky-container class
    
    * Content widths

--- src/app/shared/nav.component.ts
+++ src/app/shared/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sky-demo-nav',
-  template: require('./nav.component.html')
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
 })
 export class SkyDemoNavComponent { }
