c3685fa3e8af45802a737bb17fbf34be737e2acb
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-10-26 15:38:13 -0400

    fix tslint error

--- src/modules/tiles/fixtures/tile1.component.ts
+++ src/modules/tiles/fixtures/tile1.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'div.tile-1',
+  selector: 'sky-tile-1',
   template: `
     <sky-tile (settingsClick)="tileSettingsClick()">
       <sky-tile-title>
--- src/modules/tiles/fixtures/tile2.component.ts
+++ src/modules/tiles/fixtures/tile2.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'div.tile-2',
+  selector: 'sky-tile-2',
   template: `
     <sky-tile>
       <sky-tile-title>
