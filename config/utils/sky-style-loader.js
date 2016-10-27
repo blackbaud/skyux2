30a86b08557e49e201f8986fb5801048db7cf128
Paul Crowder <paul.crowder@blackbaud.com>
2016-10-27 14:20:36 -0400

    Don't wait for FontAwesome to load

--- config/utils/sky-style-loader.js
+++ config/utils/sky-style-loader.js
@@ -6,17 +6,12 @@ var stylesAreLoaded = false;
 
 module.exports = {
   loadStyles: function (callback) {
-    var fontAwesome = new FontFaceObserver('FontAwesome'),
-      openSans = new FontFaceObserver('Open Sans'),
+    var openSans = new FontFaceObserver('Open Sans'),
       oswald = new FontFaceObserver('Oswald'),
       promise;
 
     promise = Promise.all(
       [
-        // Specify a character for FontAwesome since some browsers will fail to detect
-        // when the font is loaded unless a known character with a different width
-        // than the default is not specified.
-        fontAwesome.load('\uf0fc'),
         openSans.load(),
         oswald.load()
       ]
