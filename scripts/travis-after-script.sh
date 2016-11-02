ecc609962c59bc25182d0b8f07e6fc725c6b2eb3
Bobby Earl <bobby.earl@blackbaud.com>
2016-11-02 15:21:56 -0400

    Using secure var instead of slug to check for fork

--- scripts/travis-after-script.sh
+++ scripts/travis-after-script.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
 # Upload coverage to https://codecov.io/gh/blackbaud/skyux2
-if [ "$IS_FORK_PR" == "false" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   bash <(curl -s https://codecov.io/bash)
   ./scripts/browserstack-cleanup.sh
 fi
--- scripts/visual-baseline.sh
+++ scripts/visual-baseline.sh
@@ -1,9 +1,9 @@
 # Fail the build if this step fails
 set -e
 
-# Update the dist folder of the current branch, as long as it's a push and not a savage- branch.
+# Update the webdriver-screenshots folder of the current branch, as long as it's a push and not a savage- branch.
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && ! $TRAVIS_BRANCH =~ $SAVAGE_BRANCH ]]; then
-  echo -e "Starting to update skyux.\n"
+  echo -e "Starting to update skyux2.\n"
 
   git config --global user.email "sky-build-user@blackbaud.com"
   git config --global user.name "Blackbaud Sky Build User"
@@ -15,11 +15,11 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && ! $TRAVIS_BRANCH =~ $SAVAGE_BRANCH ]]
   git add webdriver-screenshots/
 
   if [ -z "$(git status --porcelain)" ]; then
-    echo -e "No changes to commit to skyux."
+    echo -e "No changes to commit to skyux2."
   else
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to skyux [ci skip]"
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to skyux2 [ci skip]"
     git push -fq origin $TRAVIS_BRANCH > /dev/null
-    echo -e "skyux successfully updated.\n"
+    echo -e "skyux2 successfully updated.\n"
   fi
 
 fi
