c560fb0f9f13ed10eed992ae50b9e204a6d4670c
Bobby Earl <bobby.earl@blackbaud.com>
2016-11-02 13:20:31 -0400

    Wrapping in double brackets

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
 # Publish a tag to NPM & skyux2-releases repo
-if [ "$IS_FORK_PR" == "false" && -n "$TRAVIS_TAG" ]; then
+if [[ "$IS_FORK_PR" == "false" && -n "$TRAVIS_TAG" ]]; then
   npm run releases
   ./scripts/npm-publish.sh
   ./scripts/releases-publish.sh
