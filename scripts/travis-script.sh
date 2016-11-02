ef0efdad2b11fdebfe7da438f03706ab07727ee4
Bobby Earl <bobby.earl@blackbaud.com>
2016-11-02 16:18:00 -0400

    Using secure var instead of slug to check for fork (#211)

--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
 # Override default of `npm test`
-if [ "$IS_FORK_PR" == "false" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   npm run ci && ./scripts/visual-baseline.sh
 else
   echo -e "Pull requests from forks are run via Savage."
