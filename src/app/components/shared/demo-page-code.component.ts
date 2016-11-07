3cc0f546d7ec65fc5d48f1cd06c2a308ab2833f8
Paul Crowder <paul.crowder@blackbaud.com>
2016-11-07 14:07:20 -0500

    Added Edit in Plunker button to component docs
    Closes #220

--- src/app/components/shared/demo-page-code.component.ts
+++ src/app/components/shared/demo-page-code.component.ts
@@ -9,21 +9,44 @@ import 'prismjs/prism';
 import 'prismjs/components/prism-typescript';
 
 import { SkyDemoPageCodeFile } from './demo-page-code-file';
+import { SkyDemoPagePlunkerService } from './demo-page-plunker-service';
 
 @Component({
   selector: 'sky-demo-page-code',
   templateUrl: './demo-page-code.component.html',
   styleUrls: ['./demo-page-code.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SkyDemoPagePlunkerService]
 })
 export class SkyDemoPageCodeComponent {
   @Input()
   public codeFilesForBinding: SkyDemoPageCodeFile[];
 
   @Input()
-  public set codeFiles(value: {folder: string, name: string}[]) {
+  public get plunkerFiles(): any[] {
+    return [
+      ...this.plunkerService.getFiles(this.codeFilesForBinding)
+    ];
+  }
+
+  @Input()
+  public set codeFiles(
+    value: {
+      folder: string,
+      name: string,
+      componentName: string,
+      bootstrapSelector: string
+    }[]
+  ) {
     this.codeFilesForBinding = value.map((item) => {
-      return new SkyDemoPageCodeFile(item.folder, item.name);
+      return new SkyDemoPageCodeFile(
+        item.folder,
+        item.name,
+        item.componentName,
+        item.bootstrapSelector
+      );
     });
   };
+
+  constructor(private plunkerService: SkyDemoPagePlunkerService) { }
 }
