03c462c4c6bd1f7958eb566af1faefdd45148fe6
Paul Crowder <paul.crowder@blackbaud.com>
2016-11-17 13:26:38 -0500

    Label and modal docs (#231)

---
+++ src/app/components/label/label-demo.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-label-demo',
+  templateUrl: './label-demo.component.html'
+})
+export class SkyLabelDemoComponent { }
---
+++ src/app/components/modal/modal-demo-context.ts
@@ -0,0 +1,3 @@
+export class SkyModalDemoContext {
+  public valueA: string;
+}
