84c46cd0d93589ea10a642656c9d9d8c0f74d86f
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-12-09 18:08:53 -0500

    attempt to run tunnel through ci like how SKY UX 1 does

--- config/wdio/ci.wdio.conf.js
+++ config/wdio/ci.wdio.conf.js
@@ -20,10 +20,10 @@
       browserDisconnectTolerance: 3,
       browserNoActivityTimeout: 3e5,
       captureTimeout: 3e5,
-      build: 'mac-chrome-webdriver-' + process.env.TRAVIS_BUILD_NUMBER,
-      //name: 'SKYUX2BROWSERSTACK',
-      //'browserstack.localIdentifier': 'SKYUX2BROWSERSTACK',
-      resolution: '1280x960'
+      build: 'skyux2-mac-chrome-webdriver-' + process.env.TRAVIS_BUILD_NUMBER,
+      resolution: '1280x960',
+      name: 'SKYUX2BROWSERSTACKCI',
+      'browserstack.localIdentifier': 'SKYUX2BROWSERSTACKCI'
     },
     {
       browserName: 'firefox',
@@ -36,16 +36,16 @@
       browserDisconnectTolerance: 3,
       browserNoActivityTimeout: 3e5,
       captureTimeout: 3e5,
-      build: 'mac-firefox-webdriver-' + process.env.TRAVIS_BUILD_NUMBER,
-      //name: 'SKYUX2BROWSERSTACK',
-      //'browserstack.localIdentifier': 'SKYUX2BROWSERSTACK',
-      resolution: '1280x960'
+      build: 'skyux2-mac-firefox-webdriver-' + process.env.TRAVIS_BUILD_NUMBER,
+      resolution: '1280x960',
+      name: 'SKYUX2BROWSERSTACKCI',
+      'browserstack.localIdentifier': 'SKYUX2BROWSERSTACKCI'
     }
   ];
   config.host = 'hub.browserstack.com';
   config.port = 80;
 
-  config.maxInstances = 6;
+  config.maxInstances = 10;
 
   config.visualRegression = require('../utils/visual-browser-commands')
     .getVisualRegression(
@@ -53,8 +53,8 @@
       'webdriver-screenshots-screen',
       'webdriver-screenshots-diffs');
 
-  config.onPrepare = server.startCI;
-  config.onComplete = server.stopCI;
+  //config.onPrepare = server.startCI;
+  //config.onComplete = server.stopCI;
 
   exports.config = config;
 
