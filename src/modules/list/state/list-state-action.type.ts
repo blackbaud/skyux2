9080e428472e8577dd83030c8f611c22a0cd3951
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-12-20 13:14:49 -0500

    Delete more unnecessary files

--- src/modules/list/state/list-state-action.type.ts
+++ src/modules/list/state/list-state-action.type.ts
@@ -1,23 +1,7 @@
 import {
   ListItemsSetLoadingAction, ListItemsLoadAction
 } from './items/actions';
-import { ListViewsLoadAction, ListViewsSetActiveAction } from './views/actions';
-import {
-  ListSearchSetSearchTextAction,
-  ListSearchSetFunctionsAction,
-  ListSearchSetFieldSelectorsAction
-} from './search/actions';
-import {
-  ListSortSetFieldSelectorsAction, ListSortSetAvailableAction, ListSortSetGlobalAction
-} from './sort/actions';
-import { ListFiltersLoadAction, ListFiltersUpdateAction } from './filters/actions';
-import { ListToolbarItemsLoadAction, ListToolbarSetExistsAction } from './toolbar/actions';
-import {
-  ListSelectedSetLoadingAction,
-  ListSelectedLoadAction,
-  ListSelectedSetItemSelectedAction,
-  ListSelectedSetItemsSelectedAction
-} from './selected/actions';
+
 import {
   ListPagingSetMaxPagesAction,
   ListPagingSetItemsPerPageAction,
@@ -27,15 +11,7 @@ import {
 } from './paging/actions';
 
 export type ListStateAction =
-  ListSelectedSetLoadingAction | ListSelectedLoadAction | ListSelectedSetItemSelectedAction |
-  ListSelectedSetItemsSelectedAction |
   ListItemsSetLoadingAction | ListItemsLoadAction |
-  ListViewsLoadAction | ListViewsSetActiveAction |
-  ListSearchSetSearchTextAction | ListSearchSetFunctionsAction |
-  ListSearchSetFieldSelectorsAction |
-  ListSortSetFieldSelectorsAction | ListSortSetAvailableAction | ListSortSetGlobalAction |
-  ListFiltersLoadAction | ListFiltersUpdateAction |
-  ListToolbarItemsLoadAction | ListToolbarSetExistsAction |
   ListPagingSetMaxPagesAction | ListPagingSetItemsPerPageAction |
   ListPagingSetPageCountAction | ListPagingSetPageNumberAction |
   ListPagingSetDisplayedPagesAction;
--- src/modules/list/state/list-state.state-node.ts
+++ src/modules/list/state/list-state.state-node.ts
@@ -4,12 +4,7 @@ import { StateNode } from 'microedge-rxstate/dist';
 import { ListStateModel } from './list-state.model';
 import { ListStateDispatcher } from './list-state.rxstate';
 import { ListItemsOrchestrator } from './items/items.orchestrator';
-import { ListViewsOrchestrator } from './views/views.orchestrator';
-import { ListSearchOrchestrator } from './search/search.orchestrator';
-import { ListSortOrchestrator } from './sort/sort.orchestrator';
-import { ListFiltersOrchestrator } from './filters/filters.orchestrator';
-import { ListToolbarOrchestrator } from './toolbar/toolbar.orchestrator';
-import { ListSelectedOrchestrator } from './selected/selected.orchestrator';
+
 import { ListPagingOrchestrator } from './paging/paging.orchestrator';
 
 @Injectable()
@@ -18,13 +13,7 @@ export class ListState extends StateNode<ListStateModel> {
     super(new ListStateModel(), dispatcher);
 
     this
-      .register('views', ListViewsOrchestrator)
       .register('items', ListItemsOrchestrator)
-      .register('search', ListSearchOrchestrator)
-      .register('sort', ListSortOrchestrator)
-      .register('filters', ListFiltersOrchestrator)
-      .register('toolbar', ListToolbarOrchestrator)
-      .register('selected', ListSelectedOrchestrator)
       .register('paging', ListPagingOrchestrator)
       .begin();
   }
