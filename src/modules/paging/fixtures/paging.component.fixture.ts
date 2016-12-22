c52869efa9896d67aac75663635b05589e7b1aaa
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-12-22 12:45:55 -0500

    update test fixtures for paging

---
+++ src/modules/paging/fixtures/paging.component.fixture.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from '@angular/core';
+import { SkyPagingComponent } from '../paging.component';
+
+@Component({
+  selector: 'sky-test-cmp',
+  template: require('./paging.component.fixture.html')
+})
+export class ListPagingTestComponent {
+  @ViewChild(SkyPagingComponent) public pagingComponent: SkyPagingComponent;
+
+  public pageSize: number = 2;
+  public maxPages: number = 3;
+  public currentPage: number = 1;
+  public itemCount: number = 8;
+  public currentPageChanged(currentPage: number) {
+    this.currentPage = currentPage;
+  }
+}
