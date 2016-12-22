89dbcbf05b5ec0189f443b4affce3d27c24a0915
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-12-22 15:13:48 -0500

    Adding visual tests for sky-paging component
    Removing visual tests from list-paging since it adds nothing visual

---
+++ src/modules/paging/paging.component.visual-spec.js
@@ -0,0 +1,45 @@
+describe('paging component', function () {
+  'use strict';
+
+  it('should display first page selected', function () {
+    return browser
+      .setupTest('/paging.html')
+      .compareScreenshot({
+        screenshotName: 'paging-first',
+        selector: '#screenshot-paging'
+      });
+  });
+
+  it('should display middle page selected', function () {
+    return browser
+      .setupTest('/paging.html')
+      .click('a[cmp-id="next"]')
+      .compareScreenshot({
+        screenshotName: 'paging-middle',
+        selector: '#screenshot-paging'
+      });
+  });
+
+  it('should display middle next page selected', function () {
+    return browser
+      .setupTest('/paging.html')
+      .click('a[cmp-id="next"]')
+      .click('a[cmp-id="next"]')
+      .compareScreenshot({
+        screenshotName: 'paging-middle-next',
+        selector: '#screenshot-paging'
+      });
+  });
+
+  it('should display last page selected', function () {
+    return browser
+      .setupTest('/paging.html')
+      .click('a[cmp-id="next"]')
+      .click('a[cmp-id="next"]')
+      .click('a[cmp-id="next"]')
+      .compareScreenshot({
+        screenshotName: 'paging-last',
+        selector: '#screenshot-paging'
+      });
+  });
+});
