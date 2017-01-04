29431ea17a2b7d242ffcc34b5cc425fa73a8747f
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-01-04 16:15:37 -0500

    stubs for search component

---
+++ src/modules/search/search.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SkySearchComponent {
+
+}
