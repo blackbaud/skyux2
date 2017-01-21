9ede6c8e4262958cf876aa8af2b078205c0ea2e4
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-01-21 11:47:26 -0500

    Add code coverage for list helpers getData

--- src/modules/list/helpers.ts
+++ src/modules/list/helpers.ts
@@ -1,30 +1,30 @@
 export function getData(item: any, selector: string): any {
-   let resultFieldParts = selector.split('.');
-   if (resultFieldParts.length > 0 && resultFieldParts[0] === '') {
-     resultFieldParts.shift();
-   }
+  let resultFieldParts = selector.split('.');
+  if (resultFieldParts.length > 0 && resultFieldParts[0] === '') {
+    resultFieldParts.shift();
+  }
 
-   let result = item;
-   if (resultFieldParts.length > 0) {
-     for (let index = 0; index < resultFieldParts.length; index++) {
-       let part = resultFieldParts[index];
-       if (result[part] === undefined) {
-         break;
+  let result = item;
+  if (resultFieldParts.length > 0) {
+    for (let index = 0; index < resultFieldParts.length; index++) {
+    let part = resultFieldParts[index];
+      if (result[part] === undefined) {
+        break;
       }
       /* tslint:disable */
-       else if (result[part] === null) {
-         result = null;
-         break;
-       }
-       /* tslint:enable */
+      else if (result[part] === null) {
+        result = null;
+        break;
+      }
+      /* tslint:enable */
 
-       result = result[part];
-     }
-   }
+      result = result[part];
+    }
+  }
 
-   if (result === item) {
-     return undefined;
-   };
+  if (result === item) {
+    return undefined;
+  };
 
-   return result;
- }
+  return result;
+}
