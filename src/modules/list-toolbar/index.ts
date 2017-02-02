0421e54110daad60e3a7f849f58b5fc9a552dc46
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-02 09:40:19 -0500

    Remove list-toolbar stuff and add unit tests for standalone toolbar

--- src/modules/list-toolbar/index.ts
+++
@@ -1,4 +0,0 @@
-export { SkyListToolbarComponent } from './list-toolbar.component';
-export { SkyListToolbarItemComponent } from './list-toolbar-item.component';
-export { SkyListToolbarItemRendererComponent } from './list-toolbar-item-renderer.component';
-export { SkyListToolbarModule } from './list-toolbar.module';
