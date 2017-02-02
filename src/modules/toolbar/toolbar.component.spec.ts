830c51c1d10e3d8b2c2c6ea2f3ee5fe97bbb64cf
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-02 11:14:32 -0500

    Toolbar standalone (#326)
    * starting files for list-toolbar
    
    * Add standalone toolbar component
    
    Also:
      - tweak input-group and search styles to not use table display since
    we can use flex instead.
      - Used toolbar component in search demo.
    
    TODO:
      - toolbar unit tests
      - toolbar visual tests
      - toolbar docs
      - use toolbar in list-toolbar component
    
    * Remove list-toolbar stuff and add unit tests for standalone toolbar
    
    * Update search screenshots for new input group styles and toolbar component

---
+++ src/modules/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,38 @@
+import {
+  TestBed
+} from '@angular/core/testing';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ToolbarTestComponent } from './fixtures/toolbar.component.fixture';
+
+import { SkyToolbarModule } from '.';
+
+import { expect } from '../testing';
+
+describe('toolbar component', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ToolbarTestComponent
+      ],
+      imports: [
+        BrowserModule,
+        SkyToolbarModule
+      ]
+    });
+  });
+
+  it('should create a toolbar with transcluded items', () => {
+    let fixture = TestBed.createComponent(ToolbarTestComponent);
+    let el = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+
+    let buttonEls = el.querySelectorAll('.sky-toolbar-container .sky-toolbar-item .sky-btn');
+
+    expect(buttonEls.item(0)).toHaveText('Button 1');
+    expect(buttonEls.item(1)).toHaveText('Button 2');
+
+  });
+});
