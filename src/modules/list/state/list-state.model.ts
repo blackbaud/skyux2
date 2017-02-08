e4cf68bb325469e3273b049bc21a83193e3aedc5
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-08 15:01:50 -0500

    List selected state (#341)
    * start of adding selected state to list component
    
    * start of tests for selected list state
    
    * Finish tests for list state selected
    
    * Fix linting errors
    
    * fix moment references for bundling

--- src/modules/list/state/list-state.model.ts
+++ src/modules/list/state/list-state.model.ts
@@ -1,15 +1,19 @@
-import { AsyncList } from 'microedge-rxstate/dist';
-import { ListPagingModel } from './paging/paging.model';
+import { AsyncList, AsyncItem } from 'microedge-rxstate/dist';
+
 import { ListItemModel } from './items/item.model';
-import { ListViewsModel } from './views/views.model';
-import { ListToolbarModel } from './toolbar/toolbar.model';
+import { ListPagingModel } from './paging/paging.model';
 import { ListSearchModel } from './search/search.model';
+import { ListSelectedModel } from './selected/selected.model';
+import { ListToolbarModel } from './toolbar/toolbar.model';
+import { ListViewsModel } from './views/views.model';
 
 export class ListStateModel {
-  public paging: ListPagingModel = new ListPagingModel();
+
   public items: AsyncList<ListItemModel> = new AsyncList<ListItemModel>();
-  public views: ListViewsModel = new ListViewsModel();
-  public toolbar: ListToolbarModel = new ListToolbarModel();
+  public paging: ListPagingModel = new ListPagingModel();
   public search: ListSearchModel = new ListSearchModel();
+  public selected: AsyncItem<ListSelectedModel> = new AsyncItem<ListSelectedModel>();
+  public toolbar: ListToolbarModel = new ListToolbarModel();
+  public views: ListViewsModel = new ListViewsModel();
 
 }
