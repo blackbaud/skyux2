05f49d1b0424984137b9a48766b9d8eb9a06505a
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-22 17:17:36 -0500

    More work around column selector service

--- src/modules/grid/column-selector-modal.component.ts
+++ src/modules/grid/column-selector-modal.component.ts
@@ -1,36 +1,36 @@
 import { Component, Output, ViewChild, EventEmitter } from '@angular/core';
 import { SkyModalComponent } from '../modal';
 import { SkyListComponent } from '../list';
+import { SkyGridColumnModel } from './grid-column.model';
 
 @Component({
   selector: 'sky-column-selector',
   templateUrl: './column-selector-modal.component.html'
 })
 export class SkyColumnSelectorComponent {
-  @Output() public columnsChanged: EventEmitter<string[]> = new EventEmitter<string[]>();
-  @ViewChild(SkyModalComponent) private modal: SkyModalComponent;
-  @ViewChild('list') private list: SkyListComponent;
+  public newSelectedColumnIds: Array<string>
 
   constructor(
-    private gridState: GridState,
-    private gridDispatcher: GridStateDispatcher
+    public columns: Array<SkyGridColumnModel>,
+    private selectedColumnIds: Array<string>
   ) {
+    this.newSelectedColumnIds = selectedColumnIds;
   }
 
-  get columns() {
-    return this.gridState.map(s => s.columns.items);
+  public selectedColumnsChange(selectedMap: Map<string, boolean>) {
+    this.newSelectedColumnIds = [];
+    selectedMap.forEach((value, key) => {
+      if (value) {
+        this.newSelectedColumnIds.push(key);
+      }
+    });
   }
 
-  get displayedColumnIds() {
-    return this.gridState.map(s => s.displayedColumns.items.map(c => c.id));
+  public cancelChanges() {
+    //stub for cancelling changes to column chooser
   }
 
   public applyChanges() {
-    this.list.selectedItems
-      .take(1)
-      .subscribe(columns => {
-        this.columnsChanged.emit(columns.map(c => c.id));
-        this.modal.closeButtonClick();
-      });
+    //stub for submitting newSelectedColumnIds
   }
 }
