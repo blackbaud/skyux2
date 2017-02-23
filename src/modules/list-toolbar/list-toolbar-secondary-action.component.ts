9413b2983e67da14a0fc49d7b9a7a2a586abdc52
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-23 10:16:01 -0500

    Workaround strange dropdown item problem

--- src/modules/list-toolbar/list-toolbar-secondary-action.component.ts
+++ src/modules/list-toolbar/list-toolbar-secondary-action.component.ts
@@ -4,7 +4,8 @@ import {
 
 @Component({
   selector: 'sky-list-toolbar-secondary-action',
-  templateUrl: './list-toolbar-secondary-action.component.html'
+  templateUrl: './list-toolbar-secondary-action.component.html',
+  styleUrls: ['./list-toolbar-secondary-action.component.scss']
 })
 export class SkyListToolbarSecondaryActionComponent {
 }
