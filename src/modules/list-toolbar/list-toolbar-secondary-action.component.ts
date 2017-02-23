c34f43564dba6776014efb07878ce2e814c72cbe
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-23 12:26:06 -0500

    List secondary actions (#374)
    * start of secondary actions
    
    * continue start of secondary actions
    
    * Updating dropdown menu z-index to account for relative positioned items
    
    * Secondary actions dropdown
    
    TODO:
      - unit tests
      - documentation
      - visual tests
    
    * Add unit tests for secondary actions
    
    * Update documentation for secondary actions in list-toolbar
    
    * Add visual test for list-toolbar with secondary action
    
    * Workaround strange dropdown item problem

---
+++ src/modules/list-toolbar/list-toolbar-secondary-action.component.ts
@@ -0,0 +1,11 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-list-toolbar-secondary-action',
+  templateUrl: './list-toolbar-secondary-action.component.html',
+  styleUrls: ['./list-toolbar-secondary-action.component.scss']
+})
+export class SkyListToolbarSecondaryActionComponent {
+}
