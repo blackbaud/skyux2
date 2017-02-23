b3f2132188956c8679e21ca4c54d39cb0cce587e
Adam Hickey <Adam.Hickey@blackbaud.me>
2017-02-23 11:03:37 -0500

    Adding title, description, and action text.

--- src/modules/error/error.component.ts
+++ src/modules/error/error.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './error.component.html'
 })
 export class SkyErrorComponent {
+  public title: string = 'Sorry, something went wrong.';
+  public description: string = 'Try to refresh this page or come back later.';
+  public actionText: string = 'Refresh';
 }
