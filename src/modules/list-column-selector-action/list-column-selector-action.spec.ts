8af1f875cb4653b6a946da5063cf0e4ee09c741c
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-24 13:54:23 -0500

    start work on list column selector action tests

---
+++ src/modules/list-column-selector-action/list-column-selector-action.spec.ts
@@ -0,0 +1,13 @@
+describe('List column selector action', () => {
+  it('should show an action in the secondary actions dropdown', () => {
+
+  });
+
+  it('should open the appropriate modal on click and apply column changes on save', () => {
+
+  });
+
+  it('should keep previous columns on cancel', () => {
+
+  });
+});
