e9f5677d0b64e6ab0e00caf9fea21383717ae731
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-24 13:54:42 -0500

    More unit test work

--- src/modules/list-column-selector-action/list-column-selector-action.spec.ts
+++ src/modules/list-column-selector-action/list-column-selector-action.spec.ts
@@ -1,4 +1,105 @@
+import {
+  TestBed,
+  ComponentFixture,
+  async,
+  fakeAsync,
+  tick
+} from '@angular/core/testing';
+
+import {
+  ListState,
+  ListStateDispatcher
+} from '../list/state';
+
+import {
+  SkyListModule,
+  SkyListComponent
+} from '../list';
+
+import {
+  SkyListToolbarModule
+} from '../list-toolbar';
+
+import {
+  SkyListSecondaryActionsModule
+} from '../list-secondary-actions';
+
+import {
+  SkyListViewGridModule
+} from '../list-view-grid';
+
+import {
+  ListColumnSelectorActionTestComponent
+} from './fixtures/list-column-selector-action.component.fixture';
+
+import {
+  SkyListColumnSelectorActionModule
+} from '.';
+
 describe('List column selector action', () => {
+    let state: ListState,
+      dispatcher: ListStateDispatcher,
+      component: ListColumnSelectorActionTestComponent,
+      fixture: ComponentFixture<ListColumnSelectorActionTestComponent>,
+      nativeElement: HTMLElement;
+
+    beforeEach(async(() => {
+      dispatcher = new ListStateDispatcher();
+      state = new ListState(dispatcher);
+
+      TestBed.configureTestingModule({
+        declarations: [
+          ListColumnSelectorActionTestComponent
+        ],
+        imports: [
+          SkyListColumnSelectorActionModule,
+          SkyListModule,
+          SkyListToolbarModule,
+          SkyListSecondaryActionsModule
+
+        ]
+      })
+      .overrideComponent(SkyListComponent, {
+          set: {
+            providers: [
+              { provide: ListState, useValue: state },
+              { provide: ListStateDispatcher, useValue: dispatcher }
+            ]
+          }
+        });
+
+      fixture = TestBed.createComponent(ListColumnSelectorActionTestComponent);
+      nativeElement = fixture.nativeElement as HTMLElement;
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      // always skip the first update to ListState, when state is ready
+      // run detectChanges once more then begin tests
+      state.skip(1).take(1).subscribe(() => fixture.detectChanges());
+      fixture.detectChanges();
+    }));
+
+    it('should handle grid columns changing', () => {
+      expect(element.queryAll(By.css('th.sky-grid-heading')).length).toBe(2);
+      expect(element.query(
+        By.css('th[sky-cmp-id="name"]')).nativeElement.textContent.trim()
+      ).toBe('Name Initial');
+      expect(element.query(
+        By.css('th[sky-cmp-id="email"]')
+      ).nativeElement.textContent.trim()).toBe('Email Initial');
+
+      component.changeColumns();
+      fixture.detectChanges();
+      expect(element.queryAll(By.css('th.sky-grid-heading')).length).toBe(2);
+      expect(element.query(
+        By.css('th[sky-cmp-id="name"]')).nativeElement.textContent.trim()
+      ).toBe('Name');
+      expect(element.query(
+        By.css('th[sky-cmp-id="email"]')
+      ).nativeElement.textContent.trim()).toBe('Email');
+
+    });
+  });
   it('should show an action in the secondary actions dropdown', () => {
 
   });
@@ -8,6 +109,10 @@ describe('List column selector action', () => {
   });
 
   it('should keep previous columns on cancel', () => {
+
+  });
+
+  it('should not appear if not in grid view', () => {
 
   });
 });
