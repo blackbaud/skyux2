6fbe7e2cf7cb7589c2db76da0b0a6c81aacb7db0
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-24 13:12:03 -0500

    fix bug around overriding default search placeholder

--- src/modules/search/search.component.spec.ts
+++ src/modules/search/search.component.spec.ts
@@ -234,10 +234,10 @@ describe('Search component', () => {
   });
 
   it('should override default placeholder text when placeholder text is provided', () => {
-    component.placeholderText = '';
+    component.placeholderText = 'hey ya';
     fixture.detectChanges();
     /*tslint:disable */
-    expect(element.query(By.css('input')).attributes['placeholder']).toBe(null);
+    expect(element.query(By.css('input')).attributes['placeholder']).toBe('hey ya');
     /*tslint:enable */
   });
 
