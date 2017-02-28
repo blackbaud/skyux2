daa81c9e6e878181211cbf5e628577e07ff5d372
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-02-28 13:28:25 -0500

    Add icon buttonType functionality to dropdown component

---
+++ src/modules/sort/sort.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-sort',
+  styleUrls: ['./sort.component.scss'],
+  templateUrl: './sort.component.html'
+})
+export class SkySortComponent {
+
+}
