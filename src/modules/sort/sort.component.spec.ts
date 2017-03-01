fb5e85533b01c616f9ee0a95345fbd35de4a4fbf
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-03-01 11:20:32 -0500

    Finished unit tests for sort component

--- src/modules/sort/sort.component.spec.ts
+++ src/modules/sort/sort.component.spec.ts
@@ -38,24 +38,57 @@ describe('Sort component', () => {
     fixture.detectChanges();
   });
 
-  it('creates a sort dropdown that respects active input', () => {
+  function getDropdownButtonEl() {
     let dropdownButtonQuery = '.sky-sort .sky-dropdown .sky-dropdown-button .fa-sort';
-    expect(nativeElement.querySelector(dropdownButtonQuery)).not.toBeNull();
-    let menuHeaderQuery = '.sky-sort .sky-sort-menu-header';
-    expect(nativeElement.querySelector(menuHeaderQuery)).toHaveText('Sort by');
-    let itemQuery = '.sky-sort .sky-dropdown-menu .sky-sort-item';
-    let itemsEl = nativeElement.querySelectorAll(itemQuery);
+    return nativeElement.querySelector(dropdownButtonQuery) as HTMLElement;
+  }
 
+  function getSortItems() {
+    let itemQuery = '.sky-sort .sky-dropdown-menu .sky-sort-item';
+    return nativeElement.querySelectorAll(itemQuery);
+  }
+
+  it('creates a sort dropdown that respects active input', () => {
+    let dropdownButtonEl = getDropdownButtonEl();
+    expect(dropdownButtonEl).not.toBeNull();
+    dropdownButtonEl.click();
+    fixture.detectChanges();
+    let menuHeaderQuery = '.sky-sort .sky-sort-menu-heading';
+    expect(nativeElement.querySelector(menuHeaderQuery)).toHaveText('Sort by');
+
+    let itemsEl = getSortItems();
     expect(itemsEl.length).toBe(6);
     expect(itemsEl.item(2)).toHaveCssClass('sky-sort-item-selected');
+    expect(itemsEl.item(2)).toHaveText('Date created (newest first)');
 
   });
 
   it('changes active item on click and emits proper event', () => {
+    let dropdownButtonEl = getDropdownButtonEl();
+    dropdownButtonEl.click();
+    fixture.detectChanges();
 
+    let itemsEl = getSortItems();
+    let clickItem = itemsEl.item(1).querySelector('button') as HTMLElement;
+    clickItem.click();
+    fixture.detectChanges();
+
+    expect(component.sortedItem).toEqual( {
+      id: 2,
+      label: 'Assigned to (Z - A)',
+      name: 'assignee',
+      descending: true
+    });
+
+    itemsEl = getSortItems();
+    expect(itemsEl.item(1)).toHaveCssClass('sky-sort-item-selected');
   });
 
   it('can set active input programmatically', () => {
+    component.initialState = 4;
+    fixture.detectChanges();
 
+    let itemsEl = getSortItems();
+    expect(itemsEl.item(3)).toHaveCssClass('sky-sort-item-selected');
   });
 });
