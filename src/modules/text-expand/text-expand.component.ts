8ed5fd6828375fd8d1e1ff89f7e8d4dc7e00f0a7
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2017-03-09 16:47:21 -0500

    Modal view now in. Animation for normal view still needs implemented fully. Repeater view not yet complete.

--- src/modules/text-expand/text-expand.component.ts
+++ src/modules/text-expand/text-expand.component.ts
@@ -2,55 +2,46 @@ import {
   Component,
   Input,
   ElementRef,
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
   ViewChild
 } from '@angular/core';
 
 import {
   SkyResourcesService
 } from '../resources';
-
+import {
+  SkyModalService
+} from '../modal';
+import {
+  SkyTextExpandModalComponent
+} from './text-expand-modal.component';
+import {
+  SkyTextExpandModalContext
+} from './text-expand-modal-context';
 @Component({
   selector: 'sky-text-expand',
   templateUrl: './text-expand.component.html',
   styleUrls: ['./text-expand.component.scss'],
   providers: [
     SkyResourcesService
+  ],
+  animations: [
+    trigger('expansionChanged', [
+      state('false', style({ 'height': '*' })),
+      state('true', style({ 'height': '*' })),
+      transition('* <=> *', [style({ 'height': '*' }),
+        animate('250ms', style({ 'height': 'auto' }))])
+    ])
   ]
 })
 export class SkyTextExpandComponent {
   @Input()
   public set text(value: string) {
-    let textExpand = this;
-    if (value) {
-      this.collapsedText = this.getTruncatedText(value, this.maxLength, this.maxNewlines);
-      this.expandedText =
-        this.getTruncatedText(value, this.maxExpandedLength, this.maxExpandedNewlines);
-      if (this.collapsedText !== value) {
-        this.ellipsis.nativeElement.style.display = 'inline';
-        this.space.nativeElement.style.display = 'inline';
-        this.seeMore.nativeElement.style.display = 'inline';
-        this.seeMore.nativeElement.innerHtml = 'See more';
-        this.isExpanded = false;
-      } else {
-        this.ellipsis.nativeElement.style.display = 'none';
-        this.space.nativeElement.style.display = 'none';
-        this.seeMore.nativeElement.style.display = 'none';
-      }
-      this.textToShow = this.collapsedText;
-
-      this.seeMore.nativeElement.onclick = function(){
-        if (!textExpand.isExpanded) {
-          textExpand.animateText(textExpand.collapsedText, textExpand.expandedText, 'See less', false);
-          textExpand.isExpanded = true;
-        } else {
-          textExpand.animateText(textExpand.expandedText, textExpand.collapsedText, 'See more', true);
-          textExpand.isExpanded = false;
-        }
-      };
-
-    } else {
-      this.textToShow = '';
-    }
+    this.setup(value);
   }
   @Input()
   public maxLength: number = 200;
@@ -60,20 +51,81 @@ export class SkyTextExpandComponent {
   @Input()
   public maxExpandedNewlines: number = 2;
   @Input()
-  public expandModalTitle: string;
+  public expandModalTitle: string = 'Expanded text';
   @Input()
   public expandRepeaterMax: number;
   @Input()
   public expandRepeaterData: number;
-  public textToShow: string;
+  public seeMore: string;
+  private textToShow: string;
   private collapsedText: string;
   private expandedText: string;
-  private isExpanded: boolean;
-  @ViewChild('ellipsis') private ellipsis: ElementRef;
-  @ViewChild('space') private space: ElementRef;
-  @ViewChild('seeMore') private seeMore: ElementRef;
+  private newlineCount: number;
+  private isExpanded: boolean = false;
+  private expandable: boolean;
+  private elementHeight: string;
+  @ViewChild('container')
+  private containerEl: ElementRef;
+  @ViewChild('text')
+  private textEl: ElementRef;
 
-  constructor(private resources: SkyResourcesService, private elRef: ElementRef) { }
+  constructor(private resources: SkyResourcesService, private elRef: ElementRef,
+    private modalService: SkyModalService) { }
+
+  public textExpand() {
+    if (this.newlineCount > this.maxExpandedNewlines
+      || this.expandedText.length > this.maxExpandedLength) {
+      if (!this.isExpanded) {
+        this.modalService.open(
+          SkyTextExpandModalComponent,
+          [
+            {
+              provide: SkyTextExpandModalContext,
+              useValue: {
+                header: this.expandModalTitle,
+                text: this.expandedText
+              }
+            }
+          ]
+        );
+      }
+    } else {
+      if (!this.isExpanded) {
+        this
+          .animateText(this.collapsedText, this.expandedText, 'See less');
+        this.isExpanded = true;
+      } else {
+        this
+          .animateText(this.expandedText, this.collapsedText, 'See more');
+        this.isExpanded = false;
+      }
+    }
+  }
+
+  public animationEnd() {
+    this.textEl.nativeElement.textContent = this.textToShow;
+  }
+
+  private setup(value: string) {
+    if (value) {
+      this.newlineCount = this.getNewlineCount(value);
+      this.collapsedText = this.getTruncatedText(value, this.maxLength, this.maxNewlines);
+      this.expandedText = value.length > this.maxExpandedLength ||
+        this.newlineCount > this.maxExpandedNewlines ?
+          value : this.getTruncatedText(value, this.maxExpandedLength, this.maxExpandedNewlines);
+      if (this.collapsedText !== value) {
+        this.seeMore = 'See more';
+        this.isExpanded = false;
+        this.expandable = true;
+      } else {
+        this.expandable = false;
+      }
+      this.textToShow = this.collapsedText;
+    } else {
+      this.textToShow = '';
+    }
+    this.textEl.nativeElement.textContent = this.textToShow;
+  }
 
   private getNewlineCount(value: string) {
     let matches = value.match(/\n/gi);
@@ -87,7 +139,7 @@ export class SkyTextExpandComponent {
   private getTruncatedText(value: string, length: number, newlines: number) {
     let i;
 
-    if (newlines && this.getNewlineCount(value) >= newlines) {
+    if (newlines && this.newlineCount >= newlines) {
       value = value.replace(/\s+/gi, ' ');
     }
 
@@ -106,21 +158,22 @@ export class SkyTextExpandComponent {
   }
 
   private animateText(previousText: string, newText: string,
-    newExpandText: string, showEllipsis: boolean) {
+    newExpandText: string) {
     // Measure the current height so we can animate from it.
-    let currentHeight = this.elRef.nativeElement.style.height;
-
+    // let currentHeight = this.elementHeight;
+    let currentHeight = this.containerEl.nativeElement.offsetHeight;
     this.textToShow = newText;
-    this.seeMore.nativeElement.innerHtml = newExpandText;
-    let newHeight = this.elRef.nativeElement.style.height;
+    this.textEl.nativeElement.textContent = this.textToShow;
+    this.seeMore = newExpandText;
+    let newHeight = this.containerEl.nativeElement.offsetHeight;
 
     if (newHeight < currentHeight) {
       // The new text is smaller than the old text, so put the old text back before doing
       // the collapse animation to avoid showing a big chunk of whitespace.
-      this.textToShow = previousText;
+      this.textEl.nativeElement.textContent = previousText;
     }
-
-    this.ellipsis.nativeElement.style.display = showEllipsis ? 'inline' : 'none';
+    this.elementHeight = `${currentHeight}px`;
+    // this.elementHeight = currentHeight;
     // this.elRef.nativeElement
     //   .height(currentHeight)
     //   .animate(
--- src/modules/text-expand/text-expand.module.ts
+++ src/modules/text-expand/text-expand.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SkyResourcesModule } from '../resources';
 import { SkyTextExpandComponent } from './text-expand.component';
+import { SkyModalModule } from '../modal';
+import { SkyTextExpandModalComponent } from './text-expand-modal.component';
 
 @NgModule({
   declarations: [
-    SkyTextExpandComponent
+    SkyTextExpandComponent,
+    SkyTextExpandModalComponent
   ],
   imports: [
-    SkyResourcesModule
+    SkyResourcesModule,
+    SkyModalModule,
+    CommonModule
   ],
   exports: [
     SkyTextExpandComponent
+  ],
+  entryComponents: [
+    SkyTextExpandModalComponent
   ]
 })
 export class SkyTextExpandModule { }
