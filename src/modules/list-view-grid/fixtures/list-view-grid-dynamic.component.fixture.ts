33760d74037cbb8ba6ca09b3817841cffb5bfeea
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-03-10 15:35:32 -0500

    fix error where selectedIds are overwritten by previous values

--- src/modules/list-view-grid/fixtures/list-view-grid-dynamic.component.fixture.ts
+++ src/modules/list-view-grid/fixtures/list-view-grid-dynamic.component.fixture.ts
@@ -5,11 +5,10 @@ import { Component } from '@angular/core';
   template: require('./list-view-grid-dynamic.component.fixture.html')
 })
 export class ListViewGridDynamicTestComponent {
-  public displayedColumns: Array<string> = ['name', 'email'];
   public data: Array<any>;
   public gridColumns: Array<any>;
   constructor() {
-    this.data = [{ 'id': 1, 'name': 'Windstorm', 'email': 'windstorm@gmail.com' },
+    this.data = [{ 'id': 1, 'name': 'Windstorm', 'email': 'windstorm@gmail.com', 'other': 'something' },
     { 'id': 2, 'name': 'Bombasto', 'email': 'bombasto@gmail.com' },
     { 'id': 3, 'name': 'Magneta', 'email': 'magenta@gmail.com' },
     { 'id': 4, 'name': 'Tornado', 'email': 'tornado@gmail.com' }];
@@ -20,5 +19,9 @@ export class ListViewGridDynamicTestComponent {
   public changeColumns() {
     this.gridColumns = [{ 'id': 1, 'field': 'name', 'heading': 'Name' },
     { 'id': 2, 'field': 'email', 'heading': 'Email' }];
+  }
+
+  public changeColumnsDifferent() {
+      this.gridColumns = [{ 'id': 3, 'field': 'other', 'heading': 'Other' }];
   }
 }
