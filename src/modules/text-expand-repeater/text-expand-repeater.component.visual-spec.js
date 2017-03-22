cf50abf8e5d8e7f5a5916a0594c7c59cf7d74de0
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2017-03-22 13:00:21 -0400

    Visual test fix

--- src/modules/text-expand-repeater/text-expand-repeater.component.visual-spec.js
+++ src/modules/text-expand-repeater/text-expand-repeater.component.visual-spec.js
@@ -14,7 +14,7 @@ describe('TextExpandRepeater', function () {
   it('should match the previous text expand repeater when expanded', function () {
     return browser
       .setupTest('/text-expand-repeater.html')
-      .click('.sky-text-expand-see-more')
+      .click('.sky-text-expand-repeater-see-more')
       .compareScreenshot({
         checkAccessibility: true,
         screenshotName: 'text-expand-repeater-expanded',
