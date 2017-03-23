930b850fda6b8fb113a715639de863932cf70189
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-03-23 17:15:52 -0400

    visual tests for filters
    Also, updated screenshot for toolbar section border fix

---
+++ src/modules/filter/fixtures/filter.component.visual-fixture.ts
@@ -0,0 +1,41 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { SkyModule } from '../../../../src/core';
+
+import { Bootstrapper } from '../../../../visual/bootstrapper';
+
+@Component({
+  selector: 'sky-demo-app',
+  templateUrl: './filter.component.visual-fixture.html'
+})
+class AppComponent {
+  public filtersActive: boolean = false;
+
+  public appliedFilters: Array<any> = [
+    {
+      label: 'hide orange',
+      dismissible: false
+    },
+    {
+      label: 'berry fruit type',
+      dismissible: true
+    }
+  ];
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    SkyModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+class AppModule { }
+
+Bootstrapper.bootstrapModule(AppModule);
