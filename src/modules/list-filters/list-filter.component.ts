4e9e43c0def0c4057e288dbc034a40118ec8b922
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-03-24 15:12:24 -0400

    start of integrating filters into list

---
+++ src/modules/list-filters/list-filter.component.ts
@@ -0,0 +1,50 @@
+import {
+  Component,
+  Input,
+  ContentChildren,
+  TemplateRef,
+  QueryList,
+  OnInit
+} from '@angular/core';
+
+import { ListItemModel } from '../list/state';
+import { ListViewComponent } from '../list/list-view.component';
+
+@Component({
+  selector: 'sky-list-filter',
+  template: '<ng-content></ng-content>'
+})
+export class SkyListFilterComponent implements OnInit {
+  @Input()
+  public name: string;
+
+  @Input()
+  public label: string;
+
+  @Input()
+  public type: string;
+
+  @Input()
+  public view: ListViewComponent;
+
+  /* tslint:disable */
+  @Input('filter')
+  public filterFunction: (item: ListItemModel, filter: any) => boolean;
+
+  @Input('template')
+  public templateInput: TemplateRef<any>;
+  /* tslint:enable */
+
+  @ContentChildren(TemplateRef)
+  private templates: QueryList<TemplateRef<any>>;
+
+  public ngOnInit() {
+    if (this.name === undefined || this.name.length === 0) {
+      throw new Error('Sky List Filter requires a name.');
+    }
+  }
+
+  public get template(): TemplateRef<any> {
+    return this.templates.length > 0 ? this.templates.first : this.templateInput;
+  }
+}
---
+++ src/modules/list-filters/list-filters.component.ts
@@ -0,0 +1,152 @@
+import {
+  Component,
+  Input,
+  ContentChildren,
+  QueryList,
+  ViewChild,
+  TemplateRef,
+  AfterContentInit,
+  AfterViewInit
+} from '@angular/core';
+
+import { SkyModalService } from '../modal';
+
+import { SkyListFilterComponent } from './list-filter.component';
+
+import {
+  ListState,
+  ListStateDispatcher
+} from '../list/state';
+
+import {
+  ListFilterModel,
+  ListToolbarItemModel,
+  ListFilterDataModel
+} from '../list/state';
+
+import {
+  SkyListFiltersModalComponent
+} from './list-filters-modal.component';
+
+import {
+  Observable
+} from 'rxjs/Observable';
+
+@Component({
+  selector: 'sky-list-filters',
+  templateUrl: './list-filters.component.html',
+  styleUrls: ['./list-filters.component.scss']
+})
+export class SkyListFiltersComponent implements AfterContentInit, AfterViewInit {
+
+  // Change this to use resource service.
+  @Input()
+  public modalTitle: string = 'Filters';
+
+  @ContentChildren(SkyListFilterComponent)
+  private filters: QueryList<SkyListFilterComponent>;
+
+  @ViewChild('filterButton')
+  private filterButtonTemplate: TemplateRef<any>;
+
+  private inlineBarExpanded: boolean = false;
+
+  private inlineFilters: Observable<ListFilterModel[]>;
+
+  private modalFilters: Observable<ListFilterModel[]>;
+
+  private activeModalFilters: Observable<ListFilterDataModel[]>;
+
+  private isFiltered: Observable<boolean>;
+
+  constructor(
+    private state: ListState,
+    private dispatcher: ListStateDispatcher,
+    private modalService: SkyModalService
+  ) {}
+
+  public ngAfterContentInit() {
+    let filterModels = this.filters.map(filter => {
+      return new ListFilterModel(filter, filter.view !== undefined ? filter.view.id : undefined);
+    });
+
+    filterModels.forEach(filter => {
+      filter.type === 'inline' ?
+        filter.filterModel.onChange.subscribe(() => this.applyFilters()) :
+        undefined
+    });
+
+    this.dispatcher.filtersLoad(filterModels);
+
+    this.inlineFilters = this.state.map((s) => {
+      return s.filters.filter(f => f.type === 'inline');
+    }).distinctUntilChanged();
+
+    this.modalFilters = this.state.map((s) => {
+      return s.filters.filter(f => f.type !== 'inline');
+    }).distinctUntilChanged();
+
+    this.activeModalFilters = this.modalFilters.map((s) => {
+      return s.filter(f => f.filterModel.value !== '' && f.filterModel.value !== undefined);
+    })
+    .map(f => f.map(filter => filter.filterModel));
+
+    this.isFiltered = this.state.map(s => {
+      return s.filters
+        .filter(f => f.filterModel.value !== '' && f.filterModel.value !== undefined).length > 0;
+    });
+  }
+
+  public ngAfterViewInit() {
+    this.dispatcher.toolbarAddItems([
+      new ListToolbarItemModel({ template: this.filterButtonTemplate, location: 'right'})
+    ]);
+  }
+
+  public openFiltersModal() {
+    let providers = [
+      {
+        provide: ListState,
+        useValue: this.state
+      },
+      {
+        provide: ListStateDispatcher,
+        useValue: this.dispatcher
+      },
+      {
+        provide: String,
+        useValue: this.modalTitle
+      }
+    ];
+    this.modalService.open(SkyListFiltersModalComponent, providers);
+  }
+
+  public filterButtonClick() {
+    this.inlineFilters
+      .take(1)
+      .subscribe(filters => {
+        if (filters.length > 0) {
+          this.inlineBarExpanded = !this.inlineBarExpanded;
+        } else {
+          this.openFiltersModal();
+        }
+      });
+  }
+
+  public clearFilter(filterId: string) {
+    this.state.map(s => s.filters)
+      .take(1)
+      .subscribe(filters => {
+        let updatedFilters: ListFilterModel[] = [];
+        filters.forEach((filter) => {
+          let model = new ListFilterModel(filter, filter.view);
+          model.filterModel = new ListFilterDataModel(filter.filterModel);
+          model.filterModel.value = (model.filterModel.id === filterId) ? '' :
+            model.filterModel.value;
+          updatedFilters.push(model);
+        });
+
+        this.dispatcher.filtersUpdate(updatedFilters);
+      });
+  }
+}
