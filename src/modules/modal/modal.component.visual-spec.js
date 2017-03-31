6ad2374321e31e7fa2d658957f02e8755ce87444
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-03-31 12:35:01 -0400

    Modal style updates (#540)
    * Modal style updates
    
    * fix lint error
    
    * update screenshots for new modal styles
    
    * move cursor offscreen for modal visual tests

--- src/modules/modal/modal.component.visual-spec.js
+++ src/modules/modal/modal.component.visual-spec.js
@@ -5,6 +5,7 @@ describe('Modal', function () {
     return browser
       .setupTest('/modal.html')
       .click('.sky-btn-primary')
+      .moveCursorOffScreen()
       .pause(1000)
       .compareScreenshot({
         screenshotName: 'modal',
@@ -17,6 +18,7 @@ describe('Modal', function () {
     return browser
       .setupTest('/modal.html', 480)
       .click('.sky-btn-primary')
+      .moveCursorOffScreen()
       .pause(1000)
       .compareScreenshot({
         screenshotName: 'modal_small',
@@ -29,9 +31,36 @@ describe('Modal', function () {
     return browser
       .setupTest('/modal.html')
       .click('.sky-test-large-modal')
+      .moveCursorOffScreen()
       .pause(1000)
       .compareScreenshot({
         screenshotName: 'modal_large',
+        selector: '.sky-modal',
+        checkAccessibility: true
+      });
+  });
+
+  it('should match previous large modal screenshot on mobile', function () {
+    return browser
+      .setupTest('/modal.html', 480)
+      .click('.sky-test-large-modal')
+      .moveCursorOffScreen()
+      .pause(1000)
+      .compareScreenshot({
+        screenshotName: 'modal_large_mobile',
+        selector: '.sky-modal',
+        checkAccessibility: true
+      });
+  });
+
+  it('should match previous screenshot of modal without header or footer', function () {
+    return browser
+      .setupTest('/modal.html')
+      .click('.sky-test-content-only')
+      .moveCursorOffScreen()
+      .pause(1000)
+      .compareScreenshot({
+        screenshotName: 'modal_content_only',
         selector: '.sky-modal',
         checkAccessibility: true
       });
