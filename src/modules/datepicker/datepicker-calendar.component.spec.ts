06594c26b7ba030ca9349d7907fa63a576238085
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-04-18 14:40:51 -0400

    test stubs for datepicker calendar

---
+++ src/modules/datepicker/datepicker-calendar.component.spec.ts
@@ -0,0 +1,83 @@
+describe('datepicker calendar', () => {
+  it('should show the appropriate daypicker with the selected date', () => {
+
+  });
+
+  it('should show the month picker with selected date when clicking on the month', () => {
+
+  });
+
+  it('should show the year picker with selected date when clicking the year', () => {
+
+  });
+
+  it('should move to another day within the current month in daypicker', () => {
+
+  });
+
+  it('should move to another day in the next month in daypicker', () => {
+
+  });
+
+  it('should move to the next month using arrows in daypicker', () => {
+
+  });
+
+  it('should move to the previous month using arrows in daypicker', () => {
+
+  });
+
+  it('should move to the next month and move to daypicker when clicking a month in monthpicker',
+  () => {
+
+  });
+
+  it('should move to the next year when clicking arrows monthpicker', () => {
+
+  });
+
+  it('should move to the previous year when clicking arrows in monthpicker', () => {
+
+  });
+
+  it('should move to the next year and move to monthpicker when clicking year in yearpicker',
+  () => {
+
+  });
+
+  it('should move to the next set of years when clicking arrows in yearpicker', () => {
+
+  });
+
+  it('should move to the previous set of years when clicking arrows in yearpicker', () => {
+
+  });
+
+  it('should handle minDate in daypicker', () => {
+
+  });
+
+  it('should handle maxDate in daypicker', () => {
+
+  });
+
+  it('should handle minDate in monthpicker', () => {
+
+  });
+
+  it('should handle maxDate in monthpicker', () => {
+
+  });
+
+  it('should handle minDate in yearpicker', () => {
+
+  });
+
+  it('should handle maxDate in yearpicker', () => {
+
+  });
+
+  it('should open current month in daypicker when no selected date is provided', () => {
+
+  });
+});
