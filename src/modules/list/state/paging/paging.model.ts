7660a332b14159ad9deac5d4db4c61c7fc987b71
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-04-21 11:36:52 -0400

    Update in memory data provider to show more than 10 entries without paging

--- src/modules/list/state/paging/paging.model.ts
+++ src/modules/list/state/paging/paging.model.ts
@@ -1,17 +1,15 @@
 export class ListPagingModel {
-  public itemsPerPage: number = 10;
-  public maxDisplayedPages: number = 5;
-  public pageCount: number = 0;
-  public pageNumber: number = 1;
-  public displayedPages: number[] = [];
+  public itemsPerPage: number;
+  public maxDisplayedPages: number;
+  public pageCount: number;
+  public pageNumber: number;
 
   constructor(data?: any) {
     if (data !== undefined) {
-      this.itemsPerPage = data.itemsPerPage;
-      this.maxDisplayedPages = data.maxDisplayedPages;
-      this.pageCount = data.pageCount;
-      this.pageNumber = data.pageNumber;
-      this.displayedPages = data.displayedPages;
+      this.itemsPerPage = data.itemsPerPage || 10;
+      this.maxDisplayedPages = data.maxDisplayedPages || 5;
+      this.pageCount = data.pageCount || 0;
+      this.pageNumber = data.pageNumber || 1;
     }
   }
 }
