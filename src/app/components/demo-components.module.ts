5ff257c41d2d2c1850bec3299509d274c052f8ca
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-05-10 13:03:34 -0400

    Update to work with latest builder

--- src/app/components/demo-components.module.ts
+++ src/app/components/demo-components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SkyDemoComponentsService } from './demo-components.service';
 
-require('style!prismjs/themes/prism.css');
+require('style-loader!prismjs/themes/prism.css');
 
 @NgModule({
   providers: [
