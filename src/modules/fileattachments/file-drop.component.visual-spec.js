fe71d973b5449b00710d422c0c6f9067792081da
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-05-16 09:07:58 -0400

    Handle styles for filedrop when links are not allowed (#686)

--- src/modules/fileattachments/file-drop.component.visual-spec.js
+++ src/modules/fileattachments/file-drop.component.visual-spec.js
@@ -9,4 +9,15 @@ describe('File drop', function () {
         selector: '#screenshot-file-drop'
       });
   });
+
+  it('should match the file drop control when no links allowed', function () {
+    return browser
+      .setupTest('/fileattachments.html')
+      .click('.sky-test-allow-links')
+      .pause(1000)
+      .compareScreenshot({
+        screenshotName: 'file_drop_nolink',
+        selector: '#screenshot-file-drop'
+      });
+  });
 });
