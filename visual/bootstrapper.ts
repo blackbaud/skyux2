ce0e26d54f31662eb9353d6f7c30a1ff73eb7d7d
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-05-18 10:42:29 -0400

    Add more logging and use different travis encryption

--- visual/bootstrapper.ts
+++ visual/bootstrapper.ts
@@ -1,8 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+console.log('before requires');
 const styleLoader = require('../config/utils/sky-style-loader');
 
 require('../node_modules/axe-core/axe.min.js');
+console.log('after requires');
 
 (window as any).styleLoader = styleLoader;
 
