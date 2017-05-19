97149a4c54ffc36eb11f2129a68081f29c68d95f
Adam Hickey <adam.hickey@blackbaud.com>
2017-05-19 16:29:02 -0400

    Add visual tests.

--- src/modules/error/fixtures/error.component.visual-fixture.ts
+++ src/modules/error/fixtures/error.component.visual-fixture.ts
@@ -2,20 +2,34 @@ import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SkyErrorModule } from '../error.module';
-
 import { Bootstrapper } from '../../../../visual/bootstrapper';
+import { ErrorModalConfig } from '../error-modal-config';
+import { SkyErrorModalService } from '../error-modal.service';
+import { SkyModalModule } from '../../modal/modal.module';
 
 @Component({
   selector: 'sky-demo-app',
   templateUrl: './error.component.visual-fixture.html'
 })
 class AppComponent {
+  constructor(private modal: SkyErrorModalService) { }
+
+  public openModal() {
+    const config: ErrorModalConfig = {
+      errorTitle: 'Some error title',
+      errorDescription: 'Description of error',
+      errorCloseText: 'Close button text'
+    };
+
+    this.modal.open(config);
+  }
 }
 
 @NgModule({
   imports: [
     BrowserModule,
-    SkyErrorModule
+    SkyErrorModule,
+    SkyModalModule
   ],
   declarations: [
     AppComponent
