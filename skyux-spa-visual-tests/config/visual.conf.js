6070f63112c8f5f178e30e3aa11eb79036d6fd9c
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-06-05 15:46:16 -0400

    Continue protractor work

---
+++ skyux-spa-visual-tests/config/visual.conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['../src/app/**/*.visual-spec.js']
+};
---
+++ skyux-spa-visual-tests/src/app/alert/alert.component.visual-spec.js
@@ -0,0 +1,7 @@
+describe('Alert', function () {
+  'use strict';
+
+  it('should match previous alert screenshot', function () {
+    browser.get('https://host.nxt.blackbaud.com/visual-tests/alert');
+  });
+});
