1597b082954d4c4a097d0bd8e7ba310c95d7f9a4
Blackbaud-SandhyaRajasabeson <sandhya.rajasabeson@blackbaud.com>
2017-06-08 10:51:51 -0400

    fixed lint whitespace changes

--- src/app/components/demo-components.component.ts
+++ src/app/components/demo-components.component.ts
@@ -31,14 +31,14 @@ export class SkyDemoComponentsComponent implements OnInit {
     this.displayedItems = this.actionButtonComponents;
   }
 
-  public searchApplied(searchText: string){
+  public searchApplied(searchText: string) {
     let filteredItems = this.actionButtonComponents;
-    if(searchText){
+    if (searchText) {
       filteredItems = this.actionButtonComponents.filter(function (item: any){
         let property: any;
-        for(property in item){
-          if(item.hasOwnProperty(property) && (property === 'name' || property === 'summary')){
-            if (item[property].toLowerCase().indexOf(searchText.toLowerCase()) > -1){
+        for (property in item) {
+          if (item.hasOwnProperty(property) && (property === 'name' || property === 'summary')) {
+            if (item[property].toLowerCase().indexOf(searchText.toLowerCase()) > -1) {
               return true;
             }
           }
