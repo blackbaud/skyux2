94c347fb22fc32e323b34db0e2d7acf1dd73b4b9
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-06-09 20:45:30 -0400

    Add page summary visual tests

---
+++ skyux-spa-visual-tests/src/app/page-summary/page-summary.visual-spec.ts
@@ -0,0 +1,185 @@
+import { SkyVisualTest } from '../../../config/utils/visual-test-commands';
+
+import { browser, element, by } from 'protractor';
+
+describe('Page summary', () => {
+
+  function clickTest(
+    screenshotName: string, visibleComponents: Array<string>, screenWidth?: number) {
+    SkyVisualTest.setupTest('/page-summary', screenWidth);
+    element(by.css('#screenshots-page-summary-items')).sendKeys(visibleComponents.join(','));
+    return SkyVisualTest.compareScreenshot({
+      screenshotName: ('pagesummary-' + screenshotName),
+      selector: '#screenshots-page-summary',
+      checkAccessibility: true
+    });
+  }
+
+  it(
+    'should match previous pagesummary screenshot when all components are present',
+    () => {
+      return clickTest(
+        'all',
+        [
+            'Title',
+            'Subtitle',
+            'Image',
+            'Status',
+            'KeyInfo',
+            'Content',
+            'Alert'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when all components are present on small screens',
+    () => {
+      return clickTest(
+        'all',
+        [
+          'Title',
+          'Subtitle',
+          'Image',
+          'Status',
+          'KeyInfo',
+          'Content',
+          'Alert'
+        ],
+        480
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when no image is present',
+    () => {
+      return clickTest(
+        'noimage',
+        [
+          'Title',
+          'Subtitle',
+          'Status',
+          'KeyInfo',
+          'Content',
+          'Alert'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when no subtitle is present',
+    () => {
+      return clickTest(
+        'nosubtitle',
+        [
+          'Title',
+          'Image',
+          'Status',
+          'KeyInfo',
+          'Content',
+          'Alert'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when no status is present',
+    () => {
+      return clickTest(
+        'nostatus',
+        [
+          'Title',
+          'Subtitle',
+          'Image',
+          'KeyInfo',
+          'Content',
+          'Alert'
+        ]
+      );
+    }
+  );
+
+  it('should match previous pagesummary screenshot when no key info is present',
+  () => {
+      return clickTest(
+        'nokeyinfo',
+        [
+          'Title',
+          'Subtitle',
+          'Image',
+          'Status',
+          'Content',
+          'Alert'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when no additional content is present',
+    () => {
+      return clickTest(
+        'nocontent',
+        [
+          'Title',
+          'Subtitle',
+          'Image',
+          'Status',
+          'KeyInfo',
+          'Alert'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when no alert is present',
+    () => {
+      return clickTest(
+        'noalert',
+        [
+          'Title',
+          'Subtitle',
+          'Image',
+          'Status',
+          'KeyInfo',
+          'Content'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when only image, title, and subtitle are present',
+    () => {
+      return clickTest(
+        'image_title_subtitle',
+        [
+            'Title',
+            'Subtitle',
+            'Image'
+        ]
+      );
+    }
+  );
+
+  it(
+    'should match previous pagesummary screenshot when only image, title, and subtitle are ' +
+    'present on small screens',
+    () => {
+      return clickTest(
+        'image_title_subtitle',
+        [
+          'Title',
+          'Subtitle',
+          'Image'
+        ],
+        480
+      );
+    }
+  );
+});
