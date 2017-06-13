584a55736a0c49d2b278eaea05eb500bc160f2ee
Blackbaud-SandhyaRajasabeson <sandhya.rajasabeson@blackbaud.com>
2017-06-13 15:21:50 -0400

    created email validation ts

---
+++ src/modules/email-validation/email-validation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-email-validation',
+  templateUrl: './email-validation.component.html',
+  styleUrls: ['./email-validation.component.scss']
+})
+
+export class SkyEmailValidationComponent{
+    
+}
