ecbf5b64e0bc040a5d4b255ff3e1711d78f91183
Adam Hickey <adam.hickey@blackbaud.com>
2017-06-22 16:17:05 -0400

    Can't add mark text to innerhtml.

--- src/modules/text-highlight/text-highlight.directive.spec.ts
+++ src/modules/text-highlight/text-highlight.directive.spec.ts
@@ -9,6 +9,13 @@ import { SkyTextHighlightTestComponent } from './fixtures/text-highlight.compone
 import { SkyCheckboxModule } from '../checkbox/checkbox.module';
 import { SkyTextHighlightModule } from './text-highlight.module';
 
+function updateInputText(fixture: ComponentFixture<SkyTextHighlightTestComponent>, text: string) {
+  const inputEl = fixture.nativeElement.querySelector('.sky-input-search-term') as HTMLInputElement;
+  inputEl.value = text;
+  inputEl.dispatchEvent(new Event('input'));
+  fixture.detectChanges();
+}
+
 describe('Highlight', () => {
 
   let fixture: ComponentFixture<SkyTextHighlightTestComponent>;
@@ -44,8 +51,7 @@ describe('Highlight', () => {
   });
 
   it('should highlight search term', () => {
-      component.searchTerm = 'text';
-      fixture.detectChanges();
+      updateInputText(fixture, 'text');
 
       const containerEl = nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
       const expectedHtml = `Here is some test <mark>text</mark>.
@@ -57,8 +63,7 @@ describe('Highlight', () => {
   });
 
   it('should highlight case insensitive search term', () => {
-      component.searchTerm = 'here';
-      fixture.detectChanges();
+      updateInputText(fixture, 'here');
 
       const containerEl = nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
       const expectedHtml = `<mark>Here</mark> is some test text.
@@ -69,12 +74,83 @@ describe('Highlight', () => {
       expect(containerEl.innerHTML.trim()).toBe(expectedHtml);
   });
 
-//   it('should highlight search term in nested component', () => {
-//   });
+  it('should highlight search term in nested component', () => {
+      component.showAdditionalContent = true;
+      fixture.detectChanges();
 
-//   it('changed search term should highlight new term and old term should not highlight', () => {
-//   });
+      updateInputText(fixture, 'here');
 
-//   it('highlight search term of html that was previously hidden', () => {
-//   });
+      const containerEl = nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
+      const expectedHtml = `<mark>Here</mark> is some test text.
+    <!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><div>
+        <mark>Here</mark> is additional text that was previously hidden.
+    </div>`;
+
+      expect(containerEl.innerHTML.trim()).toBe(expectedHtml);
+  });
+
+  it('changed search term should highlight new term and old term should not highlight', () => {
+      updateInputText(fixture, 'some');
+
+      const containerEl = nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
+      const expectedHtml = `Here is <mark>some</mark> test text.
+    <!--bindings={
+  "ng-reflect-ng-if": "false"
+}-->`;
+
+      expect(containerEl.innerHTML.trim()).toBe(expectedHtml);
+
+      updateInputText(fixture, 'Here');
+
+      const containerElChanged =
+        nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
+      const expectedHtmlChanged = `<mark>Here</mark> is some test text.
+    <!--bindings={
+  "ng-reflect-ng-if": "false"
+}-->`;
+
+      expect(containerElChanged.innerHTML.trim()).toBe(expectedHtmlChanged);
+  });
+
+  it('highlight search term of html that was previously hidden', () => {
+      console.warn('1');
+      component.showAdditionalContent = false;
+      fixture.detectChanges();
+      console.warn('2');
+
+      updateInputText(fixture, 'is');
+      console.warn('3');
+
+      const containerEl = nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
+      const expectedHtml = `Here <mark>is</mark> some test text.
+    <!--bindings={
+  "ng-reflect-ng-if": "false"
+}-->`;
+
+      expect(containerEl.innerHTML.trim()).toBe(expectedHtml);
+
+      // check box to show extra content
+      const checkboxEl =
+        fixture.nativeElement.querySelector('.sky-test-checkbox') as HTMLInputElement;
+
+      checkboxEl.click();
+
+      console.warn(component.showAdditionalContent);
+      console.warn('4');
+      updateInputText(fixture, 'is1');
+
+      const containerElUpdated =
+        nativeElement.querySelector('.sky-test-div-container') as HTMLElement;
+
+      const expectedHtmlUpdated = `Here <mark>is</mark> some test text.
+    <!--bindings={
+  "ng-reflect-ng-if": "true"
+}--><div>
+        Here <mark>is</mark> additional text that was previously hidden.
+    </div>`;
+
+      expect(containerElUpdated.innerHTML.trim()).toBe(expectedHtmlUpdated);
+  });
 });
--- src/modules/text-highlight/text-highlight.directive.ts
+++ src/modules/text-highlight/text-highlight.directive.ts
@@ -7,6 +7,8 @@ import {
   ElementRef
 } from '@angular/core';
 
+const className = 'sky-highlight-mark';
+
 @Directive({
     selector: '[skyHighlight]'
 })
@@ -17,22 +19,66 @@ export class SkyTextHighlightDirective implements OnChanges, AfterViewInit {
 
   private existingHighlight = false;
 
-  private static getHighlightedNodeText(node: HTMLElement, searchText: string): string {
-    const html = node.innerHTML;
-    const searchRegex = new RegExp(searchText, 'gi');
-    const newHtml = html.replace(searchRegex, match => {
-      return `<mark>${match}</mark>`;
-    });
-
-    return newHtml;
-  }
-
   private static getRemoveHighlightedHtml(node: HTMLElement): string {
     const html = node.innerHTML;
     const searchRegex = new RegExp(`<mark>|<\/mark>`, 'gi');
     const highlightRemoved = html.replace(searchRegex, '');
 
     return highlightRemoved;
+  }
+
+  private static getRegexMatch(node: HTMLElement, searchText: string): RegExpExecArray {
+    const text = node.nodeValue;
+    const searchRegex = new RegExp(searchText, 'gi');
+
+    const result = searchRegex.exec(text);
+
+    return result;
+  }
+
+  private static markTextNodes(node: any, searchText: string) {
+    if (node.nodeType === 3) {
+      const regexMatch = SkyTextHighlightDirective.getRegexMatch(node, searchText);
+
+      if (regexMatch && regexMatch.length > 0) {
+
+        // split apart text node with mark tags in the middle on the search term
+        const matchIndex = regexMatch.index;
+
+        const middle = node.splitText(matchIndex);
+        middle.splitText(searchText.length);
+        const middleClone = middle.cloneNode(true);
+
+        const markNode = document.createElement('mark');
+        markNode.className = className;
+        markNode.appendChild(middleClone);
+        middle.parentNode.replaceChild(markNode, middle);
+
+        return 1;
+      }
+    } else if (node.nodeType === 1 && node.childNodes) {
+      for (let i = 0; i < node.childNodes.length; i++) {
+        let childNode = node.childNodes[i] as HTMLElement;
+        i += SkyTextHighlightDirective.markTextNodes(childNode, searchText);
+      }
+    }
+
+    return 0;
+  }
+
+  private static removeHighlight(el: ElementRef) {
+    const matchedElements =
+      el.nativeElement.querySelectorAll(`mark.${className}`) as NodeList;
+
+    if (matchedElements) {
+      for (let i = 0; i < matchedElements.length; i++) {
+        const node = matchedElements[i];
+        const parentNode = node.parentNode;
+
+        parentNode.replaceChild(node.firstChild, node);
+        parentNode.normalize();
+      }
+    }
   }
 
   constructor(private el: ElementRef) { }
@@ -50,19 +96,17 @@ export class SkyTextHighlightDirective implements OnChanges, AfterViewInit {
   }
 
   private highlight(): void {
-    if (this.existingHighlight) {
-      // remove existing highlight
-      const node: HTMLElement = this.el.nativeElement;
-      const newHtml = SkyTextHighlightDirective.getRemoveHighlightedHtml(this.el.nativeElement);
-      node.innerHTML = newHtml;
-    }
-
     const searchText = this.skyHighlight;
+
+    if (this.existingHighlight) {
+      SkyTextHighlightDirective.removeHighlight(this.el);
+    }
 
     if (this.readyForHighlight(searchText)) {
       const node: HTMLElement = this.el.nativeElement;
-      const newHtml = SkyTextHighlightDirective.getHighlightedNodeText(node, searchText);
-      node.innerHTML = newHtml;
+
+      // mark all matched text in the DOM
+      SkyTextHighlightDirective.markTextNodes(node, searchText);
       this.existingHighlight = true;
     }
   }
