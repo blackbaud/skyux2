8904e504cc6b5d8ba51fffce5ad69acf930d5a27
Jamin Quimby <jamin.quimby@blackbaud.me>
2017-07-13 10:31:44 -0400

    cleanup

--- skyux-spa-visual-tests/src/app/colorpicker/colorpicker.visual-spec.ts
+++ skyux-spa-visual-tests/src/app/colorpicker/colorpicker.visual-spec.ts
@@ -28,45 +28,4 @@ fdescribe('Colorpicker', () => {
     });
 
   });
-/*
-  it('should match previous yearpicker screenshot', () => {
-    return SkyVisualTest.setupTest('colorpicker')
-    .then(() => {
-      element(by.css('.sky-colorpicker-calendar-title')).click();
-      element(by.css('.sky-colorpicker-calendar-title')).click();
-      SkyVisualTest.moveCursorOffScreen()
-      return SkyVisualTest.compareScreenshot({
-        screenshotName: 'Colorpicker-yearpicker',
-        selector: '#screenshot-colorpicker-calendar'
-      });
-    });
-
-  });
-
-  it('should match previous Colorpicker input screenshot', () => {
-    return SkyVisualTest.setupTest('colorpicker')
-    .then(() => {
-      SkyVisualTest.moveCursorOffScreen()
-      return SkyVisualTest.compareScreenshot({
-        screenshotName: 'Colorpicker-input',
-        selector: '#screenshot-colorpicker'
-      });
-    });
-
-  });
-
-  it('should match previous Colorpicker input screenshot when open', () => {
-    return SkyVisualTest.setupTest('colorpicker')
-    .then(() => {
-       element(by.css('.sky-dropdown-button')).click();
-      SkyVisualTest.moveCursorOffScreen()
-      return SkyVisualTest.compareScreenshot({
-        screenshotName: 'Colorpicker-input-open',
-        selector: '#screenshot-colorpicker'
-      });
-    });
-
-  });
-
-  */
 });
