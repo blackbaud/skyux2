319170805894e5b99d18044cfab582b13a1f6e8b
Blackbaud-SandhyaRajasabeson <sandhya.rajasabeson@blackbaud.com>
2017-07-13 10:15:52 -0400

    Demo

---
+++ src/app/components/confirmation-dialogue/confirmation-dialogue-demo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sky-modal-demo',
+    templateUrl: './confirmation-dialogue-demo.component.html'
+})
+export class SkyConfirmationDialogueDemoComponent {
+    public openModal(){
+        /*
+            figure out what providers are. put that.... in demo or main??
+            filters uses modal pop up so base off of that
+            figure out how filters works
+            emitting for confirm and cancel: necessary?
+                maybe not for cancel, in filters there is a closed thing afterwards, figure that out
+                */0
+    }
+}
