89cfacf9ea4e46112462b19883816aad03c41e54
Jamin Quimby <jamin.quimby@blackbaud.com>
2017-07-18 15:02:43 -0400

    Color Picker - Code Review (#830)
    * colorpicker init
    
    * Color Picker
    
    * color picker
    
    * color picker
    
    * color picker
    
    * color picker
    
    * renaming
    
    * forgot these
    
    * colorpicker buttons
    
    * color picker
    
    * color picker
    
    * skyux color picker
    
    * color picker
    
    * update demo document
    
    * updated demo
    
    * Accessibility Esc and Tabbing
    
    * Preset Colors if none defined per spec
    
    * first test and css cleanup
    
    * fix firefox bug, updated colorpicker demo.
    
    * add unit tests
    
    * bug fix
    
    * forgot to remove the f
    
    * Cleaned up Demo page
    
    * fix lint errors
    
    * added aria label
    
    * IE Fixes.
    
    * IE mouse move fix
    
    * Firefox fix. 6 points past the decimal. =/
    
    * Safari decimal point fix
    
    * Removing the f from describe
    
    * Microsoft Edge created an Edge Case. fix
    
    * forgot to remove the f
    
    * Interface fix
    
    * updating color
    
    * css fix
    
    * Why does the builder not respect Angular aliases?
    
    * removed template refrence
    
    * fixed demo references
    
    * MouseUp fix
    
    * Patricks code review update
    
    * made a test
    
    * Color Picture visual tests
    
    * cleanup
    
    * quick fix scss & semantics
    
    * moved private variables into method scopes.
    
    * cleanup
    
    * style update
    
    * typescript
    
    * fix typescript issue
    
    * typescript did not like void
    
    * this code looks better
    
    * code coverage

---
+++ src/modules/colorpicker/colorpicker-slider.directive.ts
@@ -0,0 +1,95 @@
+// spell-checker:ignore Colorpicker
+import {
+  Directive,
+  Input,
+  Output,
+  ElementRef,
+  EventEmitter,
+  HostListener
+} from '@angular/core';
+import { SkyColorpickerChangeAxis } from './types/colorpicker-axis';
+
+@Directive({
+  selector: '[skyColorpickerSlider]'
+})
+export class SkyColorpickerSliderDirective {
+
+  @Output()
+  public newColorContrast = new EventEmitter<SkyColorpickerChangeAxis>();
+  @Input()
+  public skyColorpickerSlider: string;
+  @Input()
+  public color: string;
+  @Input()
+  public xAxis: number;
+  @Input()
+  public yAxis: number;
+
+  private listenerMove: any;
+  private listenerStop: any;
+
+  constructor(private el: ElementRef) {
+    this.listenerMove = (event: any) => { this.move(event); };
+    this.listenerStop = () => { this.stop(); };
+  }
+
+  public setCursor(event: any) {
+    let height = this.el.nativeElement.offsetHeight;
+    let width = this.el.nativeElement.offsetWidth;
+    let xAxis = Math.max(0, Math.min(this.getX(event), width));
+    let yAxis = Math.max(0, Math.min(this.getY(event), height));
+    if (this.xAxis !== undefined && this.yAxis !== undefined) {
+      this.newColorContrast.emit({
+        xCoordinate: xAxis / width,
+        yCoordinate: (1 - yAxis / height),
+        xAxis: this.xAxis,
+        yAxis: this.yAxis
+      });
+    } else {
+      this.newColorContrast.emit({ xCoordinate: xAxis / width, maxRange: this.xAxis });
+    }
+    /* // No vertical bars
+     if (this.xAxis === undefined && this.yAxis !== undefined) {
+          this.newColorContrast.emit({ yCoordinate: yAxis / height, maxRange: this.yAxis });
+    } */
+
+  }
+
+  public move(event: any) {
+    event.preventDefault();
+    this.setCursor(event);
+  }
+  @HostListener('touchstart', ['$event'])
+  @HostListener('mousedown', ['$event'])
+  public start(event: MouseEvent) {
+    this.setCursor(event);
+    document.addEventListener('mousemove', this.listenerMove);
+    document.addEventListener('touchmove', this.listenerMove);
+    document.addEventListener('mouseup', this.listenerStop);
+    document.addEventListener('touchend', this.listenerStop);
+  }
+
+  public stop() {
+    document.removeEventListener('mousemove', this.listenerMove);
+    document.removeEventListener('touchmove', this.listenerMove);
+    document.removeEventListener('mouseup', this.listenerStop);
+    document.removeEventListener('touchend', this.listenerStop);
+  }
+
+  public getX(event: any): number {
+    return (
+      /* Ignoring event.touches as tests are not run on a touch device. */
+      /* istanbul ignore next */
+      event.pageX !== undefined ? event.pageX : event.touches[0].pageX)
+      - this.el.nativeElement.getBoundingClientRect().left
+      - window.pageXOffset;
+  }
+  public getY(event: any): number {
+    return (
+      /* Ignoring event.touches as tests are not run on a touch device. */
+      /* istanbul ignore next */
+      event.pageY !== undefined ? event.pageY : event.touches[0].pageY)
+      - this.el.nativeElement.getBoundingClientRect().top
+      - window.pageYOffset;
+  }
+}
---
+++ src/modules/colorpicker/index.ts
@@ -0,0 +1,4 @@
+// spell-checker:ignore Colorpicker
+export { SkyColorpickerComponent } from './colorpicker.component';
+export { SkyColorpickerModule } from './colorpicker.module';
+export { SkyColorpickerOutput } from './types/colorpicker-output';
