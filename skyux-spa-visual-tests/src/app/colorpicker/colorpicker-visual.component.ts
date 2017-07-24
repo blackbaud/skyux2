19377bf35ac7425d4155835fbc6bf55fd77eadd9
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-07-24 11:02:11 -0400

    Add visual test for z-index change

--- skyux-spa-visual-tests/src/app/colorpicker/colorpicker-visual.component.ts
+++ skyux-spa-visual-tests/src/app/colorpicker/colorpicker-visual.component.ts
@@ -23,4 +23,22 @@ export class ColorpickerVisualComponent {
     '#68AFEF'
   ];
 
+  public color2: any;
+  public selectedColor2: string = '#2889e5';
+  public selectedOutputFormat2: string = 'rgba';
+  public presetColors2 = [
+    '#333333',
+    '#888888',
+    '#EFEFEF',
+    '#FFF',
+    '#BD4040',
+    '#617FC2',
+    '#60AC68',
+    '#3486BA',
+    '#E87134',
+    '#DA9C9C',
+    '#A1B1A7',
+    '#68AFEF'
+  ];
+
 }
