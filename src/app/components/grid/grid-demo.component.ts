b374e92dcfcb338c77a422231d0842201a6ca395
anya.cranmer <anya.cranmer@blackbaud.com>
2017-08-02 11:24:13 -0500

    cleanup demo

--- src/app/components/grid/grid-demo.component.ts
+++ src/app/components/grid/grid-demo.component.ts
@@ -8,20 +8,20 @@ import { ListSortFieldSelectorModel } from '../../../core';
 export class SkyGridDemoComponent {
   public items: any[] = [
     { id: '1', column1: 101, column2: 'Apple', column3: 'Anne eats apples', composite: 'Comp A',
-      openEmails: 1, clickThrough: 10, description: 'anne apple email'},
+      consumptionCount: 1, initialCount: 10},
     { id: '2', column1: 202, column2: 'Banana', column3: 'Ben eats bananas', composite: 'Comp B',
-      openEmails: 2, clickThrough: 20, description: 'ben Banana email' },
+      consumptionCount: 2, initialCount: 20},
     { id: '3', column1: 303, column2: 'Pear', column3: 'Patty eats pears', composite: 'Comp C',
-      openEmails: 3, clickThrough: 30, description: 'patty pear email' },
+      consumptionCount: 3, initialCount: 30},
     { id: '4', column1: 404, column2: 'Grape', column3: 'George eats grapes', composite: 'Comp D',
-      openEmails: 4, clickThrough: 40, description: 'george grape email' },
-    { id: '5', column1: 505, column2: 'Banana', column3: 'Becky eats bananas',
-      composite: 'Comp E', openEmails: 5, clickThrough: 50, description: 'becky banana email' },
+      consumptionCount: 4, initialCount: 40},
+    { id: '5', column1: 505, column2: 'Banana', column3: 'Becky eats bananas', composite: 'Comp E',
+      consumptionCount: 5, initialCount: 50},
     { id: '6', column1: 606, column2: 'Lemon', column3: 'Larry eats lemons', composite: 'Comp F',
-      openEmails: 6, clickThrough: 60, description: 'larry lemon email' },
+      consumptionCount: 6, initialCount: 60},
     { id: '7', column1: 707, column2: 'Strawberry', column3: 'Sally eats strawberries',
       composite: 'Comp G',
-      openEmails: 7, clickThrough: 70, description: 'sally strawberry email' }
+      consumptionCount: 7, initialCount: 70}
   ];
 
   public sortChanged(activeSort: ListSortFieldSelectorModel) {
