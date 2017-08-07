a5182efcf2473da09fcae2aa21c2d96859342ba6
Adam Hickey <adam.hickey@blackbaud.com>
2017-08-07 12:36:55 -0400

    Add media query for mobile.

--- src/modules/vertical-tabset/vertical-tabset.component.ts
+++ src/modules/vertical-tabset/vertical-tabset.component.ts
@@ -2,16 +2,18 @@ import {
   Component,
   ElementRef,
   ViewChild,
-  AfterContentInit
+  AfterContentInit,
+  Input
 } from '@angular/core';
 
+import { SkyResourcesService } from './../resources/resources.service';
 import { SkyVerticalTabsetService } from './vertical-tabset.service';
 
 @Component({
   selector: 'sky-vertical-tabset',
   templateUrl: './vertical-tabset.component.html',
   styleUrls: ['./vertical-tabset.component.scss'],
-  providers: [SkyVerticalTabsetService]
+  providers: [SkyVerticalTabsetService, SkyResourcesService]
 })
 export class SkyVerticalTabsetComponent implements AfterContentInit {
 
@@ -21,7 +23,12 @@ export class SkyVerticalTabsetComponent implements AfterContentInit {
   @ViewChild('skySideContent')
   public content: ElementRef;
 
-  constructor(private tabService: SkyVerticalTabsetService) {}
+  @Input()
+  public showTabsText: string = this.resources.getString('vertical_tabs_show_tabs_text');
+
+  constructor(
+    private tabService: SkyVerticalTabsetService,
+    private resources: SkyResourcesService) {}
 
   public ngAfterContentInit() {
     // move content from sub tabs to the right
