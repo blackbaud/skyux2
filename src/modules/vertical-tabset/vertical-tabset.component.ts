4be9bb9bd428c8b50d9e1ffbc16a81d9e409b59c
Adam Hickey <adam.hickey@blackbaud.com>
2017-08-07 13:04:01 -0400

    Hide and show tabs on mobile.

--- src/modules/vertical-tabset/vertical-tabset.component.ts
+++ src/modules/vertical-tabset/vertical-tabset.component.ts
@@ -8,12 +8,14 @@ import {
 
 import { SkyResourcesService } from './../resources/resources.service';
 import { SkyVerticalTabsetService } from './vertical-tabset.service';
+import { SkyMediaQueryService } from './../media-queries/media-query.service';
+import { SkyMediaBreakpoints } from 'src/modules/media-queries';
 
 @Component({
   selector: 'sky-vertical-tabset',
   templateUrl: './vertical-tabset.component.html',
   styleUrls: ['./vertical-tabset.component.scss'],
-  providers: [SkyVerticalTabsetService, SkyResourcesService]
+  providers: [SkyVerticalTabsetService, SkyResourcesService, SkyMediaQueryService]
 })
 export class SkyVerticalTabsetComponent implements AfterContentInit {
 
@@ -26,9 +28,12 @@ export class SkyVerticalTabsetComponent implements AfterContentInit {
   @Input()
   public showTabsText: string = this.resources.getString('vertical_tabs_show_tabs_text');
 
+  private _tabsVisible: boolean;
+
   constructor(
     private tabService: SkyVerticalTabsetService,
-    private resources: SkyResourcesService) {}
+    private resources: SkyResourcesService,
+    private mediaQueryService: SkyMediaQueryService) {}
 
   public ngAfterContentInit() {
     // move content from sub tabs to the right
@@ -37,5 +42,21 @@ export class SkyVerticalTabsetComponent implements AfterContentInit {
         this.content.nativeElement.appendChild(tab.tabContent.nativeElement);
       });
     }
+  }
+
+  public tabsVisible(): boolean {
+    return !this.isMobile() || this._tabsVisible;
+  }
+
+  public contentVisible(): boolean {
+    return !this.isMobile() || !this._tabsVisible;
+  }
+
+  public isMobile(): boolean {
+    return this.mediaQueryService.current === SkyMediaBreakpoints.xs;
+  }
+
+  public showTabs() {
+    this._tabsVisible = true;
   }
 }
