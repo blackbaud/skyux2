b7920db51190bea47df87cf6749e73be8987bec9
Blackbaud-AnyaCranmer <anya.cranmer@blackbaud.com>
2017-08-28 11:01:27 -0500

    List row details (#1)
    * list dropdown
    
    * list details
    
    * fix linting error
    
    * add list tests

--- src/app/components/list/list-demo.component.ts
+++ src/app/components/list/list-demo.component.ts
@@ -7,13 +7,20 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 })
 export class SkyListDemoComponent {
   public items: BehaviorSubject<any> = new BehaviorSubject([
-    { id: '1', column1: 101, column2: 'Apple', column3: 'Anne eats apples' },
-    { id: '2', column1: 202, column2: 'Banana', column3: 'Ben eats bananas' },
-    { id: '3', column1: 303, column2: 'Pear', column3: 'Patty eats pears' },
-    { id: '4', column1: 404, column2: 'Grape', column3: 'George eats grapes' },
-    { id: '5', column1: 505, column2: 'Banana', column3: 'Becky eats bananas' },
-    { id: '6', column1: 606, column2: 'Lemon', column3: 'Larry eats lemons' },
-    { id: '7', column1: 707, column2: 'Strawberry', column3: 'Sally eats strawberries' }
+    { id: '1', column1: 101, column2: 'Apple', column3: 'Anne eats apples',
+      consumptionCount: 1, initialCount: 10},
+    { id: '2', column1: 202, column2: 'Banana', column3: 'Ben eats bananas',
+      consumptionCount: 2, initialCount: 20},
+    { id: '3', column1: 303, column2: 'Pear', column3: 'Patty eats pears',
+      consumptionCount: 3, initialCount: 30},
+    { id: '4', column1: 404, column2: 'Grape', column3: 'George eats grapes',
+      consumptionCount: 4, initialCount: 40},
+    { id: '5', column1: 505, column2: 'Banana', column3: 'Becky eats bananas',
+      consumptionCount: 5, initialCount: 50},
+    { id: '6', column1: 606, column2: 'Lemon', column3: 'Larry eats lemons',
+      consumptionCount: 6, initialCount: 60},
+    { id: '7', column1: 707, column2: 'Strawberry', column3: 'Sally eats strawberries',
+      consumptionCount: 7, initialCount: 70}
   ]);
 
   public changeData() {
