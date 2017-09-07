acc1dac998274f495fd0cafd27147b6809cf0e9a
Adam Hickey <adam.hickey@blackbaud.com>
2017-09-07 10:19:12 -0400

    Remove demo scss files.

--- src/app/components/sectioned-form/sectioned-form-address.component.ts
+++ src/app/components/sectioned-form/sectioned-form-address.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sky-sectioned-form-address',
-  templateUrl: './sectioned-form-address.component.html',
-  styleUrls: ['./sectioned-form-address.component.scss']
+  templateUrl: './sectioned-form-address.component.html'
 })
 export class SkySectionedFormAddressComponent {}
--- src/app/components/sectioned-form/sectioned-form-information.component.ts
+++ src/app/components/sectioned-form/sectioned-form-information.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sky-sectioned-form-information',
-  templateUrl: './sectioned-form-information.component.html',
-  styleUrls: ['./sectioned-form-information.component.scss']
+  templateUrl: './sectioned-form-information.component.html'
 })
 export class SkySectionedFormInformationComponent {
   public name: string = '';
   public id: string = '5324901';
-  public checked: boolean = false;
+  public nameRequired: boolean = false;
 }
