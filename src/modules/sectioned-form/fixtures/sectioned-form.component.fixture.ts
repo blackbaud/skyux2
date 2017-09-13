83e8faed46822b97f6a56478abdae402f0882151
Adam Hickey <adam.hickey@blackbaud.com>
2017-09-13 11:21:14 -0400

    Add tests for sectioned modal.

--- src/modules/sectioned-form/fixtures/sectioned-form.component.fixture.ts
+++ src/modules/sectioned-form/fixtures/sectioned-form.component.fixture.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SkySectionedFormComponent } from '../sectioned-form.component';
 
 @Component({
   selector: 'sky-sectioned-form-fixture',
   templateUrl: './sectioned-form.component.fixture.html'
 })
 export class SkySectionedFormFixtureComponent {
+
+  @ViewChild(SkySectionedFormComponent)
+  public sectionedForm: SkySectionedFormComponent;
+
   public activeTab: boolean = true;
 }
--- src/modules/sectioned-form/sectioned-form.component.spec.ts
+++ src/modules/sectioned-form/sectioned-form.component.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { SkySectionedFormFixturesModule } from './fixtures/sectioned-form-fixtures.module';
 import { SkySectionedFormFixtureComponent } from './fixtures/sectioned-form.component.fixture';
+import { TestUtility } from '../testing/testutility';
 
 function getVisibleContent(el: any) {
-  return el.querySelectorAll('#verticalTab:not(.sky-vertical-tab-hidden)');
+  return el.querySelectorAll('.sky-vertical-tab-content-pane:not(.sky-vertical-tab-hidden)');
+}
+
+function getActiveSection(el: any) {
+  return el.querySelectorAll('sky-sectioned-form-section .sky-vertical-tab-active');
 }
 
 describe('Sectioned form component', () => {
@@ -26,19 +31,84 @@ describe('Sectioned form component', () => {
     fixture.detectChanges();
 
     // check correct section tab is active
-    const activeSection =
-      el.querySelectorAll('sky-sectioned-form-section .sky-vertical-tab-active');
+    let activeSection = getActiveSection(el);
     expect(activeSection.length).toBe(1);
 
-    const heading = activeSection[0].querySelector('.sky-vertical-tab-heading');
+    let heading = activeSection[0].querySelector('.sky-vertical-tab-heading');
     expect(heading.textContent.trim()).toBe('Information 2');
 
-    const count = activeSection[0].querySelector('.sky-vertical-tab-count');
+    let count = activeSection[0].querySelector('.sky-vertical-tab-count');
     expect(count.textContent.trim()).toBe('(2)');
 
     // check correct section content is displayed
-    const content = getVisibleContent(el);
+    let content = getVisibleContent(el);
     expect(content.length).toBe(1);
     expect(content[0].textContent.trim()).toBe('information 2');
+  });
+
+  it('clicking tab should show content', () => {
+    let fixture = createTestComponent();
+    let el = fixture.nativeElement;
+
+    fixture.detectChanges();
+
+    // click first tab
+    let firstTab = el.querySelectorAll('.sky-vertical-tab');
+    firstTab[0].click();
+
+    fixture.detectChanges();
+
+    // check correct section tab is active
+    let activeSection = getActiveSection(el);
+    expect(activeSection.length).toBe(1);
+
+    let heading = activeSection[0].querySelector('.sky-vertical-tab-heading');
+    expect(heading.textContent.trim()).toBe('Information 1');
+
+    let count = activeSection[0].querySelector('.sky-vertical-tab-count');
+    // tslint:disable-next-line:no-null-keyword
+    expect(count).toBe(null);
+
+    // check correct section content is displayed
+    let content = getVisibleContent(el);
+    expect(content.length).toBe(1);
+    expect(content[0].textContent.trim()).toBe('information 1');
+  });
+
+  it('section should respect required field change', () => {
+    let fixture = createTestComponent();
+    let cmp = fixture.componentInstance;
+    let el = fixture.nativeElement as HTMLElement;
+
+    fixture.detectChanges();
+
+    // check section is not required
+    let tabs = el.querySelectorAll('sky-vertical-tab');
+    expect(tabs.length).toBe(2);
+
+    let activeTab = tabs[1];
+    expect(activeTab.classList.contains('sky-tab-field-required')).toBe(false);
+
+    // mark required
+    cmp.sectionedForm.setRequired(true);
+    fixture.detectChanges();
+
+    let requiredTabs = cmp.sectionedForm.sections.filter(section => section.fieldRequired);
+    expect(requiredTabs.length).toBe(1);
+
+    // check section is required
+    expect(activeTab.classList.contains('sky-tab-field-required')).toBe(true);
+  });
+
+  it('active index should be raised when tab changed', () => {
+    let fixture = createTestComponent();
+    let cmp = fixture.componentInstance;
+    let el = fixture.nativeElement;
+
+    fixture.detectChanges();
+
+    let firstTab = el.querySelectorAll('.sky-vertical-tab');
+    firstTab[0].click();
+    expect(cmp.sectionedForm.indexChanged.getValue()).toBe(0);
   });
 });
