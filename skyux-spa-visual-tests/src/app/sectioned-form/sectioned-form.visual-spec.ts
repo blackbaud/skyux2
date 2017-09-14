05a9a0e2e0ea704c467f7fc9802609d47488d5c1
Adam Hickey <adam.hickey@blackbaud.com>
2017-09-14 11:43:11 -0400

    Update test.

--- skyux-spa-visual-tests/src/app/sectioned-form/sectioned-form.visual-spec.ts
+++ skyux-spa-visual-tests/src/app/sectioned-form/sectioned-form.visual-spec.ts
@@ -1,16 +1,58 @@
 import { SkyVisualTest } from '../../../config/utils/visual-test-commands';
+import { browser } from 'protractor';
+// import { element, by, browser } from 'protractor';
 
 describe('Sectioned form', () => {
   it('should match previous sectioned form screenshot', () => {
+    browser.sleep(8000);
+
     return SkyVisualTest.setupTest('sectioned-form')
     .then(() => {
+
+      browser.sleep(8000);
+
       return SkyVisualTest.compareScreenshot({
         screenshotName: 'sectioned-form',
         selector: '#screenshot-sectioned-form'
       });
     });
   });
-});
 
-// switch to first tab
-// switch to second tab
+  // it('should switch to first tab on click', () => {
+  //   return SkyVisualTest.setupTest('sectioned-form')
+  //   .then(() => {
+
+  //     let tabs = element.all(by.css('sky-vertical-tab'));
+  //     browser.wait(function() { return browser.isElementPresent(tabs); }, 8000);
+
+  //     // click first tab
+  //     tabs[0].click();
+
+  //     browser.sleep(1000);
+
+  //     return SkyVisualTest.compareScreenshot({
+  //       screenshotName: 'sectioned-form-first',
+  //       selector: '#screenshot-sectioned-form'
+  //     });
+  //   });
+  // });
+
+  // it('should switch to second tab on click', () => {
+  //   return SkyVisualTest.setupTest('sectioned-form')
+  //   .then(() => {
+
+  //     let tabs = element.all(by.css('sky-vertical-tab'));
+  //     browser.wait(function() { return browser.isElementPresent(tabs); }, 8000);
+
+  //     // click first tab
+  //     tabs[0].click();
+
+  //     browser.sleep(1000);
+
+  //     return SkyVisualTest.compareScreenshot({
+  //       screenshotName: 'sectioned-form-second',
+  //       selector: '#screenshot-sectioned-form'
+  //     });
+  //   });
+  // });
+});
