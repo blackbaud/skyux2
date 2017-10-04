171b7d708eb64bd708d9359903c1a8583bad7e2a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-10-04 15:17:57 -0400

    Fixed bug with click events

--- skyux-spa-visual-tests/src/app/error/error.visual-spec.ts
+++ skyux-spa-visual-tests/src/app/error/error.visual-spec.ts
@@ -18,32 +18,30 @@ describe('Error', () => {
 
   it('should match previous error modal form screenshot', () => {
     return SkyVisualTest.setupTest('error')
-    .then(() => {
-      element(by.css('.sky-test-error-modal')).click();
-      SkyVisualTest.moveCursorOffScreen();
-      return SkyVisualTest.compareScreenshot({
-        screenshotName: 'error-modal-form',
-        selector: '.sky-modal',
-        checkAccessibility: true
-      }).then(() => {
-        element(by.css('.sky-error-modal-close .sky-btn-primary')).click();
-      });
-    });
+      .then(() => element(by.css('.sky-test-error-modal')).click() as any)
+      .then(() => {
+        SkyVisualTest.moveCursorOffScreen();
+        return SkyVisualTest.compareScreenshot({
+          screenshotName: 'error-modal-form',
+          selector: '.sky-modal',
+          checkAccessibility: true
+        });
+      })
+      .then(() => element(by.css('.sky-error-modal-close .sky-btn-primary')).click() as any);
   });
 
   it('should match previous error modal with long description form screenshot', () => {
     return SkyVisualTest.setupTest('error')
-    .then(() => {
-      element(by.css('.sky-test-error-modal-long-description')).click();
-      SkyVisualTest.moveCursorOffScreen();
-      return SkyVisualTest.compareScreenshot({
-        screenshotName: 'error-modal-form-long-description',
-        selector: '.sky-modal',
-        checkAccessibility: true
-      }).then(() => {
-        element(by.css('.sky-error-modal-close .sky-btn-primary')).click();
-      });
-    });
+      .then(() => element(by.css('.sky-test-error-modal-long-description')).click() as any)
+      .then(() => {
+        SkyVisualTest.moveCursorOffScreen();
+        return SkyVisualTest.compareScreenshot({
+          screenshotName: 'error-modal-form-long-description',
+          selector: '.sky-modal',
+          checkAccessibility: true
+        });
+      })
+      .then(() => element(by.css('.sky-error-modal-close .sky-btn-primary')).click() as any);
 
   });
 });
