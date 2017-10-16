330d224bb93c509247670fa7f6f694577a01862f
Steve Brush <steve.brush@blackbaud.com>
2017-10-16 15:30:43 -0400

    Refactored visual tests (#1155)
    * Initial commit
    
    * Removed useless dependencies
    
    * Update start-visual.js
    
    * Fixed bug with CI config
    
    * Fixed a11y test
    
    * Removed useless timeouts
    
    * Removing wait for angular
    
    * Fixed bug with click events
    
    * Removed sleep
    
    * Removed duplicate code
    
    * Removed lambda functions
    
    * Re-added onPrepare for ci

--- skyux-spa-visual-tests/config/local.visual.conf.js
+++ skyux-spa-visual-tests/config/local.visual.conf.js
@@ -1,49 +1,18 @@
 /*jshint jasmine: true, node: true */
-/* global browser */
 'use strict';
 
-let builderUtils =  require('@blackbaud/skyux-builder/utils/host-utils');
+const config = require('./shared.visual.conf.js');
 
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
-
-var config = require('./shared.visual.conf.js');
-config.onPrepare = function () {
-  jasmine.getEnv().addReporter(new SpecReporter());
-  const PixDiff = require('pix-diff');
-  browser.pixDiff = new PixDiff(
-    {
-      basePath: 'screenshots-baseline-local/',
-      diffPath: 'screenshots-diff-local/',
-      baseline: true,
-      width: 1000,
-      height: 800
-    }
-  );
-
-  browser.skyVisualTestOptions = {
-    createdPath: 'screenshots-created-local/',
-    createdPathDiff: 'screenshots-created-diff-local/'
-  };
-
-  var destination = builderUtils.resolve(
-    '/',
-    browser.params.localUrl,
-    JSON.parse(browser.params.chunks),
-    JSON.parse(browser.params.skyPagesConfig)
-  );
-
-  return browser.get(destination);
-
-};
-
-config.capabilities =  {
+config.capabilities = {
   'browserName': 'chrome',
   'chromeOptions': {
-    'args': ['--ignore-certificate-errors']
+    'args': [
+      '--disable-extensions',
+      '--ignore-certificate-errors'
+    ]
   }
 };
 
 config.directConnect = true;
 
 exports.config = config;
-
