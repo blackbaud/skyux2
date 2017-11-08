675566499fa2073edf01c02964c84f147ea570ad
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-08 15:17:18 -0500

    Using build matrix

--- scripts/travis-script-build.sh
+++ scripts/travis-script-build.sh
@@ -4,8 +4,7 @@ set -e
 # Necessary to stop pull requests from forks from running outside of Savage
 # Override default of `npm test`
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  npm run ci
-  ./scripts/visual-baseline.sh
+  skyux build
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
