1227f3c927cd169295d465f4fd113768754ed769
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-08 10:42:45 -0500

    Added aria label

--- src/modules/action-button/action-button.component.ts
+++ src/modules/action-button/action-button.component.ts
@@ -4,12 +4,16 @@ import {
   Output
 } from '@angular/core';
 
+let skyActionButtonUniqueIdentifier = 0;
+
 @Component({
   selector: 'sky-action-button',
   styleUrls: ['./action-button.component.scss'],
   templateUrl: './action-button.component.html'
 })
 export class SkyActionButtonComponent {
+  public componentId = `sky-action-button-${skyActionButtonUniqueIdentifier++}`;
+
   @Output()
   public actionClick = new EventEmitter<any>();
 
