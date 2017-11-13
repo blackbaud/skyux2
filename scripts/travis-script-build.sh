09cf05f3e435e7eda8cbb62b778cc7931f6b51c8
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-13 13:57:09 -0500

    Removing output

--- scripts/travis-script-build.sh
+++ scripts/travis-script-build.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  skyux build --output-keep-alive
+  skyux build
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
