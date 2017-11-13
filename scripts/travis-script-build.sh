ef1c7f5fad8beab006527f28b2aa629a32577df1
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-13 15:27:53 -0500

    Added max old space size

--- scripts/travis-script-build.sh
+++ scripts/travis-script-build.sh
@@ -3,7 +3,7 @@ set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  skyux build --output-keep-alive
+  node --max-old-space-size=4096 `which npm` build:skyux
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
