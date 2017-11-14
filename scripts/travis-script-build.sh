3aed5c3b6f41bef2559c69d26fe7af7a09a2ed2a
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-14 12:57:41 -0500

    Increased memory needed

--- scripts/travis-script-build.sh
+++ scripts/travis-script-build.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   # Building SKY UX requires a lot of memory:
   # https://github.com/npm/npm/issues/12238#issuecomment-301645764
-  node --max-old-space-size=4096 `which npm` run build:skyux
+  node --inspect --max-old-space-size=10240 `which npm` run build:skyux
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
