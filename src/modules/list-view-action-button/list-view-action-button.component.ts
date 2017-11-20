5e08427aac039bf65933619da1fb72f84082dcf6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-20 17:56:53 -0500

    Added styles

--- src/modules/list-view-action-button/list-view-action-button.component.ts
+++ src/modules/list-view-action-button/list-view-action-button.component.ts
@@ -1,46 +1,18 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  forwardRef,
   Input
 } from '@angular/core';
-
-import {
-  ListViewComponent
-} from '../list/list-view.component';
-
-import {
-  // ListItemModel,
-  // ListSortFieldSelectorModel,
-  // ListSearchModel,
-  // ListStateDispatcher,
-  ListState
-} from '../list/state';
 
 @Component({
   selector: 'sky-list-view-action-button',
   templateUrl: './list-view-action-button.component.html',
   styleUrls: ['./list-view-action-button.component.scss'],
-  providers: [
-    /* tslint:disable:no-forward-ref */
-    {
-      provide: ListViewComponent,
-      useExisting: forwardRef(() => SkyListViewActionButtonComponent)
-    }
-    /* tslint:enable */
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SkyListViewActionButtonComponent extends ListViewComponent {
+export class SkyListViewActionButtonComponent {
   @Input()
   public items: any[];
 
-  constructor(
-    state: ListState
-    // private dispatcher: ListStateDispatcher,
-    // public gridState: GridState,
-    // public gridDispatcher: GridStateDispatcher
-  ) {
-    super(state, 'Action Button View');
-  }
+  constructor() { }
 }
