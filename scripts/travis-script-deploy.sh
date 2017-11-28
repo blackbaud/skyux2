7eb398c8393aa4afd0e53e2b982305dcd0cae461
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-28 09:53:37 -0500

    Added different message

--- scripts/travis-script-deploy.sh
+++ scripts/travis-script-deploy.sh
@@ -2,9 +2,13 @@
 set -e
 
 # Necessary to stop pull requests from forks from running outside of Savage
-if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" && -n "$TRAVIS_TAG" ]]; then
-  npm run build
-  bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
+if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
+  if [[ -n "$TRAVIS_TAG" ]]; then
+    npm run build
+    bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
+  else
+    echo -e "Git tag not found. Ignoring deployment."
+  fi
 else
   echo -e "Pull requests from forks are run via Savage."
 fi
