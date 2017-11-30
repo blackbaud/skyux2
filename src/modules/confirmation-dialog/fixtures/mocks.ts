a8c7c1a4610de53036c41d78afe1e3c08d67e8c6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-30 12:25:57 -0500

    Updated mock

--- src/modules/confirmation-dialog/fixtures/mocks.ts
+++ src/modules/confirmation-dialog/fixtures/mocks.ts
@@ -1,11 +1,15 @@
 import { EventEmitter } from '@angular/core';
+
 import { SkyModalService } from '../../modal/modal.service';
 import { SkyModalInstance } from '../../modal/modal-instance';
-import { SkyModalConfigurationInterface as IConfig } from '../../modal/modal.interface';
 
 export class SkyModalInstanceMock extends SkyModalInstance {
   public closed = new EventEmitter<any>();
-  public close(result?: any) { this.closed.emit({ data: result }); }
+  public close(result?: any) {
+    this.closed.emit({
+      data: result
+    });
+  }
   public save() {}
 }
 
@@ -23,7 +27,7 @@ export interface OpenParameters {
 export class MockModalService extends SkyModalService {
   public openCalls: OpenParameters[] = [];
 
-  public open(component: any, config?: IConfig): SkyModalInstanceMock {
+  public open(component: any, config?: any): SkyModalInstanceMock {
     this.openCalls.push({component: component, config: config });
 
     return new SkyModalInstanceMock();
