77caf12bb08663b0e1e3a6a3a9cc88ca01c4b973
Steve Brush <steve.brush@blackbaud.com>
2017-12-01 12:05:51 -0500

    URL validation module (#1334)

---
+++ src/app/components/url-validation/url-validation-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-url-validation-demo',
+  templateUrl: './url-validation-demo.component.html'
+})
+export class SkyUrlValidationDemoComponent {
+  public url: string;
+}
