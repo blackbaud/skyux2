e46a050537a26dd0f0f58416ca49dde4852cb22e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-02-13 19:57:15 -0500

    Allow for async popover refs

--- src/modules/popover/popover.directive.ts
+++ src/modules/popover/popover.directive.ts
@@ -1,9 +1,15 @@
 import {
   Directive,
   ElementRef,
-  HostListener,
-  Input
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges
 } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import { Subject } from 'rxjs/Subject';
 
 import {
   SkyWindowRefService
@@ -20,7 +26,7 @@ import { SkyPopoverComponent } from './popover.component';
 @Directive({
   selector: '[skyPopover]'
 })
-export class SkyPopoverDirective {
+export class SkyPopoverDirective implements OnChanges, OnDestroy {
   @Input()
   public skyPopover: SkyPopoverComponent;
 
@@ -33,63 +39,34 @@ export class SkyPopoverDirective {
   @Input()
   public skyPopoverTrigger: SkyPopoverTrigger = 'click';
 
+  private idled = new Subject<boolean>();
+
   constructor(
     private elementRef: ElementRef,
     private windowRef: SkyWindowRefService
   ) { }
 
-  @HostListener('keyup', ['$event'])
-  public onDocumentKeyUp(event: KeyboardEvent): void {
-    const key = event.key.toLowerCase();
-    if (key === 'escape' && this.skyPopover.isOpen) {
-      event.stopPropagation();
-      event.preventDefault();
-      this.closePopover();
-      this.elementRef.nativeElement.focus();
+  public ngOnChanges(changes: SimpleChanges) {
+    /* istanbul ignore else */
+    if (changes.skyPopover) {
+      this.removeEventListeners();
+      if (changes.skyPopover.currentValue !== undefined) {
+        this.addEventListeners();
+      }
     }
   }
 
-  @HostListener('click', ['$event'])
-  public togglePopover(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
+  public ngOnDestroy(): void {
+    this.removeEventListeners();
+  }
 
-    if (this.skyPopover.isOpen) {
+  public togglePopover() {
+    if (this.isPopoverOpen()) {
       this.closePopover();
       return;
     }
 
     this.positionPopover();
-  }
-
-  @HostListener('mouseenter', ['$event'])
-  public onMouseEnter(event: MouseEvent) {
-    this.skyPopover.isMouseEnter = true;
-    if (this.skyPopoverTrigger === 'mouseenter') {
-      event.preventDefault();
-      this.positionPopover();
-    }
-  }
-
-  @HostListener('mouseleave', ['$event'])
-  public onMouseLeave(event: MouseEvent) {
-    this.skyPopover.isMouseEnter = false;
-
-    if (this.skyPopoverTrigger === 'mouseenter') {
-      event.preventDefault();
-
-      // Give the popover a chance to set its isMouseEnter flag before checking to see
-      // if it should be closed.
-      this.windowRef.getWindow().setTimeout(() => {
-        if (this.skyPopover.isOpen) {
-          if (this.skyPopover.isMouseEnter) {
-            this.skyPopover.markForCloseOnMouseLeave();
-          } else {
-            this.closePopover();
-          }
-        }
-      });
-    }
   }
 
   private positionPopover() {
@@ -102,5 +79,75 @@ export class SkyPopoverDirective {
 
   private closePopover() {
     this.skyPopover.close();
+  }
+
+  private isPopoverOpen(): boolean {
+    return (this.skyPopover && this.skyPopover.isOpen);
+  }
+
+  private addEventListeners() {
+    const element = this.elementRef.nativeElement;
+
+    Observable
+      .fromEvent(element, 'keyup')
+      .takeUntil(this.idled)
+      .subscribe((event: KeyboardEvent) => {
+        const key = event.key.toLowerCase();
+        if (key === 'escape' && this.isPopoverOpen()) {
+          event.stopPropagation();
+          event.preventDefault();
+          this.closePopover();
+          this.elementRef.nativeElement.focus();
+        }
+      });
+
+    Observable
+      .fromEvent(element, 'click')
+      .takeUntil(this.idled)
+      .subscribe((event: MouseEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.togglePopover();
+      });
+
+    Observable
+      .fromEvent(element, 'mouseenter')
+      .takeUntil(this.idled)
+      .subscribe((event: MouseEvent) => {
+        this.skyPopover.isMouseEnter = true;
+        if (this.skyPopoverTrigger === 'mouseenter') {
+          event.preventDefault();
+          this.positionPopover();
+        }
+      });
+
+    Observable
+      .fromEvent(element, 'mouseleave')
+      .takeUntil(this.idled)
+      .subscribe((event: MouseEvent) => {
+        this.skyPopover.isMouseEnter = false;
+
+        if (this.skyPopoverTrigger === 'mouseenter') {
+          event.preventDefault();
+
+          // Give the popover a chance to set its isMouseEnter flag before checking to see
+          // if it should be closed.
+          this.windowRef.getWindow().setTimeout(() => {
+            if (this.isPopoverOpen()) {
+              if (this.skyPopover.isMouseEnter) {
+                this.skyPopover.markForCloseOnMouseLeave();
+              } else {
+                this.closePopover();
+              }
+            }
+          });
+        }
+      });
+  }
+
+  private removeEventListeners() {
+    this.idled.next(true);
+    this.idled.unsubscribe();
+    this.idled = new Subject<boolean>();
   }
 }
