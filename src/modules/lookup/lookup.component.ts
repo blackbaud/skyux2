63a32ca6a82d67e658fead2dea78f5599f158ea5
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-02-21 11:55:06 -0500

    Merged tokens branch

--- src/modules/lookup/lookup.component.ts
+++ src/modules/lookup/lookup.component.ts
@@ -140,7 +140,7 @@ export class SkyLookupComponent
     }
   }
 
-  public onTokensFocusEnd() {
+  public onTokensFocusIndexOverRange() {
     this.windowRef.getWindow().setTimeout(() => {
       this.focusInput();
     });
