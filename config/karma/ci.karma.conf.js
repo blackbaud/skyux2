e4288cc5b9d66c8a73511842bf87abd601ce7b9d
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-02-25 00:26:37 -0500

    Testing just ie

--- config/karma/ci.karma.conf.js
+++ config/karma/ci.karma.conf.js
@@ -16,49 +16,49 @@ module.exports = function (config) {
       browser: 'edge',
       os: 'Windows',
       os_version: '10'
-    },
-    bs_windows_chrome_latest: {
-      base: 'BrowserStack',
-      browser: 'chrome',
-      os: 'Windows',
-      os_version: '8.1'
-    },
-    bs_windows_firefox_latest: {
-      base: 'BrowserStack',
-      browser: 'firefox',
-      os: 'Windows',
-      os_version: '8.1'
-    },
-    bs_osx_safari_latest: {
-      base: 'BrowserStack',
-      browser: 'safari',
-      os: 'OS X',
-      os_version: 'Sierra'
-    },
-    bs_osx_chrome_latest: {
-      base: 'BrowserStack',
-      browser: 'chrome',
-      os: 'OS X',
-      os_version: 'Sierra'
-    },
-    bs_osx_firefox_latest: {
-      base: 'BrowserStack',
-      browser: 'firefox',
-      os: 'OS X',
-      os_version: 'Sierra'
     }
+    // bs_windows_chrome_latest: {
+    //   base: 'BrowserStack',
+    //   browser: 'chrome',
+    //   os: 'Windows',
+    //   os_version: '8.1'
+    // },
+    // bs_windows_firefox_latest: {
+    //   base: 'BrowserStack',
+    //   browser: 'firefox',
+    //   os: 'Windows',
+    //   os_version: '8.1'
+    // },
+    // bs_osx_safari_latest: {
+    //   base: 'BrowserStack',
+    //   browser: 'safari',
+    //   os: 'OS X',
+    //   os_version: 'Sierra'
+    // },
+    // bs_osx_chrome_latest: {
+    //   base: 'BrowserStack',
+    //   browser: 'chrome',
+    //   os: 'OS X',
+    //   os_version: 'Sierra'
+    // },
+    // bs_osx_firefox_latest: {
+    //   base: 'BrowserStack',
+    //   browser: 'firefox',
+    //   os: 'OS X',
+    //   os_version: 'Sierra'
+    // }
   };
 
   config.set({
     browsers: Object.keys(customLaunchers),
     customLaunchers,
-    browserDisconnectTimeout: 6e5,
+    browserDisconnectTimeout: 3e5,
     browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 6e5,
-    captureTimeout: 6e5,
+    browserNoActivityTimeout: 3e5,
+    captureTimeout: 3e5,
     browserStack: {
       port: 9876,
-      pollingTimeout: 600000
+      pollingTimeout: 10000
     }
   });
 };
