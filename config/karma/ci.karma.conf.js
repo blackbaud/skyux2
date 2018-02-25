0eb452c5402582d74374755f09af3db45fae6065
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-02-25 07:59:35 -0500

    Readded all browsers

--- config/karma/ci.karma.conf.js
+++ config/karma/ci.karma.conf.js
@@ -16,37 +16,37 @@ module.exports = function (config) {
       browser: 'edge',
       os: 'Windows',
       os_version: '10'
+    },
+    bs_windows_chrome_latest: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+    bs_windows_firefox_latest: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+    bs_osx_safari_latest: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      os: 'OS X',
+      os_version: 'Sierra'
+    },
+    bs_osx_chrome_latest: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'OS X',
+      os_version: 'Sierra'
+    },
+    bs_osx_firefox_latest: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      os: 'OS X',
+      os_version: 'Sierra'
     }
-    // bs_windows_chrome_latest: {
-    //   base: 'BrowserStack',
-    //   browser: 'chrome',
-    //   os: 'Windows',
-    //   os_version: '8.1'
-    // },
-    // bs_windows_firefox_latest: {
-    //   base: 'BrowserStack',
-    //   browser: 'firefox',
-    //   os: 'Windows',
-    //   os_version: '8.1'
-    // },
-    // bs_osx_safari_latest: {
-    //   base: 'BrowserStack',
-    //   browser: 'safari',
-    //   os: 'OS X',
-    //   os_version: 'Sierra'
-    // },
-    // bs_osx_chrome_latest: {
-    //   base: 'BrowserStack',
-    //   browser: 'chrome',
-    //   os: 'OS X',
-    //   os_version: 'Sierra'
-    // },
-    // bs_osx_firefox_latest: {
-    //   base: 'BrowserStack',
-    //   browser: 'firefox',
-    //   os: 'OS X',
-    //   os_version: 'Sierra'
-    // }
   };
 
   config.set({
