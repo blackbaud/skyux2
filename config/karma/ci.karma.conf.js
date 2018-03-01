f2fa6940616707127d2e0909b27c7ec7d0fa15f5
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-03-01 11:50:40 -0500

    Separated IE browserstack tests into separate travis matrix

--- config/karma/ci.karma.conf.js
+++ config/karma/ci.karma.conf.js
@@ -3,63 +3,51 @@ module.exports = function (config) {
 
   require('./shared.karma.conf')(config);
 
-  let customLaunchers = {
-      bs_windows_ie_11: {
-        base: 'BrowserStack',
-        browser: 'ie',
-        browser_version: '11.0',
-        os: 'Windows',
-        os_version: '10'
-      },
-      bs_windows_edge: {
-        base: 'BrowserStack',
-        browser: 'edge',
-        os: 'Windows',
-        os_version: '10'
-      },
-      bs_windows_chrome_latest: {
-        base: 'BrowserStack',
-        browser: 'chrome',
-        os: 'Windows',
-        os_version: '8.1'
-      },
-      bs_windows_firefox_latest: {
-        base: 'BrowserStack',
-        browser: 'firefox',
-        os: 'Windows',
-        os_version: '8.1'
-      },
-      bs_osx_safari_latest: {
-        base: 'BrowserStack',
-        browser: 'safari',
-        os: 'OS X',
-        os_version: 'Sierra'
-      },
-      bs_osx_chrome_latest: {
-        base: 'BrowserStack',
-        browser: 'chrome',
-        os: 'OS X',
-        os_version: 'Sierra'
-      },
-      bs_osx_firefox_latest: {
-        base: 'BrowserStack',
-        browser: 'firefox',
-        os: 'OS X',
-        os_version: 'Sierra'
-      }
-    };
+  const customLaunchers = {
+    bs_windows_edge: {
+      base: 'BrowserStack',
+      browser: 'edge',
+      os: 'Windows',
+      os_version: '10'
+    },
+    bs_windows_chrome_latest: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+    bs_windows_firefox_latest: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      os: 'Windows',
+      os_version: '8.1'
+    },
+    bs_osx_safari_latest: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      os: 'OS X',
+      os_version: 'Sierra'
+    },
+    bs_osx_chrome_latest: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'OS X',
+      os_version: 'Sierra'
+    },
+    bs_osx_firefox_latest: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      os: 'OS X',
+      os_version: 'Sierra'
+    }
+  };
 
   config.set({
     browsers: Object.keys(customLaunchers),
-    customLaunchers: customLaunchers,
-    browserDisconnectTimeout: 3e5,
-    browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 3e5,
-    captureTimeout: 3e5,
+    customLaunchers,
     browserStack: {
       port: 9876,
       pollingTimeout: 10000
     }
   });
-
 };
--- src/modules/modal/modal-configuration.spec.ts
+++ src/modules/modal/modal-configuration.spec.ts
@@ -1,17 +1,13 @@
 import { SkyModalConfiguration } from './modal-configuration';
 
 describe('Modal configuration', () => {
-  it('shout work initilize the configuration class', () => {
-
-    beforeEach(function () {
-      this.testService = new SkyModalConfiguration();
-      this.testService.fullPage = true;
-    });
-
-    it('should have name property fullPage', function () {
-      expect(this.testService.fullPage).toBe(true);
-    });
-
+  let testService: SkyModalConfiguration;
+  beforeEach(() => {
+    testService = new SkyModalConfiguration();
+    testService.fullPage = true;
   });
 
+  it('should have name property fullPage', () => {
+    expect(testService.fullPage).toBe(true);
+  });
 });
