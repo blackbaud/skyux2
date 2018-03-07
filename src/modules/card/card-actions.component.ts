3f4222e5e42abf20d58844d4f089cb13846d8c56
Conor Wright <Conor.Wright@blackbaud.com>
2018-03-07 17:17:05 -0500

    Resolving Card action bar still visable after hiding with *ngIf #1421

--- src/modules/card/card-actions.component.ts
+++ src/modules/card/card-actions.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sky-card-actions',
+  styleUrls: ['./card-actions.component.scss'],
   templateUrl: '../shared/simple-content.html'
 })
 export class SkyCardActionsComponent { }
