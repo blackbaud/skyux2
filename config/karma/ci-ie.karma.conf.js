c6a1058d27137dd9b6c000a15097d93f51633b7e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-03-20 12:20:22 -0400

    Created custom karma config for IE

--- config/karma/ci-ie.karma.conf.js
+++ config/karma/ci-ie.karma.conf.js
@@ -1,7 +1,10 @@
 module.exports = function (config) {
   'use strict';
 
-  require('./ci.karma.conf')(config);
+  const webpackConfig = require('../webpack/webpack.test');
+
+  // Remove instrument loader because it causes problems in IE 11:
+  webpackConfig.module.rules.pop();
 
   const customLaunchers = {
     bs_windows_ie_11: {
@@ -14,10 +17,39 @@ module.exports = function (config) {
   };
 
   config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    exclude: [],
+    files: [{
+      pattern: '../utils/spec-bundle.js',
+      watched: false
+    }, {
+      pattern: '../utils/spec-styles.js',
+      watched: false
+    }],
+    preprocessors: {
+      '../utils/spec-styles.js': ['webpack'],
+      '../utils/spec-bundle.js': ['webpack']
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    },
     browsers: Object.keys(customLaunchers),
-    customLaunchers
+    customLaunchers,
+    reporters: ['mocha'],
+    port: 9876,
+    colors: false,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    singleRun: true,
+    browserDisconnectTimeout: 3e5,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 3e5,
+    captureTimeout: 3e5,
+    browserStack: {
+      port: 9876,
+      pollingTimeout: 10000
+    }
   });
-
-  // Remove instrument loader because it causes problems in IE 11:
-  config.webpack.module.rules.pop();
 };
