a6758ca3a2d6e91223acbadcdac3a9b91f956d12
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-04-03 15:00:50 -0400

    Merged from refactor branch

--- src/modules/list-secondary-actions/list-secondary-actions.component.ts
+++ src/modules/list-secondary-actions/list-secondary-actions.component.ts
@@ -1,11 +1,16 @@
 import {
-  AfterContentInit,
+  AfterViewInit,
   Component,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  OnDestroy,
+  OnInit,
   TemplateRef,
   ViewChild
 } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 import {
   ListStateDispatcher,
@@ -21,21 +26,42 @@ import { SkyListSecondaryAction } from './list-secondary-action';
   styleUrls: ['./list-secondary-actions.component.scss'],
   providers: [
     SkyListSecondaryActionsService
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SkyListSecondaryActionsComponent implements AfterContentInit {
+export class SkyListSecondaryActionsComponent implements OnInit, AfterViewInit, OnDestroy {
   public dropdownHidden = false;
-  public actions = new BehaviorSubject<any[]>([]);
+  public actions: any[] = [];
 
   @ViewChild('secondaryActions')
   private secondaryActionsTemplate: TemplateRef<any>;
 
+  private ngUnsubscribe = new Subject();
+
   constructor(
+    private changeDetector: ChangeDetectorRef,
     private dispatcher: ListStateDispatcher,
     private actionService: SkyListSecondaryActionsService
   ) { }
 
-  public ngAfterContentInit() {
+  public ngOnInit() {
+    this.actionService.secondaryActionsSubject
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((count: number) => {
+        const hasSecondaryActions = (count > 0);
+        this.dropdownHidden = !hasSecondaryActions;
+        this.changeDetector.markForCheck();
+      });
+
+    this.actionService.actionsStream
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((actions: SkyListSecondaryAction[]) => {
+        this.actions = actions;
+        this.changeDetector.markForCheck();
+      });
+  }
+
+  public ngAfterViewInit() {
     const secondaryActionItem = new ListToolbarItemModel({
       id: 'secondary-actions',
       template: this.secondaryActionsTemplate,
@@ -43,13 +69,10 @@ export class SkyListSecondaryActionsComponent implements AfterContentInit {
     });
 
     this.dispatcher.toolbarAddItems([secondaryActionItem], -1);
+  }
 
-    this.actionService.secondaryActionsSubject.subscribe((count: number) => {
-      this.dropdownHidden = (count < 1);
-    });
-
-    this.actionService.actionsStream.subscribe((actions: SkyListSecondaryAction[]) => {
-      this.actions.next(actions);
-    });
+  public ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
--- src/modules/list-view-grid/list-view-grid.component.ts
+++ src/modules/list-view-grid/list-view-grid.component.ts
@@ -10,8 +10,9 @@ import {
   ViewChild
 } from '@angular/core';
 
-import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 import {
   getValue
@@ -160,6 +161,7 @@ export class SkyListViewGridComponent
     }).distinctUntilChanged();
 
     this.gridState.map(s => s.columns.items)
+      .takeUntil(this.ngUnsubscribe)
       .distinctUntilChanged()
       .subscribe(columns => {
         if (this.hiddenColumns) {
@@ -223,6 +225,7 @@ export class SkyListViewGridComponent
 
   public onViewActive() {
     let sub = this.gridState.map(s => s.displayedColumns.items)
+      .takeUntil(this.ngUnsubscribe)
       .distinctUntilChanged()
       .subscribe(displayedColumns => {
         let setFunctions =
@@ -246,12 +249,14 @@ export class SkyListViewGridComponent
 
   private handleColumnChange() {
      // watch for changes in column components
-    this.columnComponents.changes.subscribe((columnComponents) => {
-      let columnModels = this.columnComponents.map(column => {
-        return new SkyGridColumnModel(column.template, column);
+    this.columnComponents.changes
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((columnComponents) => {
+        let columnModels = this.columnComponents.map(column => {
+          return new SkyGridColumnModel(column.template, column);
+        });
+        this.gridDispatcher.next(new ListViewGridColumnsLoadAction(columnModels, true));
       });
-      this.gridDispatcher.next(new ListViewGridColumnsLoadAction(columnModels, true));
-    });
 
     // Watch for column heading changes:
     this.columnComponents.forEach((comp: SkyGridColumnComponent) => {
