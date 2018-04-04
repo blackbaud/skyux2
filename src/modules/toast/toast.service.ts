f4c10f0097e019c791203ac8c7c9d6024cedf10b
Conor Wright <Conor.Wright@blackbaud.com>
2018-04-04 11:21:43 -0400

    began working on toast component from scratch

--- src/modules/toast/toast.service.ts
+++ src/modules/toast/toast.service.ts
@@ -1,15 +1,89 @@
 import { Injectable } from '@angular/core';
-import { ErrorModalConfig } from './error-modal-config';
-import { SkyErrorModalFormComponent } from './error-modal-form.component';
-import { SkyModalService } from '../modal/modal.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+
+export enum ToastType {
+  Info,
+  Success,
+  Warning,
+  Danger
+}
+
+export interface Message {
+  id: string,
+  message: string,
+  toastType: string,
+  timeout?: NodeJS.Timer
+}
+export interface ToastConfig {
+  message?: string,
+  disableTimeout?: boolean,
+  timeout?: number,
+  toastType?: ToastType
+}
 
 @Injectable()
-export class SkyErrorModalService {
-  constructor(private modal: SkyModalService) {}
+export class SkyToastService {
 
-  public open(config: ErrorModalConfig) {
-    const providers = [{ provide: ErrorModalConfig, useValue: config }];
+  private _messages: Message[] = [];
+  private _messageList: BehaviorSubject<Message[]> = new BehaviorSubject([]);
+  public get messageList(): Observable<Message[]> { return this._messageList.asObservable() }
 
-    this.modal.open(SkyErrorModalFormComponent, providers);
+  constructor() {}
+
+  public openMessage(message: string, config?: ToastConfig) {
+    config.message = message;
+    return this.open(config);
+  }
+
+  public open(config: ToastConfig) {
+    let message: Message = this.createMessage(config);
+
+    this._messages.push(message);
+    this._messageList.next(this._messages);
+  }
+
+  public close(messageId: string) {
+    let message: Message = this._messages.reduce((prev, cur) => prev.id === messageId ? prev : cur);
+    if (!message) {
+      throw 'SkyToast message not found.';
+    }
+
+    if (message.timeout) {
+      clearTimeout(message.timeout);
+    }
+    this._messages.map(message => message.id !== messageId);
+    this._messageList.next(this._messages);
+  }
+
+  private createMessage(config: ToastConfig): Message {
+    if (!config.message) {
+      throw 'A message must be provided.';
+    }
+    let message: Message = {
+      message: config.message,
+      toastType: 'info',
+      id: '_' + Math.random().toString(36).substr(2, 9)
+    };
+
+    if (!config.disableTimeout) {
+      message.timeout = setTimeout(() => {
+        this.close(message.id);
+      }, config.timeout || 10000);
+    }
+
+    switch(config.toastType) {
+      case ToastType.Success:
+        message.toastType = 'success';
+        break;
+      case ToastType.Warning:
+        message.toastType = 'warning';
+        break;
+      case ToastType.Danger:
+        message.toastType = 'danger';
+        break;
+    }
+
+    return message;
   }
 }
