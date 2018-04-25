e548824e03e429f736b3ba3aae8f146f2f841039
Conor Wright <Conor.Wright@blackbaud.com>
2018-04-25 15:28:43 -0400

    added unit testing for the toast adapter

---
+++ src/modules/toast/services/toast-adapter.service.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+import { SkyWindowRefService } from '../../window';
+import { RendererFactory2 } from '@angular/core';
+import { SkyToastAdapterService } from './toast-adapter.service';
+
+describe('Toast adapter service', () => {
+
+  let adapter: SkyToastAdapterService;
+  let rendererCallCounts = {
+    appendCalledCount: 0,
+    removeCalledCount: 0
+  };
+
+  beforeEach(() => {
+    let rendererMock = {
+      appendChild: () => { rendererCallCounts.appendCalledCount++; },
+      removeChild: () => { rendererCallCounts.removeCalledCount++; }
+    };
+    TestBed.configureTestingModule({
+      providers: [
+        SkyToastAdapterService,
+        SkyWindowRefService,
+        {
+          provide: RendererFactory2,
+          useValue: {
+            createRenderer() { return rendererMock; }
+          }
+        }
+      ]
+    });
+    adapter = TestBed.get(SkyToastAdapterService);
+  });
+
+  it('should append element to body', () => {
+    adapter.appendToBody(undefined);
+    expect(rendererCallCounts.appendCalledCount).toBe(1);
+  });
+
+  it('should remove element from body', () => {
+    adapter.removeHostElement();
+    expect(rendererCallCounts.removeCalledCount).toBe(1);
+  });
+});
