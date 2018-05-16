80d1a7a464b2afe4e1571c3d0dd253a1682a75af
Conor Wright <Conor.Wright@blackbaud.com>
2018-05-16 11:35:13 -0400

    removed excess

--- src/modules/radio/radio-group/radio-group.component.ts
+++ src/modules/radio/radio-group/radio-group.component.ts
@@ -7,7 +7,8 @@ import {
   QueryList,
   EventEmitter,
   Output,
-  Component
+  Component,
+  ChangeDetectionStrategy
 } from '@angular/core';
 import {
   ControlValueAccessor,
@@ -17,16 +18,8 @@ import {
   SkyRadioComponent
 } from '../radio.component';
 
-/**
- * Auto-incrementing integer used to generate unique ids for checkbox components.
- */
 let nextId = 0;
 
-/**
- * Provider Expression that allows sky-radio-group to register as a ControlValueAccessor. This
- * allows it to support [(ngModel)] and ngControl.
- * @docs-private
- */
 // tslint:disable:no-forward-ref no-use-before-declare
 export const SKY_RADIO_GROUP_CONTROL_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -35,58 +28,31 @@ export const SKY_RADIO_GROUP_CONTROL_VALUE_ACCESSOR: any = {
 };
 // tslint:enable
 
-/** Change event object emitted by SkyRadio and SkyRadioGroup. */
 export class SkyRadioChange {
   constructor(
-    /** The SkyRadioComponent that emits the change event. */
     public source: SkyRadioComponent,
-    /** The value of the SkyRadioComponent. */
     public value: any) {}
 }
 
-/**
- * A group of radio buttons. May contain one or more `<sky-radio>` elements.
- */
 @Component({
   selector: 'sky-radio-group',
   templateUrl: 'radio-group.component.html',
   styleUrls: ['radio-group.component.scss'],
   exportAs: 'skyRadioGroup',
-  providers: [SKY_RADIO_GROUP_CONTROL_VALUE_ACCESSOR]
+  providers: [SKY_RADIO_GROUP_CONTROL_VALUE_ACCESSOR],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SkyRadioGroupComponent implements AfterContentInit, ControlValueAccessor {
-  /**
-   * Selected value for group. Should equal the value of the selected radio button if there *is*
-   * a corresponding radio button with a matching value. If there is *not* such a corresponding
-   * radio button, this value persists to be applied in case a new radio button is added with a
-   * matching value.
-   */
   private _value: any = undefined;
-
-  /** The HTML name attribute applied to radio buttons in this group. */
   private _name: string = `sky-radio-group-${nextId++}`;
-
-  /** The currently selected radio button. Should match value. */
   private _selected: SkyRadioComponent = undefined;
-
-  /** Whether the `value` has been set to its initial value. */
   private _isInitialized: boolean = false;
 
-  /** Whether the labels should appear after or before the radio-buttons. Defaults to 'after' */
-  private _labelPosition: 'before' | 'after' = 'after';
-
-  /**
-   * Event emitted when the group value changes.
-   * Change events are only emitted when the value changes due to user interaction with
-   * a radio button (the same behavior as `<input type-"radio">`).
-   */
   @Output() public readonly change: EventEmitter<SkyRadioChange> = new EventEmitter<SkyRadioChange>();
 
-  /** Child radio buttons. */
   @ContentChildren('sky-radio', { descendants: true })
   private _radios: QueryList<SkyRadioComponent>;
 
-  /** Name of the radio button group. All radio buttons inside this group will use this name. */
   @Input()
   get name(): string { return this._name; }
   set name(value: string) {
@@ -94,22 +60,10 @@ export class SkyRadioGroupComponent implements AfterContentInit, ControlValueAcc
     this._updateRadioButtonNames();
   }
 
-  /** Whether the labels should appear after or before the radio-buttons. Defaults to 'after' */
-  @Input()
-  get labelPosition(): 'before' | 'after' {
-    return this._labelPosition;
-  }
-  set labelPosition(v) {
-    this._labelPosition = v === 'before' ? 'before' : 'after';
-    this._markRadiosForCheck();
-  }
-
-  /** Value of the radio button. */
   @Input()
   get value(): any { return this._value; }
   set value(newValue: any) {
     if (this._value !== newValue) {
-      // Set this before proceeding to ensure no circular loop occurs with selection.
       this._value = newValue;
 
       this._updateSelectedRadioFromValue();
@@ -117,7 +71,6 @@ export class SkyRadioGroupComponent implements AfterContentInit, ControlValueAcc
     }
   }
 
-  /** Whether the radio button is selected. */
   @Input()
   get selected() { return this._selected; }
   set selected(selected: SkyRadioComponent | null) {
@@ -134,28 +87,16 @@ export class SkyRadioGroupComponent implements AfterContentInit, ControlValueAcc
     }
   }
 
-  /**
-   * Initialize properties once content children are available.
-   * This allows us to propagate relevant attributes to associated buttons.
-   */
   public ngAfterContentInit() {
-    // Mark this component as initialized in AfterContentInit because the initial value can
-    // possibly be set by NgModel on SkyRadioGroup, and it is possible that the OnInit of the
-    // NgModel occurs *after* the OnInit of the SkyRadioGroup.
     this._isInitialized = true;
   }
 
-  /**
-   * Mark this group as being "touched" (for ngModel). Meant to be called by the contained
-   * radio buttons upon their blur.
-   */
   public _touch() {
     if (this.onTouched) {
       this.onTouched();
     }
   }
 
-  /** Dispatch change event with current selection and group value. */
   public _emitChangeEvent(): void {
     if (this._isInitialized) {
       this.change.emit(new SkyRadioChange(this._selected!, this._value));
@@ -168,10 +109,6 @@ export class SkyRadioGroupComponent implements AfterContentInit, ControlValueAcc
     }
   }
 
-  /**
-   * Sets the model value. Implemented as part of ControlValueAccessor.
-   * @param value
-   */
   public writeValue(value: any) {
     this.value = value;
     this._changeDetector.markForCheck();
--- src/modules/radio/radio.component.ts
+++ src/modules/radio/radio.component.ts
@@ -21,14 +21,8 @@ import {
   UniqueSelectionService
 } from './unique-selection';
 
-/**
- * Auto-incrementing integer used to generate unique ids for checkbox components.
- */
 let nextId = 0;
 
-/**
- * A radio-button. Typically placed inside of `<sky-radio-group>` elements.
- */
 @Component({
   moduleId: module.id,
   selector: 'sky-radio',
@@ -42,22 +36,16 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
 
   private _uniqueId: string = `sky-radio-${++nextId}`;
 
-  /** The unique ID for the radio button. */
   @Input() public id: string = this._uniqueId;
 
-  /** Analog to HTML 'name' attribute used to group radios for unique selection. */
   @Input() public name: string;
 
-  /** Used to set the 'aria-label' attribute on the underlying input element. */
   @Input() public ariaLabel: string;
 
-  /** The 'aria-labelledby' attribute takes precedence as the element's text alternative. */
   @Input() public ariaLabelledby: string;
 
-  /** The 'aria-describedby' attribute is read after the element's label and field type. */
   @Input() public ariaDescribedby: string;
 
-  /** Whether this radio button is checked. */
   @Input()
   get checked(): boolean { return this._checked; }
   set checked(value: boolean) {
@@ -67,21 +55,16 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
       if (newCheckedState && this.radioGroup && this.radioGroup.value !== this.value) {
         this.radioGroup.selected = this;
       } else if (!newCheckedState && this.radioGroup && this.radioGroup.value === this.value) {
-
-        // When unchecking the selected radio button, update the selected radio
-        // property on the group.
         this.radioGroup.selected = undefined;
       }
 
       if (newCheckedState) {
-        // Notify all radio buttons with the same name to un-check.
         this._radioDispatcher.notify(this.id, this.name);
       }
       this._changeDetector.markForCheck();
     }
   }
 
-  /** The value of this radio button. */
   @Input()
   get value(): any { return this._value; }
   set value(value: any) {
@@ -89,7 +72,6 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
       this._value = value;
       if (this.radioGroup) {
         if (!this.checked) {
-          // Update checked when the value changed to match the radio group's value
           this.checked = this.radioGroup.value === value;
         }
         if (this.checked) {
@@ -99,7 +81,6 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
     }
   }
 
-  /** Whether the label should appear after or before the radio button. Defaults to 'after' */
   @Input()
   get labelPosition(): 'before' | 'after' {
     return this._labelPosition || (this.radioGroup && this.radioGroup.labelPosition) || 'after';
@@ -109,26 +90,16 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
   }
   private _labelPosition: 'before' | 'after';
 
-  /**
-   * Event emitted when the checked state of this radio button changes.
-   * Change events are only emitted when the value changes due to user interaction with
-   * the radio button (the same behavior as `<input type-"radio">`).
-   */
   @Output() public readonly change: EventEmitter<SkyRadioChange> = new EventEmitter<SkyRadioChange>();
 
-  /** The parent radio group. May or may not be present. */
   private radioGroup: SkyRadioGroupComponent;
 
-  /** ID of the native input element inside `<sky-radio-button>` */
   get inputId(): string { return `${this.id || this._uniqueId}-input`; }
 
-  /** Whether this radio is checked. */
   private _checked: boolean = false;
 
-  /** Value assigned to this radio. */
   private _value: any = undefined;
 
-  /** The native `<input type=radio>` element */
   @ViewChild('input') private input: ElementRef;
 
   constructor(
@@ -147,27 +118,17 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
       });
   }
 
-  /** Focuses the radio button. */
   public focus(): void {
     this.input.nativeElement.focus();
   }
 
-  /**
-   * Marks the radio button as needing checking for change detection.
-   * This method is exposed because the parent radio group will directly
-   * update bound properties of the radio button.
-   */
   public _markForCheck() {
-    // When group value changes, the button will not be notified. Use `markForCheck` to explicit
-    // update radio button's status
     this._changeDetector.markForCheck();
   }
 
   public ngOnInit() {
     if (this.radioGroup) {
-      // If the radio is inside a radio group, determine if it should be checked
       this.checked = this.radioGroup.value === this._value;
-      // Copy name from parent radio group
       this.name = this.radioGroup.name;
     }
   }
@@ -177,24 +138,10 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
   }
 
   public _onInputClick(event: Event) {
-    // We have to stop propagation for click events on the visual hidden input element.
-    // By default, when a user clicks on a label element, a generated click event will be
-    // dispatched on the associated input element. Since we are using a label element as our
-    // root container, the click event on the `radio-button` will be executed twice.
-    // The real click event will bubble up, and the generated click event also tries to bubble up.
-    // This will lead to multiple click events.
-    // Preventing bubbling for the second event will solve that issue.
     event.stopPropagation();
   }
 
-  /**
-   * Triggered when the radio button received a click or the input recognized any change.
-   * Clicking on a label element, will trigger a change event on the associated input.
-   */
   public _onInputChange(event: Event) {
-    // We always have to stop propagation on the change event.
-    // Otherwise the change event, from the input element, will bubble up and
-    // emit its event object to the `change` output.
     event.stopPropagation();
 
     const groupValueChanged = this.radioGroup && this.value !== this.radioGroup.value;
@@ -210,17 +157,14 @@ export class SkyRadioComponent implements OnInit, OnDestroy {
     }
   }
 
-  /** Function is called whenever the focus changes for the input element. */
   public onInputFocusChange(event: Event) {
     if (this.radioGroup) {
       this.radioGroup._touch();
     }
   }
 
-  /** Unregister function for _radioDispatcher */
   private _removeUniqueSelectionListener: () => void = () => {};
 
-  /** Dispatch change event with current value. */
   private _emitChangeEvent(): void {
     this.change.emit(new SkyRadioChange(this, this._value));
   }
