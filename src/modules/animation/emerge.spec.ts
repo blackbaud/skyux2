a2fb02de0606c2792d6b5275648b1ecf77a333e2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-05-18 17:22:30 -0400

    Fixed unit test for animation

--- src/modules/animation/emerge.spec.ts
+++ src/modules/animation/emerge.spec.ts
@@ -7,10 +7,12 @@ import {
 describe('Animation emerge', () => {
   it('should define an animation trigger', () => {
     const definitions: any = skyAnimationEmerge.definitions;
-
     expect(skyAnimationEmerge.name).toEqual('skyAnimationEmerge');
     expect(definitions[0].name).toEqual('open');
     expect(definitions[1].name).toEqual('closed');
-    expect(definitions[2].animation.timings).toEqual('300ms ease-in-out');
+    expect(definitions[2].expr).toEqual('void => *');
+    expect(definitions[2].animation[1].timings).toEqual('300ms ease-in-out');
+    expect(definitions[3].expr).toEqual('* <=> *');
+    expect(definitions[3].animation.timings).toEqual('300ms ease-in-out');
   });
 });
