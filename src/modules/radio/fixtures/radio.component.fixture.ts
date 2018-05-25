ba6f1da2bfc4265fe942d20bc1b01af7db882b53
Conor Wright <Conor.Wright@blackbaud.com>
2018-05-25 11:15:01 -0400

    fixed ts lint issue

--- src/modules/radio/fixtures/radio.component.fixture.ts
+++ src/modules/radio/fixtures/radio.component.fixture.ts
@@ -1,16 +1,22 @@
-import { Component } from "@angular/core";
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   templateUrl: './radio.component.fixture.html'
 })
 export class RadioTestComponent {
+  public selectedValue = '1';
+  public disabled2: boolean = false;
+
   public value1 = '1';
   public value2 = '2';
   public value3 = '3';
-  public disabled2: boolean = false;
+
   public label1: string;
   public labelledBy3: string;
+
   public tabindex2: string;
-  public selectedValue = '1';
+
   public onClick() {}
 }
