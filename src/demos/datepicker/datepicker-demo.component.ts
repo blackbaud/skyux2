a4e2e087cd1e2484d2f843b66acce4182e1037d1
Conor Wright <conor.wright@blackbaud.com>
2018-06-08 14:43:39 -0400

    Datepicker input clear fix (#1729)
    * Resolving Card action bar still visable after hiding with *ngIf #1421
    
    * Addressed PR style comments
    
    * updated demo with clear button
    
    * added datepicker support for undefined values

--- src/demos/datepicker/datepicker-demo.component.ts
+++ src/demos/datepicker/datepicker-demo.component.ts
@@ -8,4 +8,8 @@ export class SkyDatepickerDemoComponent {
   public minDate: Date;
   public maxDate: Date;
   public selectedDate: Date;
+
+  public clearSelectedDate() {
+    this.selectedDate = undefined;
+  }
 }
