ce8bf1ffeea29194efd2cb607364061539d22590
Jacob Wood <Jacob.Wood@blackbaud.me>
2018-07-12 11:15:09 -0400

    Added truncate and truncateAfter options to numeric pipe To be consumed by future currency input control in development Resolves: #1643

--- src/modules/numeric/numeric.options.ts
+++ src/modules/numeric/numeric.options.ts
@@ -2,4 +2,6 @@ export class NumericOptions {
   public digits = 1;
   public format = 'number';
   public iso = 'USD';
+  public truncate: boolean = true;
+  public truncateAfter: number = 0;
 }
--- src/modules/numeric/numeric.pipe.ts
+++ src/modules/numeric/numeric.pipe.ts
@@ -4,8 +4,8 @@ import { NumericOptions } from './numeric.options';
 
 /*
  * Shortens numbers to 1K, 1M, 1B, 1T and can format for currency
- * All three arguments in the options object, Digits, format and ISO, are optional,
- * defaulting to 1, number and USD respectively
+ * All five arguments in the options object, Digits, format, iso, truncate, truncateAfter are optional,
+ * defaulting to 1, number and USD, true, 0 respectively
  * Usage:
  *  number_expression | skyNumeric[:numbericOptions]]]
  *
@@ -13,12 +13,23 @@ import { NumericOptions } from './numeric.options';
  *    digits
  *    format
  *    iso
+ *    truncate
+ *    truncateAfter
  * Example:
  *  {{ 1075 | skyNumeric:{digits: 1, format: 'currency', iso: 'USD'} }}
  *  formats to: $1.1K
  * Example:
  *  {{ 2075000 | skyNumeric:{digits: 2} }}
  *  formats to: 2.08M
+ * Example:
+ *  {{ 2075000 | skyNumeric:{truncate: false} }}
+ *  formats to: 2,075,000
+ *  Example:
+ *  {{ 9500 | skyNumeric:{truncateAfter: 10000} }}
+ *  formats to: 9,500
+ *  Example:
+ *  {{ 10001 | skyNumeric:{truncateAfter: 10000} }}
+ *  formats to: 10K
  * Note: Be sure you have a space between the curly bracket surrounding the options object
  * and the two curly brackets closing the pipe or it will not work.  Thanks angular pipes.
 */
@@ -42,6 +53,12 @@ export class SkyNumericPipe implements PipeTransform {
       }
       if (optionsObject.iso !== undefined) {
         options.iso = optionsObject.iso;
+      }
+      if (optionsObject.truncate !== undefined) {
+        options.truncate = optionsObject.truncate;
+      }
+      if (optionsObject.truncateAfter !== undefined) {
+        options.truncateAfter = optionsObject.truncateAfter;
       }
     }
     return this.skyNumeric.formatNumber(value, options);
