db157d22479e5ee928a2f1ea266cd884bd221f92
Conor Wright <Conor.Wright@blackbaud.com>
2018-07-24 16:47:25 -0400

    added infinite scroll list demo

--- src/demos/infinite-scroll/infinite-scroll-demo.component.ts
+++ src/demos/infinite-scroll/infinite-scroll-demo.component.ts
@@ -2,8 +2,10 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { BehaviorSubject } from '../../../node_modules/rxjs';
 
 let nextId = 0;
+let nextListId = 0;
 
 @Component({
   selector: 'sky-infinite-scroll-demo',
@@ -14,8 +16,13 @@ export class SkyInfiniteScrollDemoComponent implements OnInit {
   public data: any[] = [];
   public hasMore = true;
 
+  public listItems = new BehaviorSubject<any[]>([]);
+  public listHasMore = true;
+  private _listItems: any[] = [];
+
   public ngOnInit(): void {
     this.addData();
+    this.addListData();
   }
 
   public onScrollEnd(): void {
@@ -24,10 +31,24 @@ export class SkyInfiniteScrollDemoComponent implements OnInit {
     }
   }
 
+  public onListScrollEnd(): void {
+    if (this.listHasMore) {
+      this.addListData();
+    }
+  }
+
   private addData(): void {
     this.mockRemote().then((result: any) => {
       this.data = this.data.concat(result.data);
       this.hasMore = result.hasMore;
+    });
+  }
+
+  private addListData(): void {
+    this.mockListRemote().then((result: any) => {
+      this._listItems = this._listItems.concat(result.data);
+      this.listItems.next(this._listItems);
+      this.listHasMore = result.hasMore;
     });
   }
 
@@ -46,6 +67,26 @@ export class SkyInfiniteScrollDemoComponent implements OnInit {
         resolve({
           data,
           hasMore: (nextId < 50)
+        });
+      }, 1000);
+    });
+  }
+
+  private mockListRemote(): Promise<any> {
+    const data: any[] = [];
+
+    for (let i = 0; i < 8; i++) {
+      data.push({
+        name: `List item #${++nextListId}`
+      });
+    }
+
+    // Simulate async request.
+    return new Promise((resolve: any) => {
+      setTimeout(() => {
+        resolve({
+          data,
+          hasMore: (nextListId < 50)
         });
       }, 1000);
     });
