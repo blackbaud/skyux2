d445d964a94cd32f00abd0faa70d9d52e8f28105
Conor Wright <Conor.Wright@blackbaud.com>
2018-08-03 14:06:44 -0400

    initial component scaffolding

---
+++ src/modules/checkbox/checkbox-icon-button.component.ts
@@ -0,0 +1,10 @@
+import { SkyCheckboxComponent } from './checkbox.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-checkbox-icon-button',
+  styleUrls: ['./checkbox-icon-button.component.scss'],
+  templateUrl: './checkbox-icon-button.component.html'
+})
+export class SkyCheckboxIconButtonComponent extends SkyCheckboxComponent {
+}
