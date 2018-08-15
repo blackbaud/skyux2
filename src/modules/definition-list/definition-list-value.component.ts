d52f8907af39afd66f3397ffbd0b3ad41d1a88fb
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-08-15 15:29:32 -0400

    Updated definitino list to set a has value flag directly

--- src/modules/definition-list/definition-list-value.component.ts
+++ src/modules/definition-list/definition-list-value.component.ts
@@ -1,8 +1,12 @@
 import {
-  Component
+  Component,
+  ViewChild,
+  ElementRef
 } from '@angular/core';
 
-import { SkyDefinitionListService } from './definition-list.service';
+import {
+  SkyDefinitionListService
+} from './definition-list.service';
 
 @Component({
   selector: 'sky-definition-list-value',
@@ -10,7 +14,15 @@ import { SkyDefinitionListService } from './definition-list.service';
   styleUrls: ['./definition-list-value.component.scss']
 })
 export class SkyDefinitionListValueComponent {
+
+  @ViewChild('valueEl')
+  set valueElement(newValue: ElementRef) {
+    this.hasValue = newValue.nativeElement.innerText.trim() !== undefined;
+  }
+
   public defaultValue: string;
+
+  public hasValue: boolean;
 
   constructor(public service: SkyDefinitionListService) { }
 }
