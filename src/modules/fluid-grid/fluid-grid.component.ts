21d8abf1c62f043b881b19409dd06e5a841644d3
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-08-16 14:08:37 -0400

    Added fluid grid container

---
+++ src/modules/fluid-grid/fluid-grid.component.ts
@@ -0,0 +1,10 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-fluid-grid',
+  templateUrl: './fluid-grid.component.html',
+  styleUrls: ['./fluid-grid.component.scss']
+})
+export class SkyFluidGridComponent { }
