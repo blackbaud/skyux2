f47ea6ef1693a724dfa1d4ccb650d5160b905d38
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-21 09:17:45 -0400

    Added skyux-theme and core

--- config/utils/copy-design-tokens.js
+++ config/utils/copy-design-tokens.js
@@ -2,6 +2,6 @@ var fs = require('fs-extra');
 var path = require('path');
 
 fs.copySync(
-  path.resolve(__dirname, '../../node_modules/@blackbaud/skyux-design-tokens/scss'),
+  path.resolve(__dirname, '../../node_modules/@skyux/theme/scss'),
   path.resolve(__dirname, '../../dist/scss')
 );
