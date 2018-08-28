a3ea6b1fa444f30c3f20c5ead833ba3abd0c60b1
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-28 17:42:30 -0400

    Adopt `@skyux/core`

--- src/modules/format/format.ts
+++ src/modules/format/format.ts
@@ -1,24 +1,3 @@
-export class SkyFormat {
-
-  public static formatText(format: string, ...args: any[]): string {
-
-    if (this.isEmpty(format)) {
-        return '';
-    }
-
-    return String(format).replace(/\{(\d+)\}/g, function (match, capture) {
-        return args[parseInt(capture, 10)];
-    });
-  }
-
-  /*istanbul ignore next */
-  constructor() {
-
-  }
-
-  private static isEmpty(str: string) {
-    /* tslint:disable */
-    return str === null || str === undefined;
-    /* tslint:enable */
-  }
-}
+export {
+  SkyFormat
+} from '@skyux/core/modules/format/format';
--- src/modules/log/log.service.ts
+++ src/modules/log/log.service.ts
@@ -1,11 +1,3 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class SkyLogService {
-  public warn(message?: any, ...optionalParams: any[]) {
-    /*istanbul ignore else */
-    if (window.console) {
-      window.console.warn.apply(window.console, arguments);
-    }
-  }
-}
+export {
+  SkyLogService
+} from '@skyux/core/modules/log/log.service';
