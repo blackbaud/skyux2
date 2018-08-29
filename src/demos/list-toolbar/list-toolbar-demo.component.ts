e02f5b47a2872c20f42cb783147857f8996dbe77
Conor Wright <conor.wright@blackbaud.com>
2018-08-29 15:33:26 -0400

    Radio and checkbox icon buttons (#1892)
    * initial component scaffolding
    
    * created initial icon functionality
    
    * increased size. Added colors
    
    * finished styling. Added testing
    
    * added fixed radio icon comp import
    
    * added labels to the visual tests
    
    * override max width
    
    * add css to handle grouped icon buttons
    
    * added group examples. Addressed comments
    
    * fixed scss spacing
    
    * fixed spacing issue
    
    * updated demos to have switch group displays
    
    * updated width of buttons. Moved disabled demo. Updated list-toolbar demo
    
    * removed radio-icon html file
    
    * removed icon html and empty scss file
    
    * switched to icon buttons being just property additions
    
    * minor ts lint fix
    
    * updated skyux theme and made it take all minor revisions
    
    * Change theme dependency to use exact version
    
    * added theme dependency to visual tests
    
    * fixed visual tests for icon switches

--- src/demos/list-toolbar/list-toolbar-demo.component.ts
+++ src/demos/list-toolbar/list-toolbar-demo.component.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/observable/of';
   templateUrl: './list-toolbar-demo.component.html'
 })
 export class SkyListToolbarDemoComponent {
+  public iconGroupSelectedValue = 'table';
   public items = Observable.of([
     { id: '1', column1: 101, column2: 'Apple', column3: 'Anne eats apples' },
     { id: '2', column1: 202, column2: 'Banana', column3: 'Ben eats bananas' },
