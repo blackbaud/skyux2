1f0b93f2aa902053e0b987f043bf300d03d394b1
Conor Wright <Conor.Wright@blackbaud.com>
2018-09-25 16:22:10 -0400

    added hidden note to demo;moved long template logic to ts file

--- src/modules/progress-indicator/progress-indicator-item/progress-indicator-item.component.ts
+++ src/modules/progress-indicator/progress-indicator-item/progress-indicator-item.component.ts
@@ -32,6 +32,31 @@ export class SkyProgressIndicatorItemComponent {
     }
   }
 
+  public get isLineDisplayed(): boolean {
+    return !(this.isHorizontal || this.isPassive) || this.isPassive && !this.isLastItem || this.isActive;
+  }
+
+  public get headingClassList(): {[className: string]: boolean} {
+    return {
+      'sky-text-success': this.isComplete && !this.isActive && !this.isHorizontal,
+      'sky-progress-indicator-item-passive': this.isPassive,
+      'sky-progress-indicator-item-step': !this.isHorizontal && (!this.isComplete || this.isActive),
+      'sky-progress-indicator-item-step-active': this.isActive,
+      'sky-progress-indicator-item-step-complete': this.isComplete && !this.isActive && !this.isHorizontal
+    };
+  }
+
+  public get lineClassList(): {[className: string]: boolean} {
+    return {
+      'sky-deemphasized': this.isPassive,
+      'sky-progress-indicator-item-passive': this.isPassive,
+      'sky-progress-indicator-item-horizontal': this.isHorizontal,
+      'sky-progress-indicator-item-last': this.isLastItem,
+      'sky-progress-indicator-item-line-inactive': !this.isActive && !this.isComplete,
+      'sky-progress-indicator-item-line-next-inactive': this.isNextToInactive
+    };
+  }
+
   public get isHorizontal(): boolean {
     return this.displayMode === SkyProgressIndicatorDisplayMode.Horizontal;
   }
