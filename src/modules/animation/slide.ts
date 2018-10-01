dbd7db58f2c9d8678ece6e3979d6443a96db7c39
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-10-01 14:50:47 -0400

    Ported animation library into PR

--- src/modules/animation/slide.ts
+++ src/modules/animation/slide.ts
@@ -1,28 +1,3 @@
-// #region imports
-import {
-  AnimationEntryMetadata
-} from '@angular/core';
-
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
-// #endregion
-
-export const skyAnimationSlide = trigger('skyAnimationSlide', [
-  state('down', style({
-    overflow: 'visible',
-    height: '*'
-  })),
-  state('up', style({
-    overflow: 'hidden',
-    height: 0
-  })),
-  transition(
-    'up <=> down',
-    animate('150ms ease-in')
-  )
-]) as AnimationEntryMetadata;
+export {
+  skyAnimationSlide
+} from '@skyux/animations/slide';
