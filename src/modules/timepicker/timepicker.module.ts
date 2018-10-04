7147537a529bc9f3597b9abf6164a054be5bbc46
Conor Wright <Conor.Wright@blackbaud.com>
2018-10-04 16:38:11 -0400

    replaced datepicker and timepicker with datetime lib

--- src/modules/timepicker/timepicker.module.ts
+++ src/modules/timepicker/timepicker.module.ts
@@ -1,23 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SkyTimepickerInputDirective } from './timepicker.directive';
-import { SkyTimepickerComponent } from './timepicker.component';
-import { SkyDropdownModule } from '../dropdown/dropdown.module';
-import { SkyResourcesModule } from '../resources';
-
-@NgModule({
-  declarations: [
-    SkyTimepickerInputDirective,
-    SkyTimepickerComponent
-  ],
-  imports: [
-    CommonModule,
-    SkyResourcesModule,
-    SkyDropdownModule
-  ],
-  exports: [
-    SkyTimepickerInputDirective,
-    SkyTimepickerComponent
-  ]
-})
-export class SkyTimepickerModule { }
+export {
+  SkyTimepickerModule
+} from '@skyux/datetime';
