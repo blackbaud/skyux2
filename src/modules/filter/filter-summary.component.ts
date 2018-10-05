b3956f7d645b8c472b41a34f530f968236e2ec02
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-10-05 12:53:27 -0400

    Initial work

--- src/modules/filter/filter-summary.component.ts
+++ src/modules/filter/filter-summary.component.ts
@@ -1,13 +1,3 @@
-import {
-  Component,
-  ChangeDetectionStrategy
-} from '@angular/core';
-
-@Component({
-  selector: 'sky-filter-summary',
-  styleUrls: ['./filter-summary.component.scss'],
-  templateUrl: './filter-summary.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class SkyFilterSummaryComponent {
-}
+export {
+  SkyFilterSummaryComponent
+} from '@skyux/lists/modules/filter/filter-summary.component';
--- src/modules/filter/fixtures/filter-inline.component.fixture.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sky-test-cmp',
-  templateUrl: './filter-inline.component.fixture.html'
-})
-export class FilterInlineTestComponent {
-}
