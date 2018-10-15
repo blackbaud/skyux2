f1a02a51e3186dd750e523df3a8f986fd4dc3f14
Alex Kingman <alex.kingman@blackbaud.com>
2018-10-15 14:20:12 -0400

    changed list module exports

--- src/modules/list/list.module.ts
+++ src/modules/list/list.module.ts
@@ -1,3 +1,29 @@
-export {
-  SkyListModule
-} from '@skyux/list-builder/modules/list/list.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SkyListComponent } from '@skyux/list-builder/modules/list/list.component';
+import { SkyListToolbarModule } from '../list-toolbar';
+import { SkyListViewChecklistModule } from '../list-view-checklist';
+import { SkyListViewGridModule } from '../list-view-grid';
+import { SkyListSecondaryActionsModule } from '../list-secondary-actions';
+import { SkyListFiltersModule } from '../list-filters';
+import { SkyGridModule } from '../grid';
+
+@NgModule({
+  declarations: [
+    SkyListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SkyListComponent,
+    SkyListToolbarModule,
+    SkyListViewChecklistModule,
+    SkyListViewGridModule,
+    SkyListSecondaryActionsModule,
+    SkyListFiltersModule,
+    SkyGridModule
+  ]
+})
+export class SkyListModule {
+}
