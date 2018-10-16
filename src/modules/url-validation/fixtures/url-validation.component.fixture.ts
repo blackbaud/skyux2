67c7c86a030376d3c2a781c4cc270f42782540ac
Alex <alex.kingman@blackbaud.com>
2018-10-16 17:30:54 -0400

    Adopted @skyux/validation (#2103)

--- src/modules/url-validation/fixtures/url-validation.component.fixture.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sky-test-cmp',
-  templateUrl: './url-validation.component.fixture.html'
-})
-export class UrlValidationTestComponent {
-  public urlValidator: string;
-}
