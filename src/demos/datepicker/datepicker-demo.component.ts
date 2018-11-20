ee284199962d9d3447082749409458a324284386
Conor Wright <Conor.Wright@blackbaud.com>
2018-11-20 11:34:27 -0500

    created reactive form demos for datepicker and timepicker

--- src/demos/datepicker/datepicker-demo.component.ts
+++ src/demos/datepicker/datepicker-demo.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'sky-datepicker-demo',
   templateUrl: './datepicker-demo.component.html'
 })
-export class SkyDatepickerDemoComponent {
+export class SkyDatepickerDemoComponent implements OnInit {
   public minDate: Date;
   public maxDate: Date;
   public selectedDate: Date;
+  public reactiveForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.reactiveForm = this.formBuilder.group({
+      selectedDate: new Date()
+    });
+  }
 
   public clearSelectedDate(): void {
     this.selectedDate = undefined;
--- src/demos/timepicker/timepicker-demo.component.ts
+++ src/demos/timepicker/timepicker-demo.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import {
+  FormGroup,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'sky-timepicker-demo',
   templateUrl: './timepicker-demo.component.html'
 })
-export class SkyTimepickerDemoComponent {
+export class SkyTimepickerDemoComponent implements OnInit {
   public format12 = 'hh';
   public format24 = 'HH';
   public returnFormat = 'HH:mm:ssZ';
   public selectedTime1: any = '8:30 PM';
   public selectedTime2: any = '20:30';
   public selectedTime3: any = '02:00:00-0400';
+  public reactiveForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.reactiveForm = this.formBuilder.group({
+      time: '2:15 PM'
+    });
+  }
 
   public clearSelectedTime() {
     this.selectedTime1 = undefined;
