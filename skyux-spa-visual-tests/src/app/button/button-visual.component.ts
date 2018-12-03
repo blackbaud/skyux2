2f7fda42ed3a40ad0639e02e23764220838c7ad3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-12-03 08:58:38 -0500

    Removed unused visual artifacts

--- skyux-spa-visual-tests/src/app/button/button-visual.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'button-visual',
-  templateUrl: './button-visual.component.html'
-})
-export class ButtonVisualComponent { }
--- skyux-spa-visual-tests/src/app/forms/forms-visual.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'forms-visual',
-  templateUrl: './forms-visual.component.html'
-})
-export class FormsVisualComponent {
-}
