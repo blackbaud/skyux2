abc0039cb19bd90a8c02b311462bc00b6e948927
Alex <alex.kingman@blackbaud.com>
2019-03-22 16:26:42 -0400

    list-view-grids demo > updated to include multiselect (#2328)
    * updated list-view-grids demo to include multiselect
    
    * deps bump
    
    * tslint fix

--- src/demos/list-view-grid/list-view-grid-demo.component.ts
+++ src/demos/list-view-grid/list-view-grid-demo.component.ts
@@ -1,39 +1,38 @@
 import {
-  Component,
-  OnInit
+  Component
 } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'sky-list-view-grid-demo',
   templateUrl: './list-view-grid-demo.component.html'
 })
-export class SkyListViewGridDemoComponent implements OnInit {
-  public items = Observable.of([
-    { id: '1', column1: 101, column2: 'Apple', column3: 'Anne eats apples' },
-    { id: '2', column1: 202, column2: 'Banana', column3: 'Ben eats bananas' },
-    { id: '3', column1: 303, column2: 'Pear', column3: 'Patty eats pears' },
-    { id: '4', column1: 404, column2: 'Grape', column3: 'George eats grapes' },
-    { id: '5', column1: 505, column2: 'Banana', column3: 'Becky eats bananas' },
-    { id: '6', column1: 606, column2: 'Lemon', column3: 'Larry eats lemons' },
-    { id: '7', column1: 707, column2: 'Strawberry', column3: 'Sally eats strawberries' }
-  ]);
+export class SkyListViewGridDemoComponent {
 
-  public asyncHeading = new BehaviorSubject<string>('');
+  public data = [
+    { id: '1', name: 'Niels Bohr', email: 'niels.bohr@example.com', amount: 170.75, status: 'Paid' },
+    { id: '2', name: 'Ada Lovelace', email: 'ada.lovelace@example.com', amount: 114.13, status: 'Paid' },
+    { id: '3', name: 'Marie Curie', email: 'marie.curie@example.com', amount: 111, status: 'Past due' },
+    { id: '4', name: 'Barbara McClintock', email: 'barbara.mcclintock@example.com', amount: 84.63, status: 'Paid' },
+    { id: '5', name: 'Michael Faraday', email: 'michael.faraday@example.com', amount: 83.97, status: 'Paid' },
+    { id: '6', name: 'Enrico Fermi', email: 'enrico.fermi@example.com', amount: 74.5, status: 'Past due' },
+    { id: '7', name: 'Mae C. Jemison', email: 'mae.jemison@example.com', amount: 70.86, status: 'Paid' }
+  ];
 
-  public ngOnInit() {
-    // Simulate async request:
-    setTimeout(() => {
-      this.asyncHeading.next('Column1');
-    }, 1000);
-  }
+  public dataForMultiselect = this.data.slice(0);
 
   public onHelpOpened(helpKey: string): void {
     console.log(
       `Modal header help was invoked with the following help key: ${helpKey}`
     );
+  }
+
+  public onSelectedColumnIdsChange(selectedColumnIds: string[]): void {
+    console.log(selectedColumnIds);
+  }
+
+  public onSelectedIdsChange(selectedRowsChange: Map<string, boolean>): void {
+    console.log(selectedRowsChange);
   }
 }
