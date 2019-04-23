e06c1dc5d22f2060f289f6a74945ff7971edb1eb
blackbaud-johnly <John.Lyons@blackbaud.com>
2019-04-23 08:51:52 -0400

    Remove CurrencyPipe from numeric demo

--- src/demos/numeric/numeric-demo.component.ts
+++ src/demos/numeric/numeric-demo.component.ts
@@ -2,15 +2,8 @@ import {
   Component
 } from '@angular/core';
 
-import {
-  CurrencyPipe
-} from '@angular/common';
-
 @Component({
   selector: 'sky-numeric-demo',
-  templateUrl: './numeric-demo.component.html',
-  providers: [
-    CurrencyPipe
-  ]
+  templateUrl: './numeric-demo.component.html'
 })
 export class SkyNumericDemoComponent { }
