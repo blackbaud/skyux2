fe94f1dd64fef57888df9c11e4c7fa738c95b0f7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-08-06 10:45:38 -0400

    Remove unused script

--- config/utils/copy-package-scripts.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-fs.copySync(
-  path.resolve(__dirname, '../../postinstall.js'),
-  path.resolve(__dirname, '../../dist/postinstall.js')
-);
