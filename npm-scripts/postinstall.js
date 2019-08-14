e2239b63ab407a97890b8a47ada7dbad2f58f21c
Steve Brush <steve.brush@blackbaud.com>
2019-08-14 12:04:40 -0400

    Fixed postinstall script. (#2375)

--- npm-scripts/postinstall.js
+++ npm-scripts/postinstall.js
@@ -1,6 +1,8 @@
+const logger = require('@blackbaud/skyux-logger');
+
 const fs = require('fs-extra');
 const glob = require('glob');
-const logger = require('@blackbaud/skyux-logger');
+const path = require('path');
 
 /**
  * Some of our SKY UX libraries were transpiled using TypeScript 3
@@ -11,9 +13,25 @@ const logger = require('@blackbaud/skyux-logger');
  */
 function applyMetadataBackwardsCompatability() {
 
+  const logPrefix = '[@blackbaud/skyux]';
+
+  logger.info(`${logPrefix} Checking metadata files for backwards compatibility...`);
+
+  const metadataPattern = '@skyux/**/*.metadata.json';
+
+  // The value of `process.cwd()` is different if you're running install
+  // during local development or as a consumer of `@blackbaud/skyux`.
+  const metadataPatternLocal = path.resolve(process.cwd(), 'node_modules', metadataPattern);
+  const metadataPatternConsumer = path.resolve(process.cwd(), '../../', metadataPattern);
+
   const files = glob.sync(
-    'node_modules/@skyux/**/*.metadata.json'
-  );
+    metadataPatternLocal
+  ).concat(glob.sync(
+    metadataPatternConsumer
+  ));
+
+  const numTotalFiles = files.length;
+  let numModifiedFiles = 0;
 
   files.forEach((file) => {
     const contents = fs.readFileSync(file, 'utf8').toString();
@@ -23,14 +41,22 @@ function applyMetadataBackwardsCompatability() {
       return;
     }
 
-    logger.info(`Fixing metadata version for backwards compatibility: ${file}`);
-
     const updatedContents = contents.replace(regExp, '"version":3');
 
     fs.writeFileSync(file, updatedContents, {
       encoding: 'utf8'
     });
+
+    logger.info(`${logPrefix} Modified metadata version for: ${file}`);
+
+    numModifiedFiles++;
   });
+
+  if (numModifiedFiles > 0) {
+    logger.info(`\n${logPrefix} Successfully modified ${numModifiedFiles} of ${numTotalFiles} metadata files for backwards compatibility.\n`);
+  } else {
+    logger.info(`${logPrefix} No incompatible metadata files found (out of ${numTotalFiles}).\n`);
+  }
 }
 
 applyMetadataBackwardsCompatability();
